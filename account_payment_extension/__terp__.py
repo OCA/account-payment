6b80a9a2b85998f30f89b435bdd110de99dfc350
HDA(OpenERP) <hda@tinyerp.com>
2009-10-15 16:55:40 +0530

    Changed licence terms (lp:openobject-addons/extra-trunk rev 3907.1.4)

--- account_payment_extension/__terp__.py
+++ account_payment_extension/__terp__.py
@@ -20,7 +20,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 {
     "name" : "Account Payment Extension",
     "version" : "1.1",
