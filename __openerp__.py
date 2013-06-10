e3ebb63b6c27eebe4d4e6dfee57b3e82679fea80
Virgil Dupras <virgil.dupras@savoirfairelinux.com>
2013-06-10 12:13:15 -0400

    [FIX] Renamed module name to be consistent with the supplied field's new name.

---
+++ __openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+# NOTE: The name of the supplied field was initially "display_name", but it seems that OpenERP,
+# whenever it seems "name" in the field, returns the value for "name". Well...
+
+{
+    'name': 'Display name for currencies',
+    'version': '1.0',
+    'author': 'Savoir-faire Linux',
+    'website': 'http://www.savoirfairelinux.com',
+    'category': 'Generic Modules/Accounting',
+    'description': """
+Supply res.currency.print_on_check
+==================================
+
+This module improves res.currency by adding the "print_on_check" field, which stores the human
+readable name of the currency (US Dollar, Euro, Canadian Dollar, etc.)
+""",
+    'depends': ['base'],
+    'data': [
+        'currency_view.xml',
+        'currency_data.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
