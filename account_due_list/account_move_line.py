a66f1f0bfb0dd0ff01251a6f0ac76b576086be59
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-08-27 12:10:39 +0200

    [IMP] style

--- account_due_list/account_move_line.py
+++ account_due_list/account_move_line.py
@@ -6,7 +6,8 @@
 #                       Jordi Esteve <jesteve@zikzakmedia.com>
 #    
 #    Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
-#    Copyright (C) 2013 Agile Business Group sagl (<http://www.agilebg.com>)
+#    Copyright (C) 2011-2013 Agile Business Group sagl
+#    (<http://www.agilebg.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -35,7 +36,9 @@ class account_move_line(orm.Model):
         for line in self.browse(cr, uid, ids):
             inv_ids = invoice_pool.search(cr, uid, [('move_id', '=', line.move_id.id)])
             if len(inv_ids)>1:
-                raise orm.except_orm(_('Error'), _('Incongruent data: move %s has more than one invoice') % line.move_id.name)
+                raise orm.except_orm(_('Error'),
+                    _('Incongruent data: move %s has more than one invoice')
+                    % line.move_id.name)
             if inv_ids:
                 res[line.id] = inv_ids[0]
 	    else:
@@ -66,8 +69,10 @@ class account_move_line(orm.Model):
         'invoice_origin': fields.related('invoice', 'origin', type='char', string='Source Doc', store=False),
         'invoice_date': fields.related('invoice', 'date_invoice', type='date', string='Invoice Date', store=False),
         'partner_ref': fields.related('partner_id', 'ref', type='char', string='Partner Ref', store=False),
-        'payment_term_id': fields.related('invoice', 'payment_term', type='many2one', string='Payment Term', store=False, relation="account.payment.term"),
-        'stored_invoice_id': fields.function(_get_invoice, method=True, string="Invoice", type="many2one", relation="account.invoice",
+        'payment_term_id': fields.related('invoice', 'payment_term',
+            type='many2one', string='Payment Term', store=False, relation="account.payment.term"),
+        'stored_invoice_id': fields.function(_get_invoice, method=True,
+            string="Invoice", type="many2one", relation="account.invoice",
             store={
                 'account.move.line': (lambda self, cr, uid, ids, c={}: ids, ['move_id'], 10),
                 'account.invoice': (_get_move_lines, ['move_id'], 10),
@@ -78,16 +83,21 @@ class account_move_line(orm.Model):
                 }),
         }
 
-    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context={}, toolbar=False, submenu=False):
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form',
+        context={}, toolbar=False, submenu=False):
         model_data_obj = self.pool.get('ir.model.data')
-        ids = model_data_obj.search(cr, uid, [('module','=','account_due_list'), ('name','=','view_payments_tree')])
+        ids = model_data_obj.search(cr, uid, [
+            ('module','=','account_due_list'), ('name','=','view_payments_tree')
+            ])
         if ids:
             view_payments_tree_id = model_data_obj.get_object_reference(
                 cr, uid, 'account_due_list', 'view_payments_tree')
         if ids and view_id == view_payments_tree_id[1]:
             # Use due list
-            result = super(orm.Model, self).fields_view_get(cr, uid, view_id, view_type, context, toolbar=toolbar, submenu=submenu)
+            result = super(orm.Model, self).fields_view_get(cr, uid, view_id,
+                view_type, context, toolbar=toolbar, submenu=submenu)
         else:
             # Use special views for account.move.line object (for ex. tree view contains user defined fields)
-            result = super(account_move_line, self).fields_view_get(cr, uid, view_id, view_type, context, toolbar=toolbar, submenu=submenu)
+            result = super(account_move_line, self).fields_view_get(cr, uid,
+                view_id, view_type, context, toolbar=toolbar, submenu=submenu)
         return result
