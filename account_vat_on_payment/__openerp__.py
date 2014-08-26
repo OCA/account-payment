0a95aa5cfd15c1982b833d4f4209039dac5a058d
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-08-26 09:48:43 +0200

    [ADD] draft tests

--- account_vat_on_payment/__openerp__.py
+++ account_vat_on_payment/__openerp__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2011-2012 Domsense s.r.l. (<http://www.domsense.com>).
-#    Copyright (C) 2012-2013 Agile Business Group sagl
+#    Copyright (C) 2012-2014 Agile Business Group sagl
 #    (<http://www.agilebg.com>)
+#    @authors Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+#             Stefano Sforzi <stefano.sforzi@agilebg.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -48,13 +49,17 @@ Howto:
 http://planet.agilebg.com/en/2012/10/vat-on-payment-treatment-with-openerp/
     """,
     'website': 'http://www.agilebg.com',
-    'init_xml': [],
-    'update_xml': [
+    'data': [
         'account_view.xml',
         'company_view.xml',
         'partner_view.xml',
         ],
-    'demo_xml': [],  # TODO YAML tests
+    'demo': [
+        'account_demo.xml',
+        ],
+    'test': [
+        'test/account_invoice.yml',
+        ],
     'installable': True,
     'active': False,
 }
