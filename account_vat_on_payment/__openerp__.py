16261a1a77f1624946a2673e26df6778cb7e2eec
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-08-26 15:27:09 +0200

    [ADD] test case 5

--- account_vat_on_payment/__openerp__.py
+++ account_vat_on_payment/__openerp__.py
@@ -62,6 +62,7 @@ http://planet.agilebg.com/en/2012/10/vat-on-payment-treatment-with-openerp/
         'test/account_invoice_2.yml',
         'test/account_invoice_3.yml',
         'test/account_invoice_4.yml',
+        'test/account_invoice_5.yml',
         ],
     'installable': True,
     'active': False,
