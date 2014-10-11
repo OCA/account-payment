a877aec9dfe4b0635e62e39a528fa4e01923a825
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-10-11 18:20:08 +0200

    [REF] split action_move_line_create in several methods

--- account_vat_on_payment/account_voucher.py
+++ account_vat_on_payment/account_voucher.py
@@ -52,6 +52,179 @@ class account_voucher(orm.Model):
                     is on a VAT on payment treatment"""))
         return vat_on_p
 
+    def _compute_new_line_amount(
+        self, cr, uid, voucher, inv_move_line, amounts_by_invoice, invoice,
+        context=None
+    ):
+        currency_obj = self.pool.get('res.currency')
+        # compute the VAT or base line proportionally to
+        # the paid amount
+        if (
+            voucher.exclude_write_off
+            and voucher.payment_option == 'with_writeoff'
+        ):
+            # avoid including write-off if set in voucher.
+            # That means: use the invoice's total
+            # (as we are in 'full reconcile' case)
+            allocated_amount = amounts_by_invoice[invoice.id]['allocated']
+        else:
+            allocated_amount = (
+                amounts_by_invoice[invoice.id]['allocated']
+                +
+                amounts_by_invoice[invoice.id]['write-off']
+                )
+        new_line_amount = currency_obj.round(
+            cr, uid, voucher.company_id.currency_id,
+            (allocated_amount / amounts_by_invoice[invoice.id]['total'])
+            *
+            (inv_move_line.credit or inv_move_line.debit)
+        )
+        return new_line_amount
+
+    def _compute_new_line_currency_amount(
+        self, cr, uid, voucher, inv_move_line, amounts_by_invoice, invoice,
+        context=None
+    ):
+        currency_obj = self.pool.get('res.currency')
+        new_line_amount_curr = False
+        if (
+            amounts_by_invoice[invoice.id].get('allocated_currency')
+            and amounts_by_invoice[invoice.id].get('foreign_currency_id')
+        ):
+            for_curr = currency_obj.browse(
+                cr, uid, amounts_by_invoice[invoice.id]['foreign_currency_id'],
+                context=context)
+            if (
+                voucher.exclude_write_off
+                and voucher.payment_option == 'with_writeoff'
+            ):
+                # again
+                # avoid including write-off if set in
+                # voucher.
+                allocated_amount = amounts_by_invoice[
+                    invoice.id]['allocated_currency']
+            else:
+                allocated_amount = (
+                    amounts_by_invoice[invoice.id]['allocated_currency']
+                    +
+                    amounts_by_invoice[invoice.id]['currency-write-off']
+                )
+            new_line_amount_curr = currency_obj.round(
+                cr, uid, for_curr,
+                (
+                    allocated_amount /
+                    amounts_by_invoice[invoice.id]['total_currency']
+                )
+                *
+                (inv_move_line.amount_currency)
+            )
+        return new_line_amount_curr
+
+    def _prepare_real_move_line(
+        self, cr, uid, inv_move_line, new_line_amount, new_line_amount_curr,
+        foreign_curr_id, context=None
+    ):
+        if not inv_move_line.real_account_id:
+            raise orm.except_orm(
+                _('Error'),
+                _("""We are on a VAT on payment treatment
+                but move line %s does not have a related
+                real account""")
+                % inv_move_line.name)
+        vals = {
+            'name': inv_move_line.name,
+            'account_id': inv_move_line.real_account_id.id,
+            'credit': (inv_move_line.credit and new_line_amount or 0.0),
+            'debit': (inv_move_line.debit and new_line_amount or 0.0),
+            'type': 'real',
+            'partner_id': (
+                inv_move_line.partner_id
+                and inv_move_line.partner_id.id or False)
+            }
+        if new_line_amount_curr:
+            vals['amount_currency'] = new_line_amount_curr
+            vals['currency_id'] = foreign_curr_id
+        if inv_move_line.tax_code_id:
+            if not inv_move_line.real_tax_code_id:
+                raise orm.except_orm(
+                    _('Error'),
+                    _("""We are on a VAT on payment
+                    treatment but move line %s does not
+                    have a related real tax code""")
+                    % inv_move_line.name
+                )
+            vals['tax_code_id'] = inv_move_line.real_tax_code_id.id
+            if inv_move_line.tax_amount < 0:
+                vals['tax_amount'] = -new_line_amount
+            else:
+                vals['tax_amount'] = new_line_amount
+        return vals
+
+    def _prepare_shadow_move_line(
+        self, cr, uid, inv_move_line, new_line_amount,
+        context=None
+    ):
+        vals = {
+            'name': inv_move_line.name,
+            'account_id': inv_move_line.account_id.id,
+            'credit': (
+                inv_move_line.debit
+                and new_line_amount or 0.0),
+            'debit': (
+                inv_move_line.credit
+                and new_line_amount or 0.0),
+            'type': 'shadow',
+            'partner_id': (
+                inv_move_line.partner_id
+                and inv_move_line.partner_id.id or False)
+            }
+        if inv_move_line.tax_code_id:
+            vals[
+                'tax_code_id'
+                ] = inv_move_line.tax_code_id.id
+            if inv_move_line.tax_amount < 0:
+                vals['tax_amount'] = new_line_amount
+            else:
+                vals['tax_amount'] = -new_line_amount
+        return vals
+
+    def _prepare_shadow_move(self, cr, uid, voucher, context=None):
+        return {
+            'journal_id': (
+                voucher.journal_id.vat_on_payment_related_journal_id.id
+                ),
+            'period_id': voucher.move_id.period_id.id,
+            'date': voucher.move_id.date,
+            }
+
+    def _move_payment_lines_to_shadow_entry(
+        self, cr, uid, voucher, shadow_move_id, context=None
+    ):
+        for line in voucher.move_ids:
+            if line.account_id.type != 'liquidity':
+                # If the line is related to write-off and user doesn't
+                # want to compute the tax including write-off,
+                # write-off move line must stay on the real move
+                if not (
+                    voucher.exclude_write_off
+                    and voucher.payment_option == 'with_writeoff'
+                    and line.account_id.id
+                        == voucher.writeoff_acc_id.id
+                ):
+                    line.write({
+                        'move_id': shadow_move_id,
+                        }, update_check=False)
+                # this will allow user to see the real entry from
+                # invoice payment tab
+                if (
+                    line.account_id.type == 'receivable'
+                    or line.account_id.type == 'payable'
+                ):
+                    line.write({
+                        'real_payment_move_id': voucher.move_id.id,
+                        })
+        return True
+
     def action_move_line_create(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
@@ -59,7 +232,6 @@ class account_voucher(orm.Model):
         journal_pool = self.pool.get('account.journal')
         move_line_pool = self.pool.get('account.move.line')
         move_pool = self.pool.get('account.move')
-        currency_obj = self.pool.get('res.currency')
         res = False
         for voucher in self.browse(cr, uid, ids, context):
             entry_posted = voucher.journal_id.entry_posted
@@ -96,181 +268,37 @@ class account_voucher(orm.Model):
                             inv_move_line.account_id.type != 'receivable'
                             and inv_move_line.account_id.type != 'payable'
                         ):
-                            # compute the VAT or base line proportionally to
-                            # the paid amount
-                            if (
-                                voucher.exclude_write_off
-                                and voucher.payment_option == 'with_writeoff'
-                            ):
-                                # avoid including write-off if set in voucher.
-                                # That means: use the invoice's total
-                                # (as we are in 'full reconcile' case)
-                                allocated_amount = amounts_by_invoice[
-                                    invoice.id
-                                    ]['allocated']
-                            else:
-                                allocated_amount = (
-                                    amounts_by_invoice[invoice.id]['allocated']
-                                    +
-                                    amounts_by_invoice[invoice.id]['write-off']
-                                    )
-                            new_line_amount = currency_obj.round(
-                                cr, uid, voucher.company_id.currency_id,
-                                (
-                                    allocated_amount
-                                    /
-                                    amounts_by_invoice[invoice.id]['total']
-                                )
-                                *
-                                (inv_move_line.credit or inv_move_line.debit)
-                                )
-                            new_line_amount_curr = False
-                            if (
-                                amounts_by_invoice[invoice.id].get(
-                                    'allocated_currency')
-                                and amounts_by_invoice[invoice.id].get(
-                                    'foreign_currency_id')
-                            ):
-                                for_curr = currency_obj.browse(
-                                    cr, uid,
-                                    amounts_by_invoice[invoice.id][
-                                        'foreign_currency_id'],
+                            new_line_amount = self._compute_new_line_amount(
+                                cr, uid, voucher, inv_move_line,
+                                amounts_by_invoice, invoice, context=context)
+                            new_line_amount_curr = (
+                                self._compute_new_line_currency_amount(
+                                    cr, uid, voucher, inv_move_line,
+                                    amounts_by_invoice, invoice,
                                     context=context)
-                                if (
-                                    voucher.exclude_write_off
-                                    and
-                                    voucher.payment_option == 'with_writeoff'
-                                ):
-                                    # again
-                                    # avoid including write-off if set in
-                                    # voucher.
-                                    allocated_amount = amounts_by_invoice[
-                                        invoice.id]['allocated_currency']
-                                else:
-                                    allocated_amount = (
-                                        amounts_by_invoice[invoice.id][
-                                            'allocated_currency']
-                                        +
-                                        amounts_by_invoice[invoice.id][
-                                            'currency-write-off']
-                                        )
-                                new_line_amount_curr = currency_obj.round(
-                                    cr, uid, for_curr,
-                                    (
-                                        allocated_amount
-                                        /
-                                        amounts_by_invoice[
-                                            invoice.id
-                                        ]['total_currency']
-                                    )
-                                    *
-                                    (inv_move_line.amount_currency)
-                                    )
+                            )
+                            foreign_currency_id = amounts_by_invoice[
+                                invoice.id]['foreign_currency_id']
+                            real_vals = self._prepare_real_move_line(
+                                cr, uid, inv_move_line, new_line_amount,
+                                new_line_amount_curr, foreign_currency_id,
+                                context=context)
+                            lines_to_create.append(real_vals)
 
-                            if not inv_move_line.real_account_id:
-                                raise orm.except_orm(
-                                    _('Error'),
-                                    _("""We are on a VAT on payment treatment
-                                    but move line %s does not have a related
-                                    real account""")
-                                    % inv_move_line.name)
-
-                            # prepare the real move line
-                            vals = {
-                                'name': inv_move_line.name,
-                                'account_id': inv_move_line.real_account_id.id,
-                                'credit': (
-                                    inv_move_line.credit
-                                    and new_line_amount or 0.0),
-                                'debit': (
-                                    inv_move_line.debit
-                                    and new_line_amount or 0.0),
-                                'type': 'real',
-                                'partner_id': (
-                                    inv_move_line.partner_id
-                                    and inv_move_line.partner_id.id or False)
-                                }
-                            if new_line_amount_curr:
-                                vals['amount_currency'] = new_line_amount_curr
-                                vals['currency_id'] = for_curr.id
-                            if inv_move_line.tax_code_id:
-                                if not inv_move_line.real_tax_code_id:
-                                    raise orm.except_orm(
-                                        _('Error'),
-                                        _("""We are on a VAT on payment
-                                        treatment but move line %s does not
-                                        have a related real tax code""")
-                                        % inv_move_line.name
-                                    )
-                                vals[
-                                    'tax_code_id'
-                                    ] = inv_move_line.real_tax_code_id.id
-                                if inv_move_line.tax_amount < 0:
-                                    vals['tax_amount'] = -new_line_amount
-                                else:
-                                    vals['tax_amount'] = new_line_amount
-                            lines_to_create.append(vals)
-
-                            # prepare the shadow move line
-                            vals = {
-                                'name': inv_move_line.name,
-                                'account_id': inv_move_line.account_id.id,
-                                'credit': (
-                                    inv_move_line.debit
-                                    and new_line_amount or 0.0),
-                                'debit': (
-                                    inv_move_line.credit
-                                    and new_line_amount or 0.0),
-                                'type': 'shadow',
-                                'partner_id': (
-                                    inv_move_line.partner_id
-                                    and inv_move_line.partner_id.id or False)
-                                }
-                            if inv_move_line.tax_code_id:
-                                vals[
-                                    'tax_code_id'
-                                    ] = inv_move_line.tax_code_id.id
-                                if inv_move_line.tax_amount < 0:
-                                    vals['tax_amount'] = new_line_amount
-                                else:
-                                    vals['tax_amount'] = -new_line_amount
-                            lines_to_create.append(vals)
+                            shadow_vals = self._prepare_shadow_move_line(
+                                cr, uid, inv_move_line, new_line_amount,
+                                context=context)
+                            lines_to_create.append(shadow_vals)
 
                 context['journal_id'] = (
                     voucher.journal_id.vat_on_payment_related_journal_id.id)
                 context['period_id'] = voucher.move_id.period_id.id
-                shadow_move_id = move_pool.create(cr, uid, {
-                    'journal_id': (
-                        voucher.journal_id.vat_on_payment_related_journal_id.id
-                        ),
-                    'period_id': voucher.move_id.period_id.id,
-                    'date': voucher.move_id.date,
-                    }, context)
+                shadow_move_id = move_pool.create(
+                    cr, uid, self._prepare_shadow_move(
+                        cr, uid, voucher, context=context), context)
 
-                # move the payment move lines to shadow entry
-                for line in voucher.move_ids:
-                    if line.account_id.type != 'liquidity':
-                        # If the line is related to write-off and user doesn't
-                        # want to compute the tax including write-off,
-                        # write-off move line must stay on the real move
-                        if not (
-                            voucher.exclude_write_off
-                            and voucher.payment_option == 'with_writeoff'
-                            and line.account_id.id
-                                == voucher.writeoff_acc_id.id
-                        ):
-                            line.write({
-                                'move_id': shadow_move_id,
-                                }, update_check=False)
-                        # this will allow user to see the real entry from
-                        # invoice payment tab
-                        if (
-                            line.account_id.type == 'receivable'
-                            or line.account_id.type == 'payable'
-                        ):
-                            line.write({
-                                'real_payment_move_id': voucher.move_id.id,
-                                })
+                self._move_payment_lines_to_shadow_entry(
+                    cr, uid, voucher, shadow_move_id, context=context)
 
                 for line_to_create in lines_to_create:
                     if line_to_create['type'] == 'real':
