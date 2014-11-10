e7ee4baf6db9d19cfb0b8d20d946b6aba584cf5f
Alex Comba <alex.comba@agilebg.com>
2014-11-10 18:34:09 +0100

    [FIX] copyright plus other minor fixes

--- account_vat_on_payment/account_invoice.py
+++ account_vat_on_payment/account_invoice.py
@@ -3,8 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2011-2012 Domsense s.r.l. (<http://www.domsense.com>).
-#    Copyright (C) 2012-2014 Agile Business Group sagl
-#    (<http://www.agilebg.com>)
+#    Copyright (C) 2014 Agile Business Group sagl (<http://www.agilebg.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
