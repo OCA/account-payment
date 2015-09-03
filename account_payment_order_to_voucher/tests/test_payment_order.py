2b32836c9c632c13a1f90244413c4783460fb2fa
eLBati <lorenzo.battistini@agilebg.com>
2015-09-03 17:45:55 +0200

    ADD account_payment_order_to_voucher module
    IMP  Create vouchers button

---
+++ account_payment_order_to_voucher/tests/test_payment_order.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+#   See __openerp__.py about license
+#
+
+from openerp.tests.common import TransactionCase
+from openerp import fields
+
+
+class TestOrder(TransactionCase):
+
+    def test_payment_order(self):
+        payment_wizard = self.env['payment.order.create']
+        demo_invoice_0 = self.env.ref('account.demo_invoice_0')
+        payment_order_1 = self.env.ref('account_payment.payment_order_1')
+        cr, uid = self.cr, self.uid
+        demo_invoice_0.check_total = 14
+        self.registry('account.invoice').signal_workflow(
+            cr, uid, [demo_invoice_0.id], 'invoice_open')
+        self.registry('payment.order').signal_workflow(
+            cr, uid, [payment_order_1.id], 'open')
+        wizard = payment_wizard.create({
+            'duedate': fields.Date.today(),
+            'entries': [(6, 0, [demo_invoice_0.move_id.line_id[0].id])]
+            })
+        wizard.with_context({
+            'active_model': 'payment.order',
+            'active_ids': [payment_order_1.id],
+            'active_id': payment_order_1.id,
+            }).create_payment()
+        payment_order_1.set_done()
+        payment_order_1.generate_vouchers()
+        self.assertEqual(len(payment_order_1.voucher_ids), 1)
+        payment_order_1.voucher_ids[0].proforma_voucher()
+        self.assertEqual(demo_invoice_0.state, 'paid')
