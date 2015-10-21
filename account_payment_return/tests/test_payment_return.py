6d4c0e4ff559a7d1d7247f5b6a9abfa92b200fd7
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-10-21 23:43:25 +0200

    account_payment_return: Improve behaviour

--- account_payment_return/tests/test_payment_return.py
+++ account_payment_return/tests/test_payment_return.py
@@ -1,15 +1,81 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp.exceptions import Warning as UserError
 from openerp.tests.common import TransactionCase
 
-class TestPaymentReturn(TransactionCase):
-    """Tests for payment returns
 
-    Test used to check that when doing a payment return the result will be balanced.
-    """
+class TestPaymentReturn(TransactionCase):
 
     def setUp(self):
-        a = 3
         super(TestPaymentReturn, self).setUp()
-        self.account_invoice_model = self.registry('account.invoice')
+        self.account_invoice_model = self.env['account.invoice']
+        self.payment_return_model = self.env['payment.return']
+        self.partner = self.env.ref('base.res_partner_1')
+        # Prepare invoice and pay it for making the return
+        self.invoice = self.env.ref('account.invoice_2')
+        self.invoice.journal_id.update_posted = True
+        self.invoice.signal_workflow('invoice_open')
+        self.receivable_line = self.invoice.move_id.line_id.filtered(
+            lambda x: x.account_id.type == 'receivable')
+        # Invert the move to simulate the payment
+        self.payment_move = self.invoice.move_id.copy()
+        for move_line in self.payment_move.line_id:
+            debit = move_line.debit
+            move_line.write({'debit': move_line.credit,
+                             'credit': debit})
+        self.payment_line = self.payment_move.line_id.filtered(
+            lambda x: x.account_id.type == 'receivable')
+        # Reconcile both
+        self.reconcile = self.env['account.move.reconcile'].create(
+            {'type': 'manual',
+             'line_id': [(4, self.payment_line.id),
+                         (4, self.receivable_line.id)]})
+        # Create payment return
+        self.payment_return = self.payment_return_model.create(
+            {'journal_id': self.env.ref('account.bank_journal').id,
+             'line_ids': [
+                 (0, 0, {'partner_id': self.partner.id,
+                         'move_line_id': self.receivable_line.id,
+                         'amount': self.receivable_line.debit})]})
+        self.payment_return.journal_id.update_posted = True
 
-    def test_balanced_customer_invoice(self):
-        cr, uid = self.cr, self.uid
+    def test_confirm_error(self):
+        self.payment_return.line_ids[0].move_line_id = False
+        with self.assertRaises(UserError):
+            self.payment_return.action_confirm()
+
+    def test_onchange_move_line(self):
+        with self.env.do_in_onchange():
+            record = self.env['payment.return.line'].new()
+            record.move_line_id = self.payment_line.id
+            record.onchange_move_line()
+            self.assertEqual(record.amount, self.payment_line.credit)
+
+    def test_payment_return(self):
+        self.payment_return.action_cancel()  # No effect
+        self.assertEqual(self.invoice.state, 'paid')
+        self.assertEqual(self.payment_return.state, 'draft')
+        self.payment_return.action_confirm()
+        self.assertEqual(self.payment_return.state, 'done')
+        self.assertEqual(self.invoice.state, 'open')
+        self.assertEqual(self.invoice.residual, self.receivable_line.debit)
+        self.assertEqual(
+            len(self.receivable_line.reconcile_partial_id.line_partial_ids), 3)
+        self.payment_return.action_cancel()
+        self.assertEqual(self.payment_return.state, 'cancelled')
+        self.assertEqual(self.invoice.state, 'paid')
+        self.payment_return.action_draft()
+        self.assertEqual(self.payment_return.state, 'draft')
+
+    def test_payment_partial_return(self):
+        self.payment_return.line_ids[0].amount = 5.0
+        self.assertEqual(self.invoice.state, 'paid')
+        self.payment_return.action_confirm()
+        self.assertEqual(self.invoice.state, 'open')
+        self.assertEqual(self.invoice.residual, 5.0)
+        self.assertEqual(
+            len(self.receivable_line.reconcile_partial_id.line_partial_ids), 3)
+        self.payment_return.action_cancel()
+        self.assertEqual(self.invoice.state, 'paid')
