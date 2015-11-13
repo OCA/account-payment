215cb72c71cef63a0e6f6ddccdeac3173c4a1fb4
jbeficent <jordi.ballester@eficent.com>
2015-11-13 14:31:43 +0100

    [FIX] Fixed after travis errors

--- account_check_writing_remit_to/__init__.py
+++ account_check_writing_remit_to/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # © 2015 Eficent
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-import report
+from . import report
--- account_voucher_remit_to/models/__init__.py
+++ account_voucher_remit_to/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2015 Eficent
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-import res_partner
-import account_voucher
-import account_invoice
+from . import res_partner
+from . import account_voucher
+from . import account_invoice
