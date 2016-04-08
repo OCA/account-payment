14735c9a19494c53fe06b95d90316c9d43c4bffd
carlosdauden <carlos@incaser.es>
2016-04-08 19:06:46 +0200

    account_payment_return: Implement find match with reference field (#2)
    [IMP] account_payment_return: Implement find match with reference field

--- account_payment_return/models/__init__.py
+++ account_payment_return/models/__init__.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2011-2012 7 i TRIA <http://www.7itria.cat>
-#    Copyright (c) 2011-2012 Avanzosc <http://www.avanzosc.com>
-#    Copyright (c) 2013 Serv. Tecnol. Avanzados <http://www.serviciosbaeza.com>
-#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-#    Copyright (c) 2014 initOS GmbH & Co. KG <http://initos.com/>
-#                       Markus Schneider <markus.schneider at initos.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2011-2012 7 i TRIA <http://www.7itria.cat>
+# © 2011-2012 Avanzosc <http://www.avanzosc.com>
+# © 2013 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# © 2014 Markus Schneider <markus.schneider@initos.com>
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import payment_return
 from . import account_invoice
+from . import payment_return_reason
