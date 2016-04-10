37c4c2adef6ed90e91cca89d06229514d95dd8b4
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-04-11 01:27:52 +0200

    account_payment_return_import: Some refactoring
    OCA Transbot updated translations from Transifex

---
+++ account_payment_return_import/models/payment_return.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import fields, models
+
+
+class PaymentReturnLine(models.Model):
+    _inherit = 'payment.return.line'
+
+    # Ensure transactions can be imported only once (if the import format
+    # provides unique transaction ids)
+    unique_import_id = fields.Char('Import ID', readonly=True, copy=False)
+    raw_import_data = fields.Char(
+        help="XML RAW data stored for debugging/check purposes")
+
+    _sql_constraints = [
+        ('unique_import_id',
+         'unique (unique_import_id)',
+         'A bank account transactions can be imported only once !')
+    ]
