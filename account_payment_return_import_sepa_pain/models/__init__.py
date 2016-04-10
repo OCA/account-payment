78646455fb73874bb8c628ffa5bf1c3dec52ab6d
Carlos Incaser <carlos@incaser.es>
2016-04-10 06:54:11 +0200

    [ADD] account_payment_return_import_sepa_pain: New module

---
+++ account_payment_return_import_sepa_pain/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- encoding: utf-8 -*-
+
+from . import account_payment_return_import
+from . import payment_return
---
+++ account_payment_return_import_sepa_pain/tests/test_import_sepa_pain.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from openerp.addons.account_payment_return_import.tests import (
+    TestPaymentReturnFile)
+
+
+class TestImport(TestPaymentReturnFile):
+    """Run test to import payment return import."""
+
+    def setUp(self):
+        super(TestImport, self).setUp()
+        self.company = self.env.ref('base.main_company')
+        self.acc_number = 'NL77ABNA0574908765'
+        self.acc_bank = self.env['res.partner.bank'].create({
+            'state': 'iban',
+            'acc_number': self.acc_number,
+            'bank_name': 'TEST BANK',
+            'company_id': self.company.partner_id.id,
+        })
+
+    def test_payment_return_import(self):
+        """Test correct creation of single payment return."""
+        transactions = [
+            {
+                'returned_amount': 100.00,
+                'reference': 'E2EID1',
+            },
+        ]
+        self._test_return_import(
+            'account_payment_return_import_sepa_pain', 'test-sepa-unpaid.xml',
+            'MSGID99345678912',
+            local_account='NL77ABNA0574908765',
+            date='2016-10-08', transactions=transactions
+        )
+
+    def test_zip_import(self):
+        """Test import of multiple statements from zip file."""
+        self._test_return_import(
+            'account_payment_return_import_sepa_pain', 'test-sepa-unpaid.zip',
+            'MSGID99345678912',
+            local_account='NL77ABNA0574908765',
+            date='2016-10-08'
+        )
