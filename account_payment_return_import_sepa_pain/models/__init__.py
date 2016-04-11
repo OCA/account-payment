7af41ce19a77e7117c0064dfcac1dab77fc0f221
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-04-11 02:37:21 +0200

    account_payment_return_import_sepa_pain: Some fixes + refactoring

--- account_payment_return_import_sepa_pain/models/__init__.py
+++ account_payment_return_import_sepa_pain/models/__init__.py
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-from . import account_payment_return_import
 from . import payment_return
--- account_payment_return_import_sepa_pain/wizard/pain_parser.py
+++ account_payment_return_import_sepa_pain/wizard/pain_parser.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import re
 from lxml import etree
-from openerp.addons.account_payment_return_import.parserlib import (
-    PaymentReturn)
 
 
 class PainParser(object):
@@ -27,12 +26,12 @@ class PainParser(object):
         return date_node[0].text[:10]
 
     def add_value_from_node(
-            self, ns, node, xpath_str, obj, attr_name, join_str=None):
+            self, ns, node, xpath_str, obj, key, join_str=None):
         """Add value to object from first or all nodes found with xpath.
 
         If xpath_str is a list (or iterable), it will be seen as a series
         of search path's in order of preference. The first item that results
-        in a found node will be used to set a value."""
+        in a found node will be used to set a value in the dictionary."""
         if not isinstance(xpath_str, (list, tuple)):
             xpath_str = [xpath_str]
         for search_str in xpath_str:
@@ -42,25 +41,21 @@ class PainParser(object):
                     attr_value = found_node[0].text
                 else:
                     attr_value = join_str.join([x.text for x in found_node])
-                setattr(obj, attr_name, attr_value)
+                obj[key] = attr_value
                 break
 
     def parse_transaction_details(self, ns, node, transaction):
         """Parse transaction details."""
-
-        transaction.returned_amount = self.parse_amount(ns, node)
-
+        transaction['amount'] = self.parse_amount(ns, node)
         self.add_value_from_node(
-            ns, node, './ns:ReqdColltnDt', transaction, 'value_date')
+            ns, node, './ns:ReqdColltnDt', transaction, 'date')
         self.add_value_from_node(
             ns, node, './ns:RmtInf/ns:Ustrd', transaction, 'concept')
         self.add_value_from_node(
             ns, node, './ns:Dbtr/ns:Nm', transaction, 'partner_name')
         self.add_value_from_node(
             ns, node, './ns:DbtrAcct/ns:Id/ns:IBAN', transaction,
-            'remote_account')
-        self.add_value_from_node(
-            ns, node, './ns:Dbtr/ns:Nm', transaction, 'remote_owner')
+            'account_number')
 
     def parse_transaction(self, ns, node, transaction):
         """Parse transaction (entry) node."""
@@ -76,26 +71,33 @@ class PainParser(object):
             './ns:OrgnlTxRef', namespaces={'ns': ns})
         if details_node:
             self.parse_transaction_details(ns, details_node[0], transaction)
-        transaction.data = etree.tostring(node)
+        transaction['raw_import_data'] = etree.tostring(node)
         return transaction
 
     def parse_payment_return(self, ns, node):
         """Parse a single payment return node."""
-        payment_return = PaymentReturn()
+        payment_return = {}
         self.add_value_from_node(
-            ns, node, './ns:GrpHdr/ns:MsgId', payment_return,
-            'payment_return_name')
-        payment_return.date = self.parse_date(ns, node)
+            ns, node, './ns:GrpHdr/ns:MsgId', payment_return, 'name')
+        payment_return['date'] = self.parse_date(ns, node)
         transaction_nodes = node.xpath(
             './ns:OrgnlPmtInfAndSts/ns:TxInfAndSts', namespaces={'ns': ns})
         if transaction_nodes:
             self.add_value_from_node(
                 ns, transaction_nodes[0],
                 './ns:OrgnlTxRef/ns:CdtrAcct/ns:Id/ns:IBAN', payment_return,
-                'local_account')
+                'account_number')
+        payment_return['transactions'] = []
         for entry_node in transaction_nodes:
-            transaction = payment_return.create_transaction()
+            transaction = {}
             self.parse_transaction(ns, entry_node, transaction)
+            payment_return['transactions'].append(transaction)
+        # Give an unique ID to each transaction
+        subno = 0
+        for transaction in payment_return['transactions']:
+            subno += 1
+            transaction['unique_import_id'] = (
+                payment_return['name'] + str(subno).zfill(4))
         return payment_return
 
     def check_version(self, ns, root):
