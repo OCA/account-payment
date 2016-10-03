892f0f0cea692bfae82b808e2532e69e277287cb
jbeficent <jordi.ballester@eficent.com>
2016-10-03 16:40:00 +0200

    [ADD] migrate to v9 module account_due_list

--- account_due_list/models/__init__.py
+++ account_due_list/models/__init__.py
@@ -9,5 +9,4 @@
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from . import models
-
+from . import account_move_line
