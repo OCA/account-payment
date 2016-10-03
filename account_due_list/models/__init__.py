b2b67704fec71f38623747b9bda544bb4519004c
jbeficent <jordi.ballester@eficent.com>
2016-10-03 16:33:24 +0200

    [ADD] migrate account_due_list to v9

--- account_due_list/models/__init__.py
+++ account_due_list/models/__init__.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
+# © 2008 Zikzakmedia S.L. (http://zikzakmedia.com)
+#        Jordi Esteve <jesteve@zikzakmedia.com>
+# © 2011 Domsense srl (<http://www.domsense.com>)
+# © 2011-2013 Agile Business Group sagl (<http://www.agilebg.com>)
+# © 2015 Andrea Cometa <info@andreacometa.it>
+# © 2015 Eneko Lacunza <elacunza@binovo.es>
+# © 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from . import account_move_line
