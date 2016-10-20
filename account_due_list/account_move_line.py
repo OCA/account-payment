2e389f009d60ea0822ce9ad4b43ed5cb32fd5462
Andrea Cometa <info@andreacometa.it>
2016-10-20 08:49:55 +0200

    [10][WIP] account_due_list porting

--- account_due_list/account_move_line.py
+++ account_due_list/account_move_line.py
@@ -1,28 +1,17 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2008 Zikzakmedia S.L. (http://zikzakmedia.com)
-#                       Jordi Esteve <jesteve@zikzakmedia.com>
+# OpenERP, Open Source Management Solution
+# Copyright (c) 2008 Zikzakmedia S.L. (http://zikzakmedia.com)
+#                    Jordi Esteve <jesteve@zikzakmedia.com>
 #
-#    Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
-#    Copyright (C) 2011-2013 Agile Business Group sagl
-#    (<http://www.agilebg.com>)
-#    Ported to Odoo by Andrea Cometa <info@andreacometa.it>
-#    Ported to v8 API by Eneko Lacunza <elacunza@binovo.es>
+# Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
+# Copyright (C) 2011-2013 Agile Business Group sagl
+# (<http://www.agilebg.com>)
+# Ported to Odoo by Andrea Cometa <info@andreacometa.it>
+# Ported to v8 API by Eneko Lacunza <elacunza@binovo.es>
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 #
 ##############################################################################
 
