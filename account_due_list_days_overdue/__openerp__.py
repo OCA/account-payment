e93058a310aca81bba96292af09221fe8305e1ab
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2016-10-26 08:50:22 -0600

    account_due_list_days_overdue - refactorization (#94)
    The module allows to list overdue periods in the account due list. The periods can be flexibly defined by the user.
    
    OCA Transbot updated translations from Transifex

---
+++ account_due_list_days_overdue/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+#   (<http://www.eficent.com>).
+# © 2016 Therp BV (<http://therp.nl>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Payments Due list days overdue",
+    'version': '9.0.0.1.0',
+    'category': 'Accounting',
+    'author': 'Odoo Community Association (OCA), '
+              'Eficent',
+    'website': 'https://github.com/OCA/account-payment',
+    'license': 'AGPL-3',
+    "depends": [
+        'account_due_list',
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/account_overdue_term_view.xml',
+        'views/account_move_line_view.xml'
+    ],
+    "demo": [
+        'demo/account_overdue_term_demo.xml'
+    ],
+    "installable": True,
+}
