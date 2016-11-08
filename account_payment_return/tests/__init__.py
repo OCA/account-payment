39358c1cf74b3b0c8c1b76b14ab32991db166a59
Carlos Dauden <carlos.dauden@tecnativa.com>
2016-11-08 09:51:55 +0100

    account_payment_return: Migrate 8.0 to 9.0

--- account_payment_return/tests/__init__.py
+++ account_payment_return/tests/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import test_payment_return
