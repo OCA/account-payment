9e7b93234f9eac5302bceb67414a9670bc95b919
OCA Transbot <transbot@odoo-community.org>
2016-11-25 19:48:12 -0500

    OCA Transbot updated translations from Transifex

--- account_due_list_days_overdue/__openerp__.py
+++ account_due_list_days_overdue/__openerp__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services S.L. (
-# <http://www.eficent.com>).
+# © 2016 Eficent Business and IT Consulting Services S.L.
+#   (<http://www.eficent.com>).
 # © 2016 Therp BV (<http://therp.nl>).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 {
     'name': "Payments Due list days overdue",
-    'version': '8.0.0.1.0',
+    'version': '9.0.0.1.0',
     'category': 'Accounting',
     'author': 'Odoo Community Association (OCA), '
-              'Eficent Business and IT Consulting Services S.L., ',
-    'website': 'http://www.eficent.com',
+              'Eficent',
+    'website': 'https://github.com/OCA/account-payment',
     'license': 'AGPL-3',
     "depends": [
         'account_due_list',
--- account_due_list_days_overdue/tests/test_account_due_list_days_overdue.py
+++ account_due_list_days_overdue/tests/test_account_due_list_days_overdue.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services S.L. (
-# <http://www.eficent.com>).
+# © 2016 Eficent Business and IT Consulting Services S.L.
+#   (<http://www.eficent.com>).
 # © 2016 Therp BV (<http://therp.nl>).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from openerp import fields
 import openerp.tests.common as common
 from datetime import date, timedelta
