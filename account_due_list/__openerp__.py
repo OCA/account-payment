62dec174ee5a59465e69074ba9a5e50bc8084a2b
jbeficent <jordi.ballester@eficent.com>
2016-12-28 18:18:40 +0100

    migration to v9
    * Field 'maturity_residual' is dropped, because 'amount_residual' is a stored field in v9
    * Rename fields as per names in v9
    * Add pre-migration scripts to speed-up the migration to v9

--- account_due_list/__openerp__.py
+++ account_due_list/__openerp__.py
@@ -1,36 +1,23 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
-#    Copyright (C) 2011-2013 Agile Business Group sagl
-#    (<http://www.agilebg.com>)
-#    @author Jordi Esteve <jesteve@zikzakmedia.com>
-#    @author Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-#    Ported to OpenERP 7.0 by Alex Comba <alex.comba@agilebg.com> and
-#    Bruno Bottacini <bruno.bottacini@dorella.com>
-#    Ported to Odoo by Andrea Cometa <info@andreacometa.it>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2008 Zikzakmedia S.L. (http://zikzakmedia.com)
+#        Jordi Esteve <jesteve@zikzakmedia.com>
+# © 2011 Domsense srl (<http://www.domsense.com>)
+# © 2011-2013 Agile Business Group sagl (<http://www.agilebg.com>)
+# © 2015 Andrea Cometa <info@andreacometa.it>
+# © 2015 Eneko Lacunza <elacunza@binovo.es>
+# © 2015 Tecnativa (http://www.tecnativa.com)
+# © 2016 Eficent Business and IT Consulting Services S.L.
+#        (http://www.eficent.com)
+# © 2016 David Dufresne <david.dufresne@savoirfairelinux.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 {
     'name': "Payments Due list",
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'Generic Modules/Payment',
     'author': 'Odoo Community Association (OCA), '
               'Agile Business Group, '
-              'Serv. Tecnol. Avanzados - Pedro M. Baeza,'
+              'Tecnativa,'
               'Zikzakmedia SL',
     'website': 'http://www.agilebg.com',
     'license': 'AGPL-3',
@@ -41,6 +28,5 @@
         'views/payment_view.xml',
     ],
     'pre_init_hook': 'pre_init_hook',
-    'post_init_hook': 'post_init_hook',
     "installable": True
 }
--- account_due_list/models/account_move_line.py
+++ account_due_list/models/account_move_line.py
@@ -1,29 +1,14 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (c) 2008 Zikzakmedia S.L. (http://zikzakmedia.com)
-#                       Jordi Esteve <jesteve@zikzakmedia.com>
-#    Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
-#    Copyright (C) 2011-2013 Agile Business Group sagl
-#    (<http://www.agilebg.com>)
-#    Ported to Odoo by Andrea Cometa <info@andreacometa.it>
-#    Ported to v8 API by Eneko Lacunza <elacunza@binovo.es>
-#    Copyright (c) 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2008 Zikzakmedia S.L. (http://zikzakmedia.com)
+#        Jordi Esteve <jesteve@zikzakmedia.com>
+# © 2011 Domsense srl (<http://www.domsense.com>)
+# © 2011-2013 Agile Business Group sagl (<http://www.agilebg.com>)
+# © 2015 Andrea Cometa <info@andreacometa.it>
+# © 2015 Eneko Lacunza <elacunza@binovo.es>
+# © 2015 Tecnativa (http://www.tecnativa.com)
+# © 2016 Eficent Business and IT Consulting Services S.L.
+#        (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api
 
@@ -31,12 +16,13 @@ from openerp import models, fields, api
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    invoice_origin = fields.Char(related='invoice.origin', string='Source Doc')
-    invoice_date = fields.Date(related='invoice.date_invoice',
+    invoice_origin = fields.Char(related='invoice_id.origin',
+                                 string='Source Doc')
+    invoice_date = fields.Date(related='invoice_id.date_invoice',
                                string='Invoice Date')
     partner_ref = fields.Char(related='partner_id.ref', string='Partner Ref')
     payment_term_id = fields.Many2one('account.payment.term',
-                                      related='invoice.payment_term',
+                                      related='invoice_id.payment_term_id',
                                       string='Payment Terms')
     stored_invoice_id = fields.Many2one(
         comodel_name='account.invoice', compute='_compute_invoice',
@@ -44,26 +30,9 @@ class AccountMoveLine(models.Model):
     invoice_user_id = fields.Many2one(
         comodel_name='res.users', related='stored_invoice_id.user_id',
         string="Invoice salesperson", store=True)
-    maturity_residual = fields.Float(
-        compute='_maturity_residual', string="Residual Amount", store=True,
-        help="The residual amount on a receivable or payable of a journal "
-             "entry expressed in the company currency.")
 
     @api.multi
-    @api.depends('date_maturity', 'debit', 'credit', 'reconcile_id',
-                 'reconcile_partial_id', 'account_id.reconcile',
-                 'amount_currency', 'reconcile_partial_id.line_partial_ids',
-                 'currency_id')
-    def _maturity_residual(self):
-        """
-        Inspired by amount_residual
-        """
-        for move_line in self:
-            sign = (move_line.debit - move_line.credit) < 0 and -1 or 1
-            move_line.maturity_residual = move_line.amount_residual * sign
-
-    @api.multi
-    @api.depends('move_id', 'invoice.move_id')
+    @api.depends('move_id', 'invoice_id.move_id')
     def _compute_invoice(self):
         for line in self:
             invoices = self.env['account.invoice'].search(
