dd72f7e1ce1233ecc892bc83de03f757c0e45b3e
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2017-01-23 17:08:55 -0700

    account_check_printing_report_base, account_check_printing_report_dlt103
    - account_check_printing_report_base
    - account_check_printing_report_dlt103

---
+++ account_check_printing_report_base/models/account_payment.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# (http://www.eficent.com)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api
+
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    @api.multi
+    def do_print_checks(self):
+        for rec in self:
+            if rec.company_id.check_layout_id:
+                return self.env['report'].get_action(
+                    rec, rec.company_id.check_layout_id.report)
+            return super(AccountPayment, self).do_print_checks()
