ddeb367885cacfbd7d114a65cc7bb9748f41c6b5
lreficent <lois.rilo@eficent.com>
2017-04-12 13:20:16 +0200

    account_payment_show_invoice

---
+++ account_payment_show_invoice/models/account_payment.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+
+
+class ModelName(models.Model):
+    _inherit = "account.payment"
+
+    @api.one
+    def _compute_invoice_vendor_references(self):
+        references = ''
+        for invoice in self.invoice_ids:
+            if references:
+                references += ', '
+            if invoice.reference:
+                references += invoice.reference
+            else:
+                references += invoice.number
+        self.invoice_vendor_references = references
+
+    invoice_vendor_references = fields.Char(
+        string='Invoices',
+        compute=_compute_invoice_vendor_references)
