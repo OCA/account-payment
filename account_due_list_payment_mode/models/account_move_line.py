69ee6f5edf1b25502ac2477a73baa9659bb91cb4
Roodin <santi@comunitea.com>
2017-05-29 19:47:31 +0200

    [MIG] account_due_list_payment_mode: Migration to 10.0

--- account_due_list_payment_mode/models/account_move_line.py
+++ account_due_list_payment_mode/models/account_move_line.py
@@ -3,7 +3,7 @@
 # Copyright 2015 cubells <vicent@vcubells.net>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class AccountMoveLine(models.Model):
