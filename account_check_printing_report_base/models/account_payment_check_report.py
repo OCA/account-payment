0643d633f449bf5c511cf6a7ea212ecbee2cb6bb
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2017-05-31 17:11:47 +0530

    [MIG]ADD account_check_printing_report_base module for v 10.0

---
+++ account_check_printing_report_base/models/account_payment_check_report.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# (http://www.eficent.com)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class AccountPaymentCheckReport(models.Model):
+    _name = "account.payment.check.report"
+
+    name = fields.Char(string='Name', required=True)
+    report = fields.Char(string='Report name', required=True)
