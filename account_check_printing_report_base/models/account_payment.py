29e8a9618799582c4847e8f6b2eb6d9cb56d7180
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-06-07 12:41:42 -0700

    [MIG] account_check_printing_report_base

--- account_check_printing_report_base/models/account_payment.py
+++ account_check_printing_report_base/models/account_payment.py
@@ -4,7 +4,7 @@
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class AccountPayment(models.Model):
