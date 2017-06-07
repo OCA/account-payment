76b42b4346c3715cecd99deb817abf6d55b6a22e
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-06-07 10:46:31 -0700

    [IMP] Bump the version

--- account_check_printing_report_base/__manifest__.py
+++ account_check_printing_report_base/__manifest__.py
@@ -5,24 +5,24 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Check Printing Report Base',
-    "version": "9.0.1.0.0",
+    "name": "Account Check Printing Report Base",
+    "version": "10.0.1.0.0",
     "license": "AGPL-3",
-    'author': "Eficent,"
+    "author": "Eficent,"
               "Serpent Consulting Services Pvt. Ltd.,"
-              "Odoo Community Association (OCA),"
-              "Ursa Information Systems",
-    'category': 'Generic Modules/Accounting',
-    'website': "https://github.com/OCA/account-payment",
-    'depends': ['account_check_printing'],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/report_paperformat.xml',
-        'data/account_payment_check_report_data.xml',
-        'views/report_check_base.xml',
-        'views/res_company_view.xml',
-        'views/account_payment_check_report_view.xml',
-        'report/account_check_writing_report.xml',
+              "Ursa Information Systems,"
+              "Odoo Community Association (OCA)",
+    "category": "Generic Modules/Accounting",
+    "website": "https://github.com/OCA/account-payment",
+    "depends": ["account_check_printing"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/report_paperformat.xml",
+        "data/account_payment_check_report_data.xml",
+        "views/report_check_base.xml",
+        "views/res_company_view.xml",
+        "views/account_payment_check_report_view.xml",
+        "report/account_check_writing_report.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
