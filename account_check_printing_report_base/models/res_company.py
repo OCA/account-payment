b2b27710cffd02ae052d66bc48495f4f57b33589
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-06-07 12:41:42 -0700

    [MIG] account_check_printing_report_base

--- account_check_printing_report_base/models/res_company.py
+++ account_check_printing_report_base/models/res_company.py
@@ -4,11 +4,10 @@
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ResCompany(models.Model):
-
     _inherit = "res.company"
 
     check_layout_id = fields.Many2one(
