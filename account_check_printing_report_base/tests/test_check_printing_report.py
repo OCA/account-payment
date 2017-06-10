25a24f69fb4d9abcdbd8bd65db92242654319daa
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-06-10 10:26:23 -0500

    [FIX] Bad Report Reference. This report is not loaded into the database.

--- account_check_printing_report_base/tests/test_check_printing_report.py
+++ account_check_printing_report_base/tests/test_check_printing_report.py
@@ -2,8 +2,8 @@
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests.common import TransactionCase
-from openerp.exceptions import UserError
+from odoo.tests.common import TransactionCase
+from odoo.exceptions import UserError
 import time
 
 
