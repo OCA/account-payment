1d826492fade6173ccad7f652148b68bbfc29f82
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-06-10 10:26:23 -0500

    [FIX] Bad Report Reference. This report is not loaded into the database.

--- account_check_printing_report_base/__init__.py
+++ account_check_printing_report_base/__init__.py
@@ -4,5 +4,5 @@
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from . import report
 from . import models
+from . import report
