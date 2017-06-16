d12ba23522772b8ba62c5578884cf6a60ef70059
mreficent <miquel.raich@eficent.com>
2017-06-16 18:45:14 +0200

    [MIG] account_payment_show_invoice: Migration to 10.0

--- account_payment_show_invoice/models/account_payment.py
+++ account_payment_show_invoice/models/account_payment.py
@@ -2,10 +2,10 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
-class ModelName(models.Model):
+class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     @api.one
