22dbacd16c66e7a880f6204bb10d5a6b95209317
mreficent <miquel.raich@eficent.com>
2017-06-16 16:54:07 +0200

    [MIG] account_due_list_days_overdue: Migration to 10.0

--- account_due_list_days_overdue/tests/test_account_due_list_days_overdue.py
+++ account_due_list_days_overdue/tests/test_account_due_list_days_overdue.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services S.L. (
-# <http://www.eficent.com>).
-# © 2016 Therp BV (<http://therp.nl>).
-# © 2016 Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>).
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+#   (<http://www.eficent.com>).
+# Copyright 2016 Therp BV (<http://therp.nl>).
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from openerp import fields, workflow, exceptions
-import openerp.tests.common as common
+from odoo import exceptions, fields, workflow
+from odoo.tests.common import TransactionCase
 from datetime import date, timedelta
 
 
-class TestAccountDueListDaysOverdue(common.TransactionCase):
+class TestAccountDueListDaysOverdue(TransactionCase):
 
     def setUp(self):
         super(TestAccountDueListDaysOverdue, self).setUp()
