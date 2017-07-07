fe225df84271d500bf0c37cdd16308b0796e8120
David Vidal <chienandalu@gmail.com>
2017-07-07 15:35:57 +0200

    [MIG] account_payment_return_import: Migration to 10.0

--- account_payment_return_import/models/payment_return.py
+++ account_payment_return_import/models/payment_return.py
@@ -3,7 +3,7 @@
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class PaymentReturnLine(models.Model):
