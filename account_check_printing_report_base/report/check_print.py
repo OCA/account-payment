72a6cf5c633c25e60817606659441628723f8d67
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-08-26 19:01:30 -0500

    [FIX] Bad Report Reference. This report is not loaded into the database

--- account_check_printing_report_base/report/check_print.py
+++ account_check_printing_report_base/report/check_print.py
@@ -4,8 +4,8 @@
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, exceptions, models, _
 import time
+from odoo import api, exceptions, models, _
 from odoo.tools import float_is_zero
 
 
