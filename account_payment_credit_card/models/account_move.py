84823be245c037773fa73545f8847457734ad112
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2017-09-20 23:01:17 +0530

    [FIX] Singleton id issue

--- account_payment_credit_card/models/account_move.py
+++ account_payment_credit_card/models/account_move.py
@@ -11,40 +11,36 @@ class AccountMove(models.Model):
 
     @api.multi
     def post(self):
-
-        # Call super method
-        res = super(AccountMove, self).post()
-
-        # Set variables
+        # set variables
         move_line_pool = self.env['account.move.line']
-        result = []
 
-        # Check whether journal has Transfer AP to Credit Card Company checked
-        # or not
-        if self.journal_id and self.journal_id.credit_card:
+        for move in self:
 
-            # Browse move lines
-            for move_line in self.line_ids:
+            result = []
+            # check weather jouranl has Transfer AP to Credit Card Company = checked or not
+            if move.journal_id and move.journal_id.support_creditcard_transactions:
 
-                # Prepare move line values
-                result.append({
-                    'name': move_line.name,
-                    'ref': move_line.ref,
-                    'partner_id': self.journal_id.partner_id and self.journal_id.partner_id.id or move_line.partner_id and move_line.partner_id.id or False,  # noqa
-                    'journal_id': move_line.journal_id and move_line.journal_id.id,  # noqa
-                    'account_id': move_line.account_id and move_line.account_id.id,  # noqa
-                    'debit': move_line.credit,
-                    'credit': move_line.debit,
-                    'date_maturity': move_line.date_maturity,
-                    'move_id': move_line.move_id and move_line.move_id.id,
-                    'date': move_line.date
-                })
+                # browse move lines
+                for move_line in move.line_ids:
+                    # prepare move line values
+                    result.append({
+                        'name': move_line.name,
+                        'ref': move_line.ref,
+                        'partner_id': move.journal_id.partner_id and move.journal_id.partner_id.id or move_line.partner_id and move_line.partner_id.id or False,
+                        'journal_id': move_line.journal_id and move_line.journal_id.id,
+                        'account_id': move_line.account_id and move_line.account_id.id,
+                        'debit': move_line.credit,
+                        'credit': move_line.debit,
+                        'date_maturity': move_line.date_maturity,
+                        'move_id': move_line.move_id and move_line.move_id.id,
+                        'date': move_line.date
+                    })
 
-        # Check result list
-        if result:
+                # check result list
+                if result:
 
-            # Create new move lines
-            for vals in result:
-                move_line_pool.create(vals)
+                    # create new move lines
+                    for vals in result:
+                        move_line_pool.create(vals)
 
-        return res
+        return super(AccountMove, self).post()
