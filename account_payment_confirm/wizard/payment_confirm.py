f6e1f83321d35f6b671b6b37c484c9b29c53227c
Achref Mhadhbi <mhadhbi.achref@gmail.com>
2017-10-30 15:28:56 +0100

    update file account payment

--- account_payment_confirm/wizard/payment_confirm.py
+++ account_payment_confirm/wizard/payment_confirm.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Mhadhbi Achraf(https://github.com/AMhadhbi).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from odoo import models, api, _
 from odoo.exceptions import UserError
 
 
-class AccountPaymentConfirm(models.TransientModel):
+class AccountPaymentConfirm( models.TransientModel ):
     """
     This wizard will confirm the all the selected draft payments
     """
@@ -15,12 +14,12 @@ class AccountPaymentConfirm(models.TransientModel):
     _description = "Confirm the selected payments"
 
     @api.multi
-    def payment_confirm(self):
+    def payment_confirm( self ):
         context = dict(self._context or {})
         active_ids = context.get('active_ids', []) or []
 
-        for record in self.env['account.payment'].browse(active_ids):
+        for record in self.env['account.payment'].browse( active_ids ):
             if record.state != 'draft':
-                raise UserError(_("Selected payment(s) cannot be confirmed as they are not in 'Draft' state."))
+                raise UserError( _( "Selected payment(s) cannot be confirmed as they are not in 'Draft' state." ) )
             record.post()
         return {'type': 'ir.actions.act_window_close'}
