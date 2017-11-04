f9dda5af8975bfff9ab7879f24ded27cc52e193d
Eduardo Magdalena <emagdalena@c2i.es>
2017-11-04 16:06:40 +0100

    [11.0] [FIX] Category in account_cash_invoice (#1)
    This is a minor fix in the manifest.py file of account_cash_invoice:
    
    Change category from "Account" to "Accounting" in order to follow the categories set in Odoo 11
    Change the website to the repository web https://github.com/OCA/account-payment
    Remove the lines # -- coding: utf-8 --

--- account_cash_invoice/__manifest__.py
+++ account_cash_invoice/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017 Creu Blanca
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'Account cash invoice',
-    'version': '11.0.1.0.0',
-    'category': 'Account',
+    'version': '11.0.1.1.0',
+    'category': 'Accounting',
     'author': "Creu Blanca,"
               "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/pos',
+    'website': 'https://github.com/OCA/account-payment',
     'summary': 'Pay and receive invoices from bank statements',
     "license": "LGPL-3",
     'depends': [
