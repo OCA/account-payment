5a5adb72b3ad6b9e1691b489d94d97980590e704
Florent Cayré <florent.cayre@logilab.fr>
2018-01-15 16:22:58 +0100

    initial revision

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Slimpay Payment Acquirer',
+    'category': 'Accounting',
+    'summary': 'Payment Acquirer: Slimpay Implementation',
+    'version': '10.0.1.0.0',
+    'description': """Slimpay Payment Acquirer""",
+    'author': "Commown SCIC SAS",
+    'license': "AGPL-3",
+    'website': "https://www.commown.fr",
+    'depends': ['payment'],
+    'data': [
+    ],
+    'installable': True,
+}
