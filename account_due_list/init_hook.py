a28ca23fb3da696366b46b631dc41f07622cadca
Francisco Ivan Anton Prieto <ozono@ozonomultimedia.com>
2018-01-30 18:38:27 +0100

    [MIG] account_due_list: Migration to 11.0

--- account_due_list/init_hook.py
+++ account_due_list/init_hook.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
 # © 2016 David Dufresne <david.dufresne@savoirfairelinux.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
-
-
 logger = logging.getLogger(__name__)
 
 
