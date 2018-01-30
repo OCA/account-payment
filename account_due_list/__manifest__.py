8213c1e3063b999edf85951b47b15c94c040928d
Francisco Ivan Anton Prieto <ozono@ozonomultimedia.com>
2018-01-30 14:28:38 +0100

    Some manifest file fixes

--- account_due_list/__manifest__.py
+++ account_due_list/__manifest__.py
@@ -1,18 +1,19 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Payments Due list',
-    'version': '11.0.1.0.0',
-    'category': 'Generic Modules/Payment',
-    'summary': 'Check printing commons',
-    'description': """
-    """,
-    'website': '',
-    'depends': ['account'],
-    'data': [
+    "name": "Payment due list with payment mode",
+    "version": "11.0.1.0.0",
+    "category": "Generic Modules/Payment",
+    'author': 'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/account-payment',
+    'license': 'LGPL-3',
+    "depends": [
+        "account_payment_partner",
+        "account_due_list",
+    ],
+    "data": [
         'views/payment_view.xml',
     ],
-    'pre_init_hook': 'pre_init_hook',
     'installable': True,
-    'auto_install': False,
+    "auto_install": False,
 }
