139c0b9bb41525e40a4b1de75436398f6a4d6630
Benjamin Willig <benjamin.willig@acsone.eu>
2018-03-06 09:23:48 +0100

    [MIG] Migration to 10.0 + some improvements
    [ADD] Allow to configure discount options directly on the payment term
    
    [ADD] Add account cash discount payment module
    
    [MIG] account_cash_discount_payment migration. Also removed force_discount_amount from the account.invoice
    
    [ADD] Allow to tell if a payment line is paid using the discount or not. Keep the original amount in order to compute it again

---
+++ account_cash_discount_payment/models/account_payment_line.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models, fields
+
+
+class PaymentLine(models.Model):
+
+    _inherit = 'account.payment.line'
+
+    pay_with_discount = fields.Boolean(
+        default=False,
+    )
+    discount_due_date = fields.Date(
+        compute='_compute_discount_due_date',
+    )
+    discount_amount = fields.Monetary(
+        default=0.0
+    )
+    original_amount_currency = fields.Monetary(
+        readonly=True,
+    )
+
+    @api.multi
+    @api.depends(
+        'move_line_id.invoice_id'
+    )
+    def _compute_discount_due_date(self):
+        for rec in self:
+            if rec.move_line_id and rec.move_line_id.invoice_id:
+                invoice = rec.move_line_id.invoice_id
+                rec.discount_due_date = invoice.discount_due_date
+
+    @api.onchange(
+        'discount_amount',
+        'original_amount_currency',
+        'pay_with_discount',
+    )
+    def _onchange_pay_with_discount(self):
+        if self.pay_with_discount:
+            self.amount_currency = (
+                self.original_amount_currency - self.discount_amount)
+        else:
+            self.amount_currency = self.original_amount_currency
