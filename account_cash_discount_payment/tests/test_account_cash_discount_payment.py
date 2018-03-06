ac0e739a2b6fa9217b8c7d2d6af5b1e98ab5285b
Benjamin Willig <benjamin.willig@acsone.eu>
2018-03-06 09:23:48 +0100

    [MIG] Migration to 10.0 + some improvements
    [ADD] Allow to configure discount options directly on the payment term
    
    [ADD] Add account cash discount payment module
    
    [MIG] account_cash_discount_payment migration. Also removed force_discount_amount from the account.invoice
    
    [ADD] Allow to tell if a payment line is paid using the discount or not. Keep the original amount in order to compute it again

---
+++ account_cash_discount_payment/tests/test_account_cash_discount_payment.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.fields import Date
+from .common import TestAccountCashDiscountPaymentCommon
+
+
+class TestAccountCashDiscountPayment(TestAccountCashDiscountPaymentCommon):
+
+    def test_invoice_payment_discount(self):
+        invoice_date = Date.today()
+        invoice = self.create_supplier_invoice(
+            invoice_date, self.payment_mode_out, 2000, 25, [])
+        invoice.action_invoice_open()
+
+        move = invoice.move_id
+        move.post()
+
+        payment_order = self.PaymentOrder.create({
+            'payment_mode_id': self.payment_mode_out.id,
+            'payment_type': 'outbound',
+        })
+
+        payment_line_wizard = self.PaymentLineCreate.with_context(
+            active_model=payment_order._name,
+            active_id=payment_order.id,
+        ).create({
+            'cash_discount_date_start': invoice_date,
+            'cash_discount_date_end': invoice_date,
+            'date_type': 'discount_due_date',
+            'journal_ids': [(6, 0, [self.purchase_journal.id])],
+        })
+        self.assertEqual(payment_line_wizard.order_id, payment_order)
+
+        payment_line_wizard.populate()
+        move_lines = payment_line_wizard.move_line_ids
+        self.assertEqual(len(move_lines), 1)
+
+        move_line = move_lines[0]
+        self.assertAlmostEqual(move_line.discount_amount, 500, 2)
+
+        payment_line_wizard.create_payment_lines()
+
+        self.assertEqual(len(payment_order.payment_line_ids), 1)
+        move_line = payment_order.payment_line_ids[0]
+        self.assertTrue(move_line.pay_with_discount)
+        self.assertAlmostEqual(move_line.discount_amount, 500, 2)
+        self.assertAlmostEqual(move_line.amount_currency, 1500, 2)
