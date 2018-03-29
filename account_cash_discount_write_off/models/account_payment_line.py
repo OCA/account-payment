709b0194a0489c87b185b9ae4c12c973958bcacc
Benjamin Willig <benjamin.willig@acsone.eu>
2018-03-29 10:21:40 +0200

    [CHG] Create the tax adjustment based on move lines instead of invoice tax lines
    [CHG] changed the way to populate tax's move lines debit/credit fields
    
    [FIX] the discount due date should be recomputed when the invoice data changes
    
    [FIX] the amount of the payment line should not include the discount if the current date is posterior to the discount due date
    
    [IMP] Display a button on the payment line tree view to toggle the pay with discount flag
    
    [ADD] Show the discount amount/due date in the payment line generation wizard
    
    [FIX] Keep the base line with taxes
    
    [FIX] do not allow to change the pay_with_discount field value if the payment order is done
    
    [CHG] display a filled/emtpy checkbox depending if the discount has been enabled or not
    
    [FIX] use the writeoff account for a non tax line
    
    [ADD] display the discount due date/amount in the move line search more
    
    [FIX] the domain should be recomputed when the discount due date changes
    
    [FIX] new onchange method to disable the discount if the amount of a payment line has been changed manually. The writeoff shouldn't be created if the amount of the line doesn't correspond to the invoice total with discount
    
    [FIX] the pay with discount field should not be visible if the invoice has already been paid partially
    
    [FIX] discount data should only be visible in a supplier invoice

--- account_cash_discount_write_off/models/account_payment_line.py
+++ account_cash_discount_write_off/models/account_payment_line.py
@@ -4,6 +4,7 @@
 
 from odoo import api, models, _
 from odoo.exceptions import UserError
+from odoo.tools import float_compare
 
 
 class PaymentLine(models.Model):
@@ -13,7 +14,16 @@ class PaymentLine(models.Model):
     @api.multi
     def _check_cash_discount_write_off_creation(self):
         self.ensure_one()
-        return self.pay_with_discount
+        return (
+            self.pay_with_discount and
+            self.move_line_id and
+            self.move_line_id.invoice_id and
+            self.move_line_id.invoice_id.has_discount and
+            float_compare(
+                self.amount_currency,
+                self.move_line_id.invoice_id.amount_total_with_discount,
+                precision_rounding=self.currency_id.rounding) == 0
+        )
 
     @api.multi
     def get_cash_discount_writeoff_move_values(self):
@@ -42,7 +52,8 @@ class PaymentLine(models.Model):
         supplier_account_amount = invoice.discount_amount
         discount_amount_credit = supplier_account_amount
 
-        lines_values = list()
+        lines_values = []
+
         lines_values.append({
             'partner_id': partner.id,
             'name': move_line_name,
@@ -51,23 +62,40 @@ class PaymentLine(models.Model):
         })
 
         if tax_adjustment:
-            for tax_line in invoice.tax_line_ids:
-                amount = tax_line.amount * invoice.discount_percent / 100.0
+            tax_move_lines = self.env['account.move.line'].search([
+                ('move_id', '=', move_line.move_id.id),
+                '|',
+                ('tax_line_id', '!=', False),
+                ('tax_ids', '!=', False)
+            ])
+
+            for tax_move_line in tax_move_lines:
+                amount = (
+                    abs(tax_move_line.balance) *
+                    invoice.discount_percent / 100.0
+                )
                 discount_amount_credit -= amount
+                if tax_move_line.tax_line_id:
+                    account = tax_move_line.account_id
+                else:
+                    account = woff_account
                 lines_values.append({
                     'partner_id': partner.id,
                     'name': move_line_name,
-                    'credit': amount,
-                    'account_id': tax_line.account_id.id,
-                    'tax_line_id': tax_line.tax_id.id,
+                    'debit': tax_move_line.credit > 0 and amount or 0.0,
+                    'credit': tax_move_line.debit > 0 and amount or 0.0,
+                    'account_id': account.id,
+                    'tax_line_id': tax_move_line.tax_line_id.id,
+                    'tax_ids': [(6, 0, tax_move_line.tax_ids.ids)]
                 })
 
-        lines_values.append({
-            'partner_id': partner.id,
-            'name': move_line_name,
-            'credit': discount_amount_credit,
-            'account_id': woff_account.id,
-        })
+        if discount_amount_credit > 0:
+            lines_values.append({
+                'partner_id': partner.id,
+                'name': move_line_name,
+                'credit': discount_amount_credit,
+                'account_id': woff_account.id,
+            })
 
         move_values = {
             'journal_id': woff_journal.id,
