aadf7593c76421d620fd0365237407fd4c9dcf79
Benjamin Willig <benjamin.willig@acsone.eu>
2018-03-29 10:08:17 +0200

    [CHG] Don't allow to pay with discount if the payment line is not linked to an invoice which has a discount
    [CHG] Use a computed flag to know if there is tax adjustments or not
    
    [CHG] When the pay_with_discount flag is changed, the amount to paid is computed based on the invoice total and the invoice total with the discount amount deducted
    
    [FIX] Invoice report should display the amount_total_with_discount instead of the discount_amount
    
    [CHG] Use a single date as a search filter for the discount due date
    
    [FIX] The generated discount write off move should be posted depending on the payment mode configuration
    
    [CHG] only the pay_with_discount flag should decide if the write off should be created or not
    
    [CHG] The accounting partner is already the one defined on the move line

--- account_cash_discount_base/models/res_company.py
+++ account_cash_discount_base/models/res_company.py
@@ -14,6 +14,10 @@ class ResCompany(models.Model):
         required=True,
         default='untaxed',
     )
+    cash_discount_use_tax_adjustment = fields.Boolean(
+        compute='_compute_cash_discount_tax_adjustment',
+        store=True,
+    )
 
     @api.model
     def _get_cash_discount_base_amount_types(self):
@@ -21,3 +25,13 @@ class ResCompany(models.Model):
             ('untaxed', _("Excluding tax")),
             ('total', _("Including all taxes")),
         ]
+
+    @api.multi
+    @api.depends(
+        'cash_discount_base_amount_type',
+    )
+    def _compute_cash_discount_tax_adjustment(self):
+        for rec in self:
+            rec.cash_discount_use_tax_adjustment = (
+                rec.cash_discount_base_amount_type == 'total'
+            )
