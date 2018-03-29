f1307e16e2820673a6d23fc05fd294bfccea3072
Benjamin Willig <benjamin.willig@acsone.eu>
2018-03-29 10:08:17 +0200

    [CHG] Don't allow to pay with discount if the payment line is not linked to an invoice which has a discount
    [CHG] Use a computed flag to know if there is tax adjustments or not
    
    [CHG] When the pay_with_discount flag is changed, the amount to paid is computed based on the invoice total and the invoice total with the discount amount deducted
    
    [FIX] Invoice report should display the amount_total_with_discount instead of the discount_amount
    
    [CHG] Use a single date as a search filter for the discount due date
    
    [FIX] The generated discount write off move should be posted depending on the payment mode configuration
    
    [CHG] only the pay_with_discount flag should decide if the write off should be created or not
    
    [CHG] The accounting partner is already the one defined on the move line

--- account_cash_discount_payment/models/account_payment_line.py
+++ account_cash_discount_payment/models/account_payment_line.py
@@ -2,7 +2,8 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, fields
+from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError
 
 
 class PaymentLine(models.Model):
@@ -13,33 +14,47 @@ class PaymentLine(models.Model):
         default=False,
     )
     discount_due_date = fields.Date(
-        compute='_compute_discount_due_date',
+        related='move_line_id.invoice_id.discount_due_date',
+        readonly=True,
     )
     discount_amount = fields.Monetary(
-        default=0.0
-    )
-    original_amount_currency = fields.Monetary(
+        related='move_line_id.invoice_id.discount_amount',
         readonly=True,
     )
 
     @api.multi
-    @api.depends(
-        'move_line_id.invoice_id'
+    @api.constrains(
+        'pay_with_discount',
+        'move_line_id',
     )
-    def _compute_discount_due_date(self):
+    def _check_pay_with_discount(self):
         for rec in self:
-            if rec.move_line_id and rec.move_line_id.invoice_id:
-                invoice = rec.move_line_id.invoice_id
-                rec.discount_due_date = invoice.discount_due_date
+            if not rec.pay_with_discount:
+                continue
+            move_line = rec.move_line_id
+            invoice = move_line and move_line.invoice_id or False
+            if not invoice or not invoice.has_discount:
+                raise ValidationError(
+                    _("You can't pay with a discount if the payment line is "
+                      "not linked to an invoice which has a discount."))
 
     @api.onchange(
         'discount_amount',
-        'original_amount_currency',
+        'move_line_id',
         'pay_with_discount',
     )
     def _onchange_pay_with_discount(self):
+        """
+        This onchange should be executed completely only when the payment line
+        is linked to a move line which is linked to an invoice which has a
+        discount.
+
+        If the above condition is ok, the amount will change based on the
+        invoice total and invoice discount amount.
+        """
+        self._check_pay_with_discount()
+        invoice = self.move_line_id.invoice_id
         if self.pay_with_discount:
-            self.amount_currency = (
-                self.original_amount_currency - self.discount_amount)
+            self.amount_currency = invoice.amount_total_with_discount
         else:
-            self.amount_currency = self.original_amount_currency
+            self.amount_currency = invoice.amount_total
