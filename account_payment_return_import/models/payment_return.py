6deb59683167b777c01b112f7c2ec6c6fd9a6052
Luis M. Ontalba <luis.martinez@tecnativa.com>
2018-04-05 10:26:23 +0200

    [11.0][MIG] account_payment_return_import

--- account_payment_return_import/models/payment_return.py
+++ account_payment_return_import/models/payment_return.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
