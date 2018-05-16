5d9b36bd059817f7ae332b1aef61d70876d6a563
Roberto Fichera <roberto.fichera@levelprime.com>
2018-05-16 10:50:20 +0200

    [IMP] account_due_list: Add both pivot and graph views (#185)

--- account_due_list/__manifest__.py
+++ account_due_list/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Payments Due list',
-    'version': '11.0.1.0.0',
+    'version': '11.0.2.0.0',
     'category': 'Generic Modules/Payment',
     'author': 'Odoo Community Association (OCA)',
     'summary': 'Check printing commons',
