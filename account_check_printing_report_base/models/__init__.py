3e35b9b399471e6bf872d6b50ad8898d18be6600
Luis M. Ontalba <luismaront@gmail.com>
2018-05-18 19:02:39 +0200

    [IMP] account_check_printing_report_base: auto check printing (#171)

--- account_check_printing_report_base/models/__init__.py
+++ account_check_printing_report_base/models/__init__.py
@@ -3,6 +3,7 @@
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from . import account_journal
 from . import account_payment
 from . import res_company
 from . import account_payment_check_report
