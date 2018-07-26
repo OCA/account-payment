cbaf9669b4977b9cd5bcd9ec593846f2e40d8624
Sandip Mangukiya <smangukiya@ursainfosystems.com>
2018-07-26 04:12:31 -0700

    [IMP]Partner Aging, improve copy right lines

--- partner_aging/__manifest__.py
+++ partner_aging/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Ursa Information Systems <http://www.ursainfosystems.com>
+# Copyright (C) 2012 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Interactive Partner Aging at any date',
