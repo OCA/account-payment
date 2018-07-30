3f47d59e5c2eeefce5fa2191835757c9d675934d
Sudarshan1607 <skadalazhi@ursainfosystems.com>
2018-07-30 14:41:43 -0700

    another formatting change

--- partner_aging/tests/test_partner_aging.py
+++ partner_aging/tests/test_partner_aging.py
@@ -20,7 +20,7 @@ class TestPartnerAging(TransactionCase):
         res = partner_aging_date.open_customer_aging()
         self.assertEqual(res['context']['age_date'], current_date)
 
-        # call open_supplier_aging here and update current_date for good measure
+        # call open_supplier_aging and update current_date for good measure
         current_date = fields.Datetime.now()
         partner_aging_date.age_date = current_date
         res = partner_aging_date.open_supplier_aging()
