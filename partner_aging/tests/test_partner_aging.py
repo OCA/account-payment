8e980f453475434971ae58afe04d4ad2101e05fc
Sudarshan1607 <skadalazhi@ursainfosystems.com>
2018-07-30 14:35:48 -0700

    Further formatting

--- partner_aging/tests/test_partner_aging.py
+++ partner_aging/tests/test_partner_aging.py
@@ -5,9 +5,9 @@
 from openerp.tests.common import TransactionCase
 from openerp import fields
 
+
 class TestPartnerAging(TransactionCase):
 
-    
     def test_partner_aging_date(self):
         current_date = fields.Datetime.now()
         partner_aging_date = self.env['res.partner.aging.date'].create(
@@ -16,12 +16,12 @@ class TestPartnerAging(TransactionCase):
             }
         )
         partner_aging_date.age_date = current_date
-        #call function open_customer_aging here
+        # call function open_customer_aging here
         res = partner_aging_date.open_customer_aging()
         self.assertEqual(res['context']['age_date'], current_date)
 
-        #call open_supplier_aging here and update current_date for good measure
+        # call open_supplier_aging here and update current_date for good measure
         current_date = fields.Datetime.now()
         partner_aging_date.age_date = current_date
         res = partner_aging_date.open_supplier_aging()
-        self.assertEqual(res['context']['age_date'],current_date)
+        self.assertEqual(res['context']['age_date'], current_date)
