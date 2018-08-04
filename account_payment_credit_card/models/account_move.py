5a11ff3c05a4fae47cf7d114963bb3d7125b8b9b
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-08-04 11:52:16 +0530

    Improve code

--- account_payment_credit_card/models/account_move.py
+++ account_payment_credit_card/models/account_move.py
@@ -10,45 +10,37 @@ class AccountMove(models.Model):
 
     @api.multi
     def post(self):
-        # set variables
-        move_line_pool = self.env['account.move.line']
-
         for move in self:
 
             result = []
-            # check whether journal has Transfer AP to Credit Card
+            # Check whether journal has Transfer AP to Credit Card
             # Company = checked or not
             if move.journal_id and\
                     move.journal_id.credit_card:
 
-                # browse move lines
+                # Browse move lines
                 for move_line in move.line_ids:
                     # prepare move line values
                     result.append({
                         'name': move_line.name,
                         'ref': move_line.ref,
                         'partner_id':
-                            move.journal_id.partner_id and
                             move.journal_id.partner_id.id or
-                            move_line.partner_id and
                             move_line.partner_id.id or False,
-                        'journal_id':
-                            move_line.journal_id and move_line.journal_id.id,
-                        'account_id':
-                            move_line.account_id and move_line.account_id.id,
+                        'journal_id': move_line.journal_id.id,
+                        'account_id': move_line.account_id.id,
                         'debit': move_line.credit,
                         'credit': move_line.debit,
                         'date_maturity': move_line.date_maturity,
-                        'move_id':
-                            move_line.move_id and move_line.move_id.id,
+                        'move_id': move_line.move_id.id,
                         'date': move_line.date
                     })
 
-                # check result list
+                # Check result list
                 if result:
 
-                    # create new move lines
+                    # Create new move lines
                     for vals in result:
-                        move_line_pool.create(vals)
+                        self.env['account.move.line'].create(vals)
 
         return super(AccountMove, self).post()
