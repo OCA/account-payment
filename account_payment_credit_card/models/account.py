4791316c8efbf3e7519cb33a86c160de357b5434
mgosai <31255575+mgosai@users.noreply.github.com>
2018-08-17 14:54:24 -0400

    account_payment_credit_card: Fix contibutors (#196)

--- account_payment_credit_card/models/account.py
+++ account_payment_credit_card/models/account.py
@@ -1,6 +1,5 @@
-# Copyright 2017 Ursa Information Systems <http://www.ursainfosystems.com>
+# Copyright 2017 Open Source Integrators <http://www.opensourceintegrators.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 
 from odoo import fields, models
 
