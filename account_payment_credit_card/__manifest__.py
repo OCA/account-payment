a41df6a3b72d230de66c7986979091df646a5f77
mgosai <31255575+mgosai@users.noreply.github.com>
2018-08-17 14:54:24 -0400

    account_payment_credit_card: Fix contibutors (#196)

--- account_payment_credit_card/__manifest__.py
+++ account_payment_credit_card/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Ursa Information Systems <http://www.ursainfosystems.com>
+# Copyright 2017 Open Source Integrators <http://www.opensourceintegrators.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -6,9 +6,8 @@
     "summary": "Add support for credit card payments",
     "version": "11.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Ursa Information Systems, Odoo Community Association (OCA)",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Accounting & Finance",
-    "maintainer": "Ursa Information Systems",
     "website": "https://github.com/OCA/account-payment",
     "depends": [
         "account",
--- account_payment_credit_card/readme/CONTRIBUTORS.rst
+++ account_payment_credit_card/readme/CONTRIBUTORS.rst
@@ -1,6 +1,6 @@
 
 * Ray Carnes <rcarnes@ursainfosystems.com>
-* Balaji Kannan <bkannan@ursainfosystems.com>
-* Bhavesh Odedra <bodedra@ursainfosystems.com>
-* Sudarshan Kadalazhi <skadalazhi@ursainfosystems.com>
+* Balaji Kannan <bkannan@opensourceintegrators.com>
+* Bhavesh Odedra <bodedra@opensourceintegrators.com>
+* Sudarshan Kadalazhi <skadalazhi@opensourceintegrators.com>
 * Odoo Dev Team: DS, JA
