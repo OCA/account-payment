a3a35199e7d7af16db11fbc99df9a9f236b65dc4
mreficent <miquel.raich@eficent.com>
2018-08-27 19:57:11 +0200

    [MIG] account_payment_show_invoice: Migration to 11.0

--- account_payment_show_invoice/__manifest__.py
+++ account_payment_show_invoice/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment Show Invoice",
     "summary": "Extends the tree view of payments to show the paid invoices "
                "related to the payments using the vendor reference by default",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Account-payment",
     "website": "https://odoo-community.org/",
     "author": "Eficent, Odoo Community Association (OCA)",
