4ecc64dadb69daf3b45cf03835e4cc3af064220f
mreficent <miquel.raich@eficent.com>
2018-08-27 16:40:47 +0200

    [MIG] account_due_list_aging_comments: Migration to 11.0

--- account_due_list_aging_comments/__manifest__.py
+++ account_due_list_aging_comments/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': "Payments Due list aging comments",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Generic Modules/Payment',
     'author': 'Eficent,'
               'Odoo Community Association (OCA)',
@@ -11,7 +10,6 @@
     'license': 'AGPL-3',
     "depends": [
         'account_due_list',
-        'account_accountant',
     ],
     "data": [
         'views/payment_view.xml',
