e32832bdbf244b5399d52de628af6b247ed7250c
Denis Roussel <denis.roussel@acsone.eu>
2018-10-19 13:46:37 +0200

    [10.0][IMP] account_cash_discount_base: Add precision to discounts
    [FIX] Fixed error with rounding when creating a writeoff

--- account_cash_discount_base/models/account_payment_term.py
+++ account_cash_discount_base/models/account_payment_term.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+from odoo.addons import decimal_precision as dp
 
 
 class AccountPaymentTerm(models.Model):
@@ -10,7 +11,8 @@ class AccountPaymentTerm(models.Model):
     _inherit = 'account.payment.term'
 
     discount_percent = fields.Float(
-        string='Discount (%)'
+        string='Discount (%)',
+        digits=dp.get_precision('Discount'),
     )
     discount_delay = fields.Integer(
         string='Discount Delay (days)'
