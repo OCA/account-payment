32b21f4faaeaffa2cdd6b3f6f003aab330142543
Benjamin Willig <benjamin.willig@acsone.eu>
2018-11-28 13:56:16 +0100

    [ADD] handle supplier refund when paying a supplier invoice which contains a discount
    [ADD] use the supplier payment term when creating a refund

--- account_cash_discount_write_off/models/account_payment_line.py
+++ account_cash_discount_write_off/models/account_payment_line.py
@@ -14,16 +14,35 @@ class PaymentLine(models.Model):
     @api.multi
     def _check_cash_discount_write_off_creation(self):
         self.ensure_one()
-        return (
+        create_write_off = (
             self.pay_with_discount and
             self.move_line_id and
             self.move_line_id.invoice_id and
-            self.move_line_id.invoice_id.has_discount and
-            float_compare(
-                self.amount_currency,
-                self.move_line_id.invoice_id.amount_total_with_discount,
-                precision_rounding=self.currency_id.rounding) == 0
+            self.move_line_id.invoice_id.has_discount
         )
+        if not create_write_off:
+            return False
+
+        invoice = self.move_line_id.invoice_id
+        refunds_amount_total = 0.0
+        for pmove_line in invoice.payment_move_line_ids:
+            pmove_line_inv = pmove_line.invoice_id
+            if pmove_line_inv and pmove_line_inv.type == 'in_refund':
+                refunds_amount_total += pmove_line_inv.amount_total
+
+        # Invoice residual has already changed so we need to compute
+        # the residual with discount before the first reconciliation
+        amount_to_pay = (
+            invoice.amount_total -
+            refunds_amount_total -
+            invoice.discount_amount +
+            invoice.refunds_discount_amount
+        )
+        return float_compare(
+            self.amount_currency,
+            amount_to_pay,
+            precision_rounding=self.currency_id.rounding
+        ) == 0
 
     @api.multi
     def get_cash_discount_writeoff_move_values(self):
@@ -50,7 +69,10 @@ class PaymentLine(models.Model):
             )
 
         move_line_name = _("Cash Discount Write-Off")
-        supplier_account_amount = invoice.discount_amount
+        supplier_account_amount = (
+            invoice.discount_amount -
+            invoice.refunds_discount_amount
+        )
         discount_amount_credit = supplier_account_amount
 
         lines_values = [{
@@ -61,20 +83,29 @@ class PaymentLine(models.Model):
         }]
 
         if tax_adjustment:
+            refund_moves = invoice.payment_move_line_ids.filtered(
+                lambda line: line.invoice_id.type == 'in_refund'
+            ).mapped('move_id')
+            target_move_ids = refund_moves.ids + [move_line.move_id.id]
+
             tax_move_lines = self.env['account.move.line'].search([
-                ('move_id', '=', move_line.move_id.id),
+                ('move_id', 'in', target_move_ids),
                 '|',
                 ('tax_line_id', '!=', False),
                 ('tax_ids', '!=', False)
             ])
 
             for tax_move_line in tax_move_lines:
+                tax_invoice = tax_move_line.invoice_id
                 amount = float_round(
                     abs(tax_move_line.balance) *
-                    invoice.discount_percent / 100.0,
+                    tax_invoice.discount_percent / 100.0,
                     precision_rounding=rounding,
                 )
-                discount_amount_credit -= amount
+                if tax_move_line.credit > 0:
+                    discount_amount_credit += amount
+                elif tax_move_line.debit > 0:
+                    discount_amount_credit -= amount
 
                 if tax_move_line.tax_line_id:
                     account = tax_move_line.account_id
