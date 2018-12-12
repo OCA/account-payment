cfbc46ef38c5db2994e6d5ec77c67f3e18fa5f7f
Benjamin Willig <benjamin.willig@acsone.eu>
2018-12-12 10:32:30 +0100

    [FIX] Hide pay_with_discount field in the payment order line view
    [ADD] allow to toggle the discount on the payment order even if the discount due date is in the past
    
    [FIX] force to load discount payment term options when the partner changes

--- account_cash_discount_payment/models/account_invoice.py
+++ account_cash_discount_payment/models/account_invoice.py
@@ -11,7 +11,7 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     @api.multi
-    def _can_pay_invoice_with_discount(self):
+    def _can_pay_invoice_with_discount(self, check_due_date=True):
         self.ensure_one()
         today = fields.Date.today()
         rounding = self.currency_id.rounding
@@ -24,8 +24,13 @@ class AccountInvoice(models.Model):
             if pmove_line_inv and pmove_line_inv.type == 'in_refund':
                 refunds_amount_total += pmove_line_inv.amount_total
 
+        if check_due_date:
+            date_check_valid = self.discount_due_date >= today
+        else:
+            date_check_valid = True
+
         return (
-            self.discount_due_date >= today and
+            date_check_valid and
             float_compare(
                 self.residual,
                 self.amount_total - refunds_amount_total,
