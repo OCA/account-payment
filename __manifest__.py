507c4ab98d2228d8f1364db0b2cda3d3eed0b6df
Florent Cayré <florent@commown.fr>
2019-04-02 10:12:10 +0200

    Bump version to 1.1.5

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Slimpay Payment Acquirer',
     'category': 'Accounting',
     'summary': 'Payment Acquirer: Slimpay Implementation',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.5',
     'description': """Slimpay Payment Acquirer""",
     'author': "Commown SCIC SAS",
     'license': "AGPL-3",
