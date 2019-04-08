327b7167005797a16f539eabfd0588aca0791e87
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-04-08 16:59:55 +0200

    [10.0][ADD] account_check_report module

---
+++ account_check_report/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Check Report',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "Eficent,"
+              "Odoo Community Association (OCA)",
+    'category': 'Generic Modules/Accounting',
+    'website': "https://github.com/OCA/account-payment",
+    'depends': [],
+    'data': [
+        'report/account_check_report.xml',
+    ],
+    'installable': True,
+}
