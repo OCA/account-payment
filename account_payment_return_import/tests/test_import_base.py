cf7bb554a6d386fccd473d64ccddf74cf2323d8b
Thomas Binsfeld (ACSONE) <thomas.binsfeld@acsone.eu>
2019-05-08 14:02:42 +0200

    [12.0] [ADD] account_payment_return_import_iso20022 (#236)
    * [ADD] Account Payment Return Import CAMT: parser camt.054.001.02 files
    
    * [REF] Account Payment Return Import ISO20022: merge the two iso20022 modules in one
    
    * [REF] Account Payment Return Import: use SavepointCase instead of TransactionCase
    
    * [REF] Increase global code coverage

--- account_payment_return_import/tests/test_import_base.py
+++ account_payment_return_import/tests/test_import_base.py
@@ -8,66 +8,67 @@ from odoo.exceptions import UserError
 class TestImportBase(TestPaymentReturnFile):
     """Run test to import payment return import."""
 
-    def setUp(self):
-        super(TestImportBase, self).setUp()
-        self.company = self.env.ref('base.main_company')
-        self.acc_number = 'NL77ABNA0574908765'
-        self.acc_bank = self.env['res.partner.bank'].create({
-            'acc_number': self.acc_number,
+    @classmethod
+    def setUpClass(cls):
+        super(TestImportBase, cls).setUpClass()
+        cls.company = cls.env.ref('base.main_company')
+        cls.acc_number = 'NL77ABNA0574908765'
+        cls.acc_bank = cls.env['res.partner.bank'].create({
+            'acc_number': cls.acc_number,
             'bank_name': 'TEST BANK',
-            'company_id': self.company.id,
-            'partner_id': self.company.partner_id.id,
+            'company_id': cls.company.id,
+            'partner_id': cls.company.partner_id.id,
         })
-        self.journal = self.env['account.journal'].create({
+        cls.journal = cls.env['account.journal'].create({
             'name': 'Test Bank Journal',
             'code': 'BANK',
             'type': 'bank',
             'update_posted': True,
-            'bank_account_id': self.acc_bank.id,
+            'bank_account_id': cls.acc_bank.id,
         })
-        self.journal.bank_account_id = self.acc_bank
-        self.partner = self.env['res.partner'].create({
+        cls.journal.bank_account_id = cls.acc_bank
+        cls.partner = cls.env['res.partner'].create({
             'name': 'Test partner',
         })
-        self.reason = self.env['payment.return.reason'].create({
+        cls.reason = cls.env['payment.return.reason'].create({
             'code': 'RTEST',
             'name': 'Reason Test'
         })
-        self.account_type = self.env['account.account.type'].create({
+        cls.account_type = cls.env['account.account.type'].create({
             'name': 'Test',
             'type': 'receivable',
         })
-        self.account = self.env['account.account'].create({
+        cls.account = cls.env['account.account'].create({
             'name': 'Test account',
             'code': 'TEST',
-            'user_type_id': self.account_type.id,
+            'user_type_id': cls.account_type.id,
             'reconcile': True,
         })
-        self.account_income = self.env['account.account'].create({
+        cls.account_income = cls.env['account.account'].create({
             'name': 'Test income account',
             'code': 'INCOME',
-            'user_type_id': self.env['account.account.type'].create(
+            'user_type_id': cls.env['account.account.type'].create(
                 {'name': 'Test income'}).id,
         })
-        self.invoice = self.env['account.invoice'].create({
-            'journal_id': self.journal.id,
-            'account_id': self.account.id,
-            'company_id': self.env.user.company_id.id,
-            'currency_id': self.env.user.company_id.currency_id.id,
-            'partner_id': self.partner.id,
+        cls.invoice = cls.env['account.invoice'].create({
+            'journal_id': cls.journal.id,
+            'account_id': cls.account.id,
+            'company_id': cls.env.user.company_id.id,
+            'currency_id': cls.env.user.company_id.currency_id.id,
+            'partner_id': cls.partner.id,
             'invoice_line_ids': [(0, 0, {
-                'account_id': self.account_income.id,
+                'account_id': cls.account_income.id,
                 'name': 'Test line',
                 'price_unit': 250.9,
                 'quantity': 1,
             })]
         })
-        self.invoice.action_invoice_open()
-        self.receivable_line = self.invoice.move_id.line_ids.filtered(
+        cls.invoice.action_invoice_open()
+        cls.receivable_line = cls.invoice.move_id.line_ids.filtered(
             lambda x: x.account_id.internal_type == 'receivable')
         # Invert the move to simulate the payment
-        self.payment_move = self.invoice.move_id.copy({
-            'journal_id': self.journal.id
+        cls.payment_move = cls.invoice.move_id.copy({
+            'journal_id': cls.journal.id
         })
 
     def test_payment_return_import(self):
