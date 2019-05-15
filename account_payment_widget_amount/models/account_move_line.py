405080ef9bf666bc7f15ec1d3ab96e74485ba954
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-05-15 16:22:32 +0200

    [12.0][MIG] account_payment_widget_amount module

--- account_payment_widget_amount/models/account_move_line.py
+++ account_payment_widget_amount/models/account_move_line.py
@@ -1,38 +1,61 @@
-# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
 
 from odoo import api, models, _
 from odoo.exceptions import UserError
+
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
     @api.model
     def update_amount_reconcile(
-            self, amount_reconcile, amount_reconcile_currency,
-            sm_credit_move, sm_debit_move):
+            self, temp_amount_residual, temp_amount_residual_currency,
+            amount_reconcile, credit_move, debit_move):
+
         super(AccountMoveLine, self).update_amount_reconcile(
-            amount_reconcile, amount_reconcile_currency,
-            sm_credit_move, sm_debit_move)
-        # Check for the currency and amount_currency we can set
-        amount_reconcile_currency = 0
+            temp_amount_residual, temp_amount_residual_currency,
+            amount_reconcile, credit_move, debit_move)
+
         paid_amt = self.env.context.get('paid_amount', False)
+
         if paid_amt < 0:
             raise UserError(_(
                 "The specified amount has to be strictly positive"))
-        if sm_debit_move.currency_id == sm_credit_move.currency_id \
-                and sm_debit_move.currency_id.id:
-            if paid_amt:
-                amount_reconcile_currency = min(
-                    sm_debit_move.amount_residual_currency, paid_amt)
-                paid_amt = paid_amt * sm_credit_move.company_currency_id.rate
-            else:
-                amount_reconcile_currency = min(
-                    sm_debit_move.amount_residual_currency,
-                    -sm_credit_move.amount_residual_currency)
-        if paid_amt:
-            amount_reconcile = min(sm_debit_move.amount_residual, paid_amt)
-        else:
-            amount_reconcile = min(sm_debit_move.amount_residual,
-                                   -sm_credit_move.amount_residual)
 
-        return amount_reconcile, amount_reconcile_currency
+        # We need those temporary value otherwise the computation might
+        # be wrong below
+        if paid_amt:
+            temp_amount_residual_currency = min(
+                debit_move.amount_residual_currency, paid_amt)
+            if temp_amount_residual_currency:
+                paid_amt = paid_amt * credit_move.company_currency_id.rate
+            temp_amount_residual = min(debit_move.amount_residual,
+                                       paid_amt)
+        else:
+            temp_amount_residual = min(debit_move.amount_residual,
+                                       -credit_move.amount_residual)
+            temp_amount_residual_currency = min(
+                debit_move.amount_residual_currency,
+                -credit_move.amount_residual_currency)
+
+        amount_reconcile = temp_amount_residual_currency or \
+            temp_amount_residual
+
+        return temp_amount_residual, temp_amount_residual_currency, \
+            amount_reconcile
+
+    @api.model
+    def _check_remove_debit_move(self, amount_reconcile, debit_move, field):
+        res = super(AccountMoveLine, self)._check_remove_debit_move(
+            amount_reconcile, debit_move, field)
+        if self.env.context.get('paid_amount', False):
+            return True
+        return res
+
+    @api.model
+    def _check_remove_credit_move(self, amount_reconcile, credit_move, field):
+        res = super(AccountMoveLine, self)._check_remove_credit_move(
+            amount_reconcile, credit_move, field)
+        if self.env.context.get('paid_amount', False):
+            return True
+        return res
