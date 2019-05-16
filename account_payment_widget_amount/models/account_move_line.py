d707bfd0c34a0d1f3cd20dc23864a48d433c446f
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-05-16 10:13:11 +0200

    [IMP] replace popover depending on screen size

--- account_payment_widget_amount/models/account_move_line.py
+++ account_payment_widget_amount/models/account_move_line.py
@@ -16,27 +16,33 @@ class AccountMoveLine(models.Model):
             temp_amount_residual, temp_amount_residual_currency,
             amount_reconcile, credit_move, debit_move)
 
+        # Check if amount is positive
         paid_amt = self.env.context.get('paid_amount', False)
-
         if paid_amt < 0:
             raise UserError(_(
                 "The specified amount has to be strictly positive"))
 
         # We need those temporary value otherwise the computation might
         # be wrong below
-        if paid_amt:
-            temp_amount_residual_currency = min(
-                debit_move.amount_residual_currency, paid_amt)
-            if temp_amount_residual_currency:
-                paid_amt = paid_amt * credit_move.company_currency_id.rate
-            temp_amount_residual = min(debit_move.amount_residual,
-                                       paid_amt)
-        else:
-            temp_amount_residual = min(debit_move.amount_residual,
-                                       -credit_move.amount_residual)
-            temp_amount_residual_currency = min(
-                debit_move.amount_residual_currency,
-                -credit_move.amount_residual_currency)
+
+        # Compute paid_amount currency
+        paid_amt_currency = paid_amt and min(
+            float(paid_amt), -credit_move.amount_residual_currency) or \
+            -credit_move.amount_residual_currency
+
+        temp_amount_residual_currency = min(
+            debit_move.amount_residual_currency, paid_amt_currency)
+
+        # If previous value is not 0 we compute paid amount in the company
+        # currency taking into account the rate
+        if temp_amount_residual_currency:
+            paid_amt = paid_amt * credit_move.company_currency_id.rate
+        paid_amt = paid_amt and min(
+            float(paid_amt), -credit_move.amount_residual_currency) or \
+            -credit_move.amount_residual
+
+        temp_amount_residual = min(debit_move.amount_residual,
+                                   paid_amt)
 
         amount_reconcile = temp_amount_residual_currency or \
             temp_amount_residual
@@ -48,7 +54,7 @@ class AccountMoveLine(models.Model):
     def _check_remove_debit_move(self, amount_reconcile, debit_move, field):
         res = super(AccountMoveLine, self)._check_remove_debit_move(
             amount_reconcile, debit_move, field)
-        if self.env.context.get('paid_amount', False):
+        if not isinstance(self.env.context.get('paid_amount', False), bool):
             return True
         return res
 
@@ -56,6 +62,6 @@ class AccountMoveLine(models.Model):
     def _check_remove_credit_move(self, amount_reconcile, credit_move, field):
         res = super(AccountMoveLine, self)._check_remove_credit_move(
             amount_reconcile, credit_move, field)
-        if self.env.context.get('paid_amount', False):
+        if not isinstance(self.env.context.get('paid_amount', False), bool):
             return True
         return res
