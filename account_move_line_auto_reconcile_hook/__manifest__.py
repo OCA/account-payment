cc50a8342b7f311dd6af473d1e10cefc66a724d7
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-05-17 08:57:27 +0200

    [11.0][MIG] account_payment_widget_amount module

--- account_move_line_auto_reconcile_hook/__manifest__.py
+++ account_move_line_auto_reconcile_hook/__manifest__.py
@@ -1,9 +1,8 @@
 # Copyright 2019 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Account Payment Widget Amount",
-    "summary": "Extends the payment widget to be able to choose the payment "
-               "amount",
+    "name": "Account Move Line Auto Reconcile Hook",
+    "summary": "Adds Hook to account move line",
     "version": "11.0.1.0.0",
     "category": "Account-payment",
     "website": "https://odoo-community.org/",
@@ -11,11 +10,6 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": ["account_move_line_auto_reconcile_hook"],
-    "data": [
-        'views/account.xml',
-    ],
-    'qweb': [
-        "static/src/xml/account_payment.xml",
-    ],
+    "depends": ["account"],
+    'post_load': 'post_load_hook',
 }
--- account_payment_widget_amount/models/account_move_line.py
+++ account_payment_widget_amount/models/account_move_line.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Eficent Business and IT Consulting Services, S.L.
 
 from odoo import api, models, _
@@ -17,24 +16,26 @@ class AccountMoveLine(models.Model):
             sm_credit_move, sm_debit_move)
         # Check for the currency and amount_currency we can set
         amount_reconcile_currency = 0
+
+        # Check if amount is positive
         paid_amt = self.env.context.get('paid_amount', False)
         if paid_amt < 0:
             raise UserError(_(
                 "The specified amount has to be strictly positive"))
+
         if sm_debit_move.currency_id == sm_credit_move.currency_id \
                 and sm_debit_move.currency_id.id:
+            paid_amt = paid_amt and min(
+                float(paid_amt), -sm_credit_move.amount_residual_currency) or \
+                -sm_credit_move.amount_residual_currency
+            amount_reconcile_currency = min(
+                sm_debit_move.amount_residual_currency, paid_amt)
             if paid_amt:
-                amount_reconcile_currency = min(
-                    sm_debit_move.amount_residual_currency, paid_amt)
                 paid_amt = paid_amt * sm_credit_move.company_currency_id.rate
-            else:
-                amount_reconcile_currency = min(
-                    sm_debit_move.amount_residual_currency,
-                    -sm_credit_move.amount_residual_currency)
-        if paid_amt:
-            amount_reconcile = min(sm_debit_move.amount_residual, paid_amt)
-        else:
-            amount_reconcile = min(sm_debit_move.amount_residual,
-                                   -sm_credit_move.amount_residual)
+
+        paid_amt = paid_amt and min(
+            float(paid_amt), -sm_credit_move.amount_residual) or \
+            -sm_credit_move.amount_residual
+        amount_reconcile = min(sm_debit_move.amount_residual, paid_amt)
 
         return amount_reconcile, amount_reconcile_currency
