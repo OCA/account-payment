648df75f236ff9eece2d22e9b744c9420f60ca1d
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-05-20 14:50:19 +0200

    [FIX] logic to reconcile amount

--- account_payment_widget_amount/models/account_move_line.py
+++ account_payment_widget_amount/models/account_move_line.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Eficent Business and IT Consulting Services, S.L.
 
-from odoo import api, models, _
+from odoo import api, models, fields, _
 from odoo.exceptions import UserError
 
 
@@ -17,25 +17,24 @@ class AccountMoveLine(models.Model):
         # Check for the currency and amount_currency we can set
         amount_reconcile_currency = 0
 
-        # Check if amount is positive
+        # Check if amount in context and if amount is positive
         paid_amt = self.env.context.get('paid_amount', False)
+        if not paid_amt:
+            return amount_reconcile, amount_reconcile_currency
+        paid_amt = float(paid_amt)
         if paid_amt < 0:
             raise UserError(_(
                 "The specified amount has to be strictly positive"))
 
         if sm_debit_move.currency_id == sm_credit_move.currency_id \
                 and sm_debit_move.currency_id.id:
-            paid_amt = paid_amt and min(
-                float(paid_amt), -sm_credit_move.amount_residual_currency) or \
-                -sm_credit_move.amount_residual_currency
             amount_reconcile_currency = min(
-                sm_debit_move.amount_residual_currency, paid_amt)
-            if paid_amt:
-                paid_amt = paid_amt * sm_credit_move.company_currency_id.rate
-
-        paid_amt = paid_amt and min(
-            float(paid_amt), -sm_credit_move.amount_residual) or \
-            -sm_credit_move.amount_residual
-        amount_reconcile = min(sm_debit_move.amount_residual, paid_amt)
+                sm_debit_move.amount_residual_currency,
+                -sm_credit_move.amount_residual_currency, paid_amt)
+            paid_amt = sm_debit_move.currency_id.with_context(
+                date=sm_credit_move.date or fields.Date.today()).compute(
+                paid_amt, sm_debit_move.company_id.currency_id)
+        amount_reconcile = min(sm_debit_move.amount_residual,
+                               -sm_credit_move.amount_residual, paid_amt)
 
         return amount_reconcile, amount_reconcile_currency
