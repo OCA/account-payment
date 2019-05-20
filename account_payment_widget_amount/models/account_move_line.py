0f48e5fba4a337f7049ebb77c0cdc78c469732e3
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-05-20 13:59:00 +0200

    - adds fixes to the logic to reconcile the amount maid - adds tests to the python side

--- account_payment_widget_amount/models/account_move_line.py
+++ account_payment_widget_amount/models/account_move_line.py
@@ -1,6 +1,6 @@
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 
-from odoo import api, models, _
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 
@@ -15,9 +15,12 @@ class AccountMoveLine(models.Model):
         super(AccountMoveLine, self).update_amount_reconcile(
             temp_amount_residual, temp_amount_residual_currency,
             amount_reconcile, credit_move, debit_move)
-
         # Check if amount is positive
-        paid_amt = self.env.context.get('paid_amount', False)
+        paid_amt = self.env.context.get('paid_amount', 0.0)
+        if not paid_amt:
+            return temp_amount_residual, temp_amount_residual_currency, \
+                amount_reconcile
+        paid_amt = float(paid_amt)
         if paid_amt < 0:
             raise UserError(_(
                 "The specified amount has to be strictly positive"))
@@ -26,24 +29,26 @@ class AccountMoveLine(models.Model):
         # be wrong below
 
         # Compute paid_amount currency
-        paid_amt_currency = paid_amt and min(
-            float(paid_amt), -credit_move.amount_residual_currency) or \
-            -credit_move.amount_residual_currency
+        if debit_move.amount_residual_currency or \
+                credit_move.amount_residual_currency:
 
-        temp_amount_residual_currency = min(
-            debit_move.amount_residual_currency, paid_amt_currency)
+            temp_amount_residual_currency = min(
+                debit_move.amount_residual_currency,
+                -credit_move.amount_residual_currency,
+                paid_amt)
+        else:
+            temp_amount_residual_currency = 0.0
 
         # If previous value is not 0 we compute paid amount in the company
         # currency taking into account the rate
         if temp_amount_residual_currency:
-            paid_amt = paid_amt * credit_move.company_currency_id.rate
-        paid_amt = paid_amt and min(
-            float(paid_amt), -credit_move.amount_residual_currency) or \
-            -credit_move.amount_residual
-
+            paid_amt = debit_move.currency_id._convert(
+                paid_amt, debit_move.company_id.currency_id,
+                debit_move.company_id,
+                credit_move.date or fields.Date.today())
         temp_amount_residual = min(debit_move.amount_residual,
+                                   -credit_move.amount_residual,
                                    paid_amt)
-
         amount_reconcile = temp_amount_residual_currency or \
             temp_amount_residual
 
