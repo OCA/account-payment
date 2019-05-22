d8fd2d16649fbb50b505d5a1e5f911d546134323
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-05-22 16:07:36 +0530

    [MIG][11.0] account_payment_batch_process

--- account_payment_batch_process/__manifest__.py
+++ account_payment_batch_process/__manifest__.py
@@ -1,22 +1,23 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Ursa Information Systems <http://www.ursainfosystems.com>
+# Copyright 2019 Open Source Integrators
+# <https://www.opensourceintegrators.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Batch Payments Processing",
     "summary": "Process Payments in Batch",
-    "version": "10.0.1.0.1",
+    "version": "11.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Ursa Information Systems, Odoo Community Association (OCA)",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Generic Modules/Payment",
-    "website": "http://www.ursainfosystems.com",
+    "website": "https://github.com/OCA/account-payment",
     "depends": [
-        "account",
         "account_check_printing",
     ],
     "data": [
         "wizard/invoice_batch_process_view.xml",
         "views/invoice_view.xml"
     ],
-    "installable": True,
+    "application": False,
+    "development_status": "Beta",
+    "maintainers": ["max3903"],
 }
