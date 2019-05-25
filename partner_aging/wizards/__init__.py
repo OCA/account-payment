95ca6a74ccbf2aa8defdbaad32fcc55d008299e9
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-05-25 17:32:19 +0530

    [11.0][MIG] partner_aging

--- partner_aging/wizards/__init__.py
+++ partner_aging/wizards/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2012 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import res_partner_aging_customer
