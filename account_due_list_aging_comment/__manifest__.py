c57720af836dd35af27bcefb62ab372da2c53e9f
David Martinez <david.martinez@dmpyme.com>
2019-06-08 10:38:55 +0200

    [MIG] account_due_list_aging_comment: Migration to 12.0
    [UPD] README.rst
    
    [UPD] Update account_due_list_aging_comment.pot

--- account_due_list_aging_comment/__manifest__.py
+++ account_due_list_aging_comment/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2015-2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    'name': "Payments Due list aging comment",
-    'version': '11.0.1.0.0',
+    'name': "Account Due List Aging Comment",
+    'version': '12.0.1.0.0',
     'category': 'Generic Modules/Payment',
     'author': 'Eficent,'
-              'Odoo Community Association (OCA)',
-    'website': 'http://www.eficent.com',
+              'Odoo Community Association (OCA),',
+    'website': 'https://github.com/OCA/account-payment',
     'license': 'AGPL-3',
     "depends": [
         'account_due_list',
---
+++ account_due_list_aging_comment/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Jordi Ballester Alomar <jordi.ballester@eficent.com>
+* Meyomesse Gilles <meyomesse.gilles@gmail.com>
+* David Martinez <david.martinez@dmpyme.com>
