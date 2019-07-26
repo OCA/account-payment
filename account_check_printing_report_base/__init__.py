284a02a1e29d5a7707e405e542cd4d4f98ff250d
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-07-26 18:59:15 +0200

    add modules description

--- account_check_printing_report_base/__init__.py
+++ account_check_printing_report_base/__init__.py
@@ -1,7 +1,2 @@
-# Copyright 2016 Eficent Business and IT Consulting Services S.L.
-# (http://www.eficent.com)
-# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from . import models
 from . import report
