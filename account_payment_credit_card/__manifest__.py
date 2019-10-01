0c8bb0b9afb0a3cc5ce7e96cfe06529b4dc4bab6
Nikul-Chaudhary <nikulchaudhary2112@gmail.com>
2019-10-01 22:05:36 +0530

    [MIG] Migration account_payment_credit_card v12 to v13

--- account_payment_credit_card/__manifest__.py
+++ account_payment_credit_card/__manifest__.py
@@ -4,17 +4,13 @@
 {
     "name": "Credit Card Payments",
     "summary": "Add support for credit card payments",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.1",
     "license": "AGPL-3",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-payment",
-    "depends": [
-        "account",
-    ],
-    "data": [
-        "views/account_view.xml",
-    ],
+    "depends": ["account"],
+    "data": ["views/account_view.xml"],
     "installable": True,
     "development_status": "Beta",
     "maintainers": ["max3903"],
