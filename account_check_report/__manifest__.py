d5044185c5441904cf288edfbda421ab19d2a237
mreficent <miquel.raich@eficent.com>
2019-11-08 19:13:15 +0100

    [MIG] account_check_report: Migration to 12.0

--- account_check_report/__manifest__.py
+++ account_check_report/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Check Report',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Eficent,"
               "Odoo Community Association (OCA)",
