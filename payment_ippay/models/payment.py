8f3fb3c94d28af613185fe1153553e4ed3fc0611
Daniel Reis <dreis@opensourceintegrators.com>
2020-01-22 13:22:55 +0000

    [ADD] IPPay Credit Card payment acquirer

---
+++ payment_ippay/models/payment.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import requests
+import xmltodict
+from odoo import api, fields, models, _
+from odoo.addons.payment.models.payment_acquirer import ValidationError
+
+
+class PaymentAcquirer(models.Model):
+    _inherit = 'payment.acquirer'
+
+    provider = fields.Selection(selection_add=[('ippay', 'IPpay')])
+    api_url = fields.Char("Api URL", required_if_provider='ippay')
+    ippay_terminal_id = fields.Char("IPpay TerminalID",
+                                    required_if_provider='ippay')
+
+
+class PaymentToken(models.Model):
+    _inherit = 'payment.token'
+
+    @api.model
+    def ippay_create(self, values):
+        acquirer = self.env['payment.acquirer'].browse(
+            values['acquirer_id'])
+        if values.get('cc_number'):
+            values['cc_number'] = values['cc_number'].replace(' ', '')
+            card_detail = {'cc_number': values['cc_number'],
+                           'expiry_month': values['cc_expiry_month'],
+                           'expiry_year': values['cc_expiry_year']}
+
+            xml = '''<ippay>
+            <TransactionType>TOKENIZE</TransactionType>
+            <TerminalID>%s</TerminalID>
+            <CardNum>%s</CardNum>
+            <CardExpMonth>%s</CardExpMonth>
+            <CardExpYear>%s</CardExpYear>
+            </ippay>''' % (acquirer.ippay_terminal_id,
+                           card_detail.get('cc_number'),
+                           card_detail.get('expiry_month'),
+                           card_detail.get('expiry_year'))
+            if acquirer.api_url:
+                url = acquirer.api_url
+            r = requests.post(url,
+                              data=xml, headers={'Content-Type': 'text/xml'})
+            data = xmltodict.parse(
+                r.content)
+            token = data['IPPayResponse'].get('Token')
+            if token:
+                return {
+                    'name': 'XXXXXXXXXXXX%s - %s' % (
+                        values['cc_number'][-4:], values['cc_holder_name']),
+                    'acquirer_ref': token,
+                }
+            else:
+                raise ValidationError(
+                    _('Customer payment token creation in IPpay failed: %s' % (
+                        data['IPPayResponse'].get('ErrMsg'))
+                      ))
+        else:
+            return values
---
+++ payment_ippay/models/payment_transaction.py
@@ -0,0 +1,64 @@
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import requests
+import xmltodict
+import logging
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class PaymentTansaction(models.Model):
+    _inherit = 'payment.transaction'
+
+    @api.multi
+    def _ippay_s2s_do_payment(self, invoice):
+        acquirer = self.acquirer_id
+        request = '''
+            <ippay>
+                <TransactionType>SALE</TransactionType>
+                <TerminalID>%s</TerminalID>
+                <Token>%s</Token>
+                <TotalAmount>%s</TotalAmount>
+            </ippay>''' % (
+            acquirer.ippay_terminal_id,
+            self.payment_token_id.acquirer_ref,
+            str(self.amount).replace('.', ''))
+        # TODO_ add verbosity parameter?
+        _logger.info("Request to get IPPay Transaction ID: %s" % (request))
+        try:
+            r = requests.post(
+                acquirer.api_url,
+                data=request,
+                headers={'Content-Type': 'text/xml'})
+        except Exception as e:
+            raise ValidationError(_(e))
+        _logger.info("Transaction Received: %s" % (r.content))
+
+        data = xmltodict.parse(r.content)
+        response = data.get('ippayResponse')
+        # TODO: don't set as really paid if using test environment
+        # - risk of wrong data in Production db!
+        if response.get('ResponseText') == 'APPROVED':
+            self.acquirer_reference = response.get(
+                'TransactionID')
+            self.date = fields.Datetime.now()
+            self.state = 'done'
+        else:
+            self.state = 'cancel'
+            invoice.message_post(
+                body=_("IPPay Credit Card Payment DECLINED: %s - %s") % (
+                    response.get('ActionCode'),
+                    response.get('ErrMsg'),
+                ))
+
+    @api.multi
+    def ippay_s2s_do_transaction(self):
+        context = self.env.context
+        inv_rec = self.env['account.invoice'].browse(context.get('active_ids'))
+        for inv in inv_rec:
+            if inv.type == 'out_invoice':
+                self._ippay_s2s_do_payment(invoice=inv)
