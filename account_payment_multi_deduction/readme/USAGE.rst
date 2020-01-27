44f100eb9dfdbebbe6e6f984c0f8e8d79e340c18
kittiu <kittiu@gmail.com>
2020-01-27 11:18:07 +0700

    [13.0][MIG] account_payment_multi_deduction

--- account_payment_multi_deduction/readme/USAGE.rst
+++ account_payment_multi_deduction/readme/USAGE.rst
@@ -1,6 +1,8 @@
-- Select invoice(s), either on form view or tree view
+- Select 1 invoice, either on form view or tree view
 - Click to Register Payment, a payment wizard will open
 - Reduce the amount to pay and payment difference amount will appear
 - Choose "Mark invoice as fully paid (multi deduct)", and a new deduction table will appear
 - Add deduction amount, make sure total deduction amount is equal to the payment difference
 - Click validate to finish the payment
+
+Note: this feature only works for 1 invoice payment
--- account_payment_multi_deduction/tests/test_payment_multi_deduction.py
+++ account_payment_multi_deduction/tests/test_payment_multi_deduction.py
@@ -1,70 +1,54 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+from odoo import fields
+from odoo.exceptions import UserError
 from odoo.tests.common import Form, SavepointCase
 
 
 class TestPaymentMultiDeduction(SavepointCase):
     @classmethod
-    def setUpClass(self):
-        super(TestPaymentMultiDeduction, self).setUpClass()
+    def setUpClass(cls):
+        super(TestPaymentMultiDeduction, cls).setUpClass()
 
-        self.account_receivable = self.env["account.account"].search(
+        cls.partner = cls.env.ref("base.res_partner_12")
+        cls.account_receivable = cls.partner.property_account_receivable_id
+        cls.account_revenue = cls.env["account.account"].search(
             [
                 (
                     "user_type_id",
                     "=",
-                    self.env.ref("account.data_account_type_receivable").id,
+                    cls.env.ref("account.data_account_type_revenue").id,
                 )
             ],
             limit=1,
         )
-        self.account_payable = self.env["account.account"].search(
+        cls.account_expense = cls.env["account.account"].search(
             [
                 (
                     "user_type_id",
                     "=",
-                    self.env.ref("account.data_account_type_payable").id,
-                )
-            ],
-            limit=1,
-        )
-        self.account_revenue = self.env["account.account"].search(
-            [
-                (
-                    "user_type_id",
-                    "=",
-                    self.env.ref("account.data_account_type_revenue").id,
-                )
-            ],
-            limit=1,
-        )
-        self.account_expense = self.env["account.account"].search(
-            [
-                (
-                    "user_type_id",
-                    "=",
-                    self.env.ref("account.data_account_type_expenses").id,
+                    cls.env.ref("account.data_account_type_expenses").id,
                 )
             ],
             limit=1,
         )
 
-        self.cust_invoice_1 = self.env["account.invoice"].create(
+        cls.cust_invoice = cls.env["account.move"].create(
             {
                 "name": "Test Customer Invoice",
-                "journal_id": self.env["account.journal"]
+                "type": "out_invoice",
+                "journal_id": cls.env["account.journal"]
                 .search([("type", "=", "sale")], limit=1)
                 .id,
-                "partner_id": self.env.ref("base.res_partner_12").id,
-                "account_id": self.account_receivable.id,
+                "partner_id": cls.env.ref("base.res_partner_12").id,
                 "invoice_line_ids": [
                     (
                         0,
                         0,
                         {
-                            "product_id": self.env.ref("product.product_product_3").id,
+                            "product_id": cls.env.ref("product.product_product_3").id,
                             "quantity": 1.0,
-                            "account_id": self.account_revenue.id,
+                            "account_id": cls.account_revenue.id,
                             "name": "[PCSC234] PC Assemble SC234",
                             "price_unit": 450.00,
                         },
@@ -73,18 +57,45 @@ class TestPaymentMultiDeduction(SavepointCase):
             }
         )
 
-        self.cust_invoice_2 = self.cust_invoice_1.copy()
+        # New currency, 2X lower
+        cls.company_currency = cls.cust_invoice.currency_id
+        cls.currency_2x = cls.env["res.currency"].create(
+            {
+                "name": "2X",  # Foreign currency, 2 time
+                "symbol": "X",
+                "rate_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": fields.Date.today(),
+                            "rate": cls.company_currency.rate * 2,
+                        },
+                    )
+                ],
+            }
+        )
 
-    def test_1_one_invoice_payment(self):
+    def test_one_invoice_payment(self):
         """ Validate 1 invoice and make payment with 2 deduction """
-        self.cust_invoice_1.action_invoice_open()  # total amount 450.0
+        self.cust_invoice.post()  # total amount 450.0
         ctx = {
-            "active_ids": [self.cust_invoice_1.id],
-            "active_id": self.cust_invoice_1.id,
-            "active_model": "account.invoice",
+            "active_ids": [self.cust_invoice.id],
+            "active_id": self.cust_invoice.id,
+            "active_model": "account.move",
         }
         PaymentWizard = self.env["account.payment"]
-        view_id = "account_payment_multi_deduction." "view_account_payment_invoice_form"
+        view_id = "account_payment_multi_deduction." "view_account_payment_form"
+        with self.assertRaises(UserError):  # Deduct only 20.0, throw error
+            with Form(PaymentWizard.with_context(ctx), view=view_id) as f:
+                f.amount = 400.0
+                f.payment_difference_handling = "reconcile_multi_deduct"
+                with f.deduction_ids.new() as f2:
+                    f2.account_id = self.account_expense
+                    f2.name = "Expense 1"
+                    f2.amount = 20.0
+            f.save()
+
         with Form(PaymentWizard.with_context(ctx), view=view_id) as f:
             f.amount = 400.0  # Reduce to 400.0, and mark fully paid (multi)
             f.payment_difference_handling = "reconcile_multi_deduct"
@@ -96,8 +107,9 @@ class TestPaymentMultiDeduction(SavepointCase):
                 f2.account_id = self.account_expense
                 f2.name = "Expense 2"
                 f2.amount = 30.0
+
         payment = f.save()
-        payment.action_validate_invoice_payment()
+        payment.post()
 
         self.assertEqual(payment.state, "posted")
 
@@ -109,18 +121,16 @@ class TestPaymentMultiDeduction(SavepointCase):
             or payment.journal_id.default_credit_account_id
         )
 
-        self.assertEqual(self.cust_invoice_1.state, "paid")
-
+        self.assertEqual(self.cust_invoice.invoice_payment_state, "paid")
         self.assertRecordValues(
             move_lines,
             [
-                {"account_id": bank_account.id, "debit": 400.0, "credit": 0.0},
                 {
-                    "account_id": self.account_expense.id,
-                    "name": "Expense 2",
-                    "debit": 30.0,
-                    "credit": 0.0,
+                    "account_id": self.account_receivable.id,
+                    "debit": 0.0,
+                    "credit": 450.0,
                 },
+                {"account_id": bank_account.id, "debit": 400.0, "credit": 0.0},
                 {
                     "account_id": self.account_expense.id,
                     "name": "Expense 1",
@@ -128,39 +138,40 @@ class TestPaymentMultiDeduction(SavepointCase):
                     "credit": 0.0,
                 },
                 {
-                    "account_id": self.account_receivable.id,
-                    "debit": 0.0,
-                    "credit": 450.0,
+                    "account_id": self.account_expense.id,
+                    "name": "Expense 2",
+                    "debit": 30.0,
+                    "credit": 0.0,
                 },
             ],
         )
 
-    def test_2_two_invoices_register_payment(self):
-        """ Validate 2 invoice and make payment on both with 2 deduction """
-        self.cust_invoice_1.action_invoice_open()  # Total 900.0 = 450 + 450
-        self.cust_invoice_2.action_invoice_open()
+    def test_one_invoice_payment_foreign_currency(self):
+        """ Validate 1 invoice and make payment with 2 deduction """
+        self.cust_invoice.post()  # total amount 450.0
         ctx = {
-            "active_ids": [self.cust_invoice_1.id, self.cust_invoice_2.id],
-            "active_model": "account.invoice",
+            "active_ids": [self.cust_invoice.id],
+            "active_id": self.cust_invoice.id,
+            "active_model": "account.move",
         }
-        PaymentWizard = self.env["account.register.payments"]
-        view_id = (
-            "account_payment_multi_deduction." "view_account_payment_from_invoices"
-        )
+        PaymentWizard = self.env["account.payment"]
+        view_id = "account_payment_multi_deduction." "view_account_payment_form"
+
         with Form(PaymentWizard.with_context(ctx), view=view_id) as f:
-            f.amount = 600.0  # Reduce to 600.0, and mark fully paid (multi)
+            f.currency_id = self.currency_2x
+            f.amount = 800.0  # 400 -> 800 as we use currency 2x
             f.payment_difference_handling = "reconcile_multi_deduct"
             with f.deduction_ids.new() as f2:
                 f2.account_id = self.account_expense
                 f2.name = "Expense 1"
-                f2.amount = 100.0
+                f2.amount = 40.0  # 20 -> 40
             with f.deduction_ids.new() as f2:
                 f2.account_id = self.account_expense
                 f2.name = "Expense 2"
-                f2.amount = 200.0
-        register_payment = f.save()
-        vals = register_payment.create_payments()
-        payment = self.env["account.payment"].browse(vals.get("res_id"))
+                f2.amount = 60.0  # 60 -> 80
+
+        payment = f.save()
+        payment.post()
 
         self.assertEqual(payment.state, "posted")
 
@@ -172,29 +183,39 @@ class TestPaymentMultiDeduction(SavepointCase):
             or payment.journal_id.default_credit_account_id
         )
 
-        self.assertEqual(self.cust_invoice_1.state, "paid")
-        self.assertEqual(self.cust_invoice_2.state, "paid")
-
+        self.assertEqual(self.cust_invoice.invoice_payment_state, "paid")
         self.assertRecordValues(
             move_lines,
             [
-                {"account_id": bank_account.id, "debit": 600.0, "credit": 0.0},
                 {
-                    "account_id": self.account_expense.id,
-                    "name": "Expense 2",
-                    "debit": 200.0,
+                    "account_id": self.account_receivable.id,
+                    "debit": 0.0,
+                    "credit": 450.0,
+                    "amount_currency": -900.0,
+                    "currency_id": self.currency_2x.id,
+                },
+                {
+                    "account_id": bank_account.id,
+                    "debit": 400.0,
                     "credit": 0.0,
+                    "amount_currency": 800.0,
+                    "currency_id": self.currency_2x.id,
                 },
                 {
                     "account_id": self.account_expense.id,
                     "name": "Expense 1",
-                    "debit": 100.0,
+                    "debit": 20.0,
                     "credit": 0.0,
+                    "amount_currency": 40.0,
+                    "currency_id": self.currency_2x.id,
                 },
                 {
-                    "account_id": self.account_receivable.id,
-                    "debit": 0.0,
-                    "credit": 900.0,
+                    "account_id": self.account_expense.id,
+                    "name": "Expense 2",
+                    "debit": 30.0,
+                    "credit": 0.0,
+                    "amount_currency": 60.0,
+                    "currency_id": self.currency_2x.id,
                 },
             ],
         )
