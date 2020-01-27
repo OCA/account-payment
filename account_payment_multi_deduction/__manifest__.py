25cedc85808ef822e1b5822692e58b5584e4d74c
kittiu <kittiu@gmail.com>
2020-01-27 11:18:07 +0700

    [13.0][MIG] account_payment_multi_deduction

--- account_payment_multi_deduction/__manifest__.py
+++ account_payment_multi_deduction/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Payment Register with Multiple Deduction",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Ecosoft,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/account-payment/",
--- account_payment_multi_deduction/readme/HISTORY.rst
+++ account_payment_multi_deduction/readme/HISTORY.rst
@@ -1,3 +1,9 @@
+13.0.1.0.0 (2020-01-27)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* Migration to version 13
+* Following how Odoo 13 works with deduction, the multiple deduction feature only works for one invoice at a time.
+
 12.0.1.0.0 (2019-05-05)
 ~~~~~~~~~~~~~~~~~~~~~~~
 
