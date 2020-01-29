959317e4bd7adf27b638c74c8195490d1f2fbca6
mreficent <miquel.raich@eficent.com>
2020-01-29 11:04:01 +0100

    [IMP] account_due_list: black, isort

--- account_due_list/__manifest__.py
+++ account_due_list/__manifest__.py
@@ -1,19 +1,17 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Payments Due list',
-    'version': '12.0.1.0.0',
-    'category': 'Generic Modules/Payment',
-    'development_status': 'Beta',
-    'author': 'Odoo Community Association (OCA)',
-    'summary': 'List of open credits and debits, with due date',
-    'website': 'https://github.com/OCA/account-payment',
-    'license': 'LGPL-3',
-    'depends': ['account'],
-    'data': [
-        'views/payment_view.xml',
-    ],
-    'pre_init_hook': 'pre_init_hook',
-    'installable': True,
-    'auto_install': False,
+    "name": "Payments Due list",
+    "version": "12.0.1.0.0",
+    "category": "Generic Modules/Payment",
+    "development_status": "Beta",
+    "author": "Odoo Community Association (OCA)",
+    "summary": "List of open credits and debits, with due date",
+    "website": "https://github.com/OCA/account-payment",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "data": ["views/payment_view.xml"],
+    "pre_init_hook": "pre_init_hook",
+    "installable": True,
+    "auto_install": False,
 }
