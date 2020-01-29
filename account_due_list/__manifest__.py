b8af2200435c8031a820358e72283c568f258005
mreficent <miquel.raich@eficent.com>
2020-01-29 12:04:57 +0100

    [MIG] account_due_list: Migration to 13.0

--- account_due_list/__manifest__.py
+++ account_due_list/__manifest__.py
@@ -1,14 +1,14 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Payments Due list",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Generic Modules/Payment",
     "development_status": "Beta",
     "author": "Odoo Community Association (OCA)",
     "summary": "List of open credits and debits, with due date",
     "website": "https://github.com/OCA/account-payment",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": ["account"],
     "data": ["views/payment_view.xml"],
     "pre_init_hook": "pre_init_hook",
