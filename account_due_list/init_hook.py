aba18b287ab1e74153c89671dc5399cbb8ce44eb
mreficent <miquel.raich@eficent.com>
2020-01-29 12:04:57 +0100

    [MIG] account_due_list: Migration to 13.0

--- account_due_list/init_hook.py
+++ account_due_list/init_hook.py
@@ -20,37 +20,7 @@ def pre_init_hook(cr):
 
     The post init script sets the value of maturity_residual.
     """
-    store_field_stored_invoice_id(cr)
     store_field_invoice_user_id(cr)
-
-
-def store_field_stored_invoice_id(cr):
-
-    cr.execute(
-        """SELECT column_name
-    FROM information_schema.columns
-    WHERE table_name='account_move_line' AND
-    column_name='stored_invoice_id'"""
-    )
-    if not cr.fetchone():
-        cr.execute(
-            """
-            ALTER TABLE account_move_line ADD COLUMN stored_invoice_id integer;
-            COMMENT ON COLUMN account_move_line.stored_invoice_id IS 'Invoice';
-            """
-        )
-
-    logger.info("Computing field stored_invoice_id on account.move.line")
-
-    cr.execute(
-        """
-        UPDATE account_move_line aml
-        SET stored_invoice_id = inv.id
-        FROM account_move AS am, account_invoice AS inv
-        WHERE am.id = aml.move_id
-        AND am.id = inv.move_id
-        """
-    )
 
 
 def store_field_invoice_user_id(cr):
@@ -75,8 +45,8 @@ def store_field_invoice_user_id(cr):
     cr.execute(
         """
         UPDATE account_move_line aml
-        SET invoice_user_id = inv.user_id
-        FROM account_invoice AS inv
-        WHERE aml.stored_invoice_id = inv.id
+        SET invoice_user_id = am.invoice_user_id
+        FROM account_move AS am
+        WHERE aml.move_id = am.id
         """
     )
