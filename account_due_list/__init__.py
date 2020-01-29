7450f76567cda7ae0cd88a9ff46a6a5277d07fa8
mreficent <miquel.raich@eficent.com>
2020-01-29 12:04:57 +0100

    [MIG] account_due_list: Migration to 13.0

--- account_due_list/__init__.py
+++ account_due_list/__init__.py
@@ -1,4 +1,4 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import models
 from .init_hook import pre_init_hook
