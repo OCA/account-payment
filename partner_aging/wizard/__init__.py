2cf3024b023ebd6412ce590f755f281a149e0be4
mreficent <miquel.raich@eficent.com>
2020-01-29 11:04:01 +0100

    [IMP] account_due_list: black, isort

--- partner_aging/wizard/__init__.py
+++ partner_aging/wizard/__init__.py
@@ -1,5 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import res_partner_aging_customer
-from . import res_partner_aging_supplier
-from . import res_partner_aging_date
+from . import (
+    res_partner_aging_customer,
+    res_partner_aging_date,
+    res_partner_aging_supplier,
+)
