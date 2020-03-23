13bab95a05a843e87d6caf86a1531a6faa24f7ce
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-03-23 12:44:46 -0400

    [IMP] account_payment_return: black, isort

--- account_payment_return/models/account_journal.py
+++ account_payment_return/models/account_journal.py
@@ -8,16 +8,20 @@ class AccountJournal(models.Model):
     _inherit = "account.journal"
 
     default_expense_account_id = fields.Many2one(
-        comodel_name='account.account', string='Default Charges Account',
-        help='Default account for bank charges')
+        comodel_name="account.account",
+        string="Default Charges Account",
+        help="Default account for bank charges",
+    )
     default_expense_partner_id = fields.Many2one(
-        comodel_name="res.partner", string="Default Charges Partner",
-        domain=[('supplier', '=', True)], help='Default partner for '
-                                               'charge expenses')
+        comodel_name="res.partner",
+        string="Default Charges Partner",
+        domain=[("supplier", "=", True)],
+        help="Default partner for " "charge expenses",
+    )
     return_auto_reconcile = fields.Boolean(
         string="Reconcile payment returns",
         help="Enable automatic payment return reconciliation. This option "
-             "is meant to be used only when working when transfer accounts, "
-             "not if working directly with bank accounts.",
+        "is meant to be used only when working when transfer accounts, "
+        "not if working directly with bank accounts.",
         default=False,
     )
