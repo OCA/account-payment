a663f1d3e0cf36aea52b4611334eb57942a360c8
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-03-23 12:44:46 -0400

    [IMP] account_payment_return: black, isort

--- account_payment_return/models/account_move.py
+++ account_payment_return/models/account_move.py
@@ -5,22 +5,22 @@ from odoo import fields, models
 
 
 class AccountPartialReconcile(models.Model):
-    _inherit = 'account.partial.reconcile'
+    _inherit = "account.partial.reconcile"
 
     origin_returned_move_ids = fields.Many2many(
-        comodel_name='account.move.line',
-        relation='account_partial_reconcile_account_move_line_rel',
-        column1='partial_reconcile_id',
-        column2='move_line_id',
+        comodel_name="account.move.line",
+        relation="account_partial_reconcile_account_move_line_rel",
+        column1="partial_reconcile_id",
+        column2="move_line_id",
     )
 
 
 class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
     partial_reconcile_returned_ids = fields.Many2many(
-        comodel_name='account.partial.reconcile',
-        relation='account_partial_reconcile_account_move_line_rel',
-        column1='move_line_id',
-        column2='partial_reconcile_id',
+        comodel_name="account.partial.reconcile",
+        relation="account_partial_reconcile_account_move_line_rel",
+        column1="move_line_id",
+        column2="partial_reconcile_id",
     )
