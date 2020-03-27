01bc4a16136fd347d401f3934f25ac9dd8687cb9
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-03-27 11:44:22 -0400

    [MIG] account_payment_return_import: Migration to 13.0
    [UPD] Update account_payment_return_import.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-payment-13.0/account-payment-13.0-account_payment_return_import
    Translate-URL: https://translation.odoo-community.org/projects/account-payment-13-0/account-payment-13-0-account_payment_return_import/
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-payment-13.0/account-payment-13.0-account_payment_return_import
    Translate-URL: https://translation.odoo-community.org/projects/account-payment-13-0/account-payment-13-0-account_payment_return_import/

--- account_payment_return_import/__manifest__.py
+++ account_payment_return_import/__manifest__.py
@@ -6,15 +6,14 @@
 {
     "name": "Account Payment Return Import",
     "category": "Accounting",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "summary": "This module adds a generic wizard to import payment return"
     "file formats. Is only the base to be extended by another"
     "modules",
     "license": "AGPL-3",
-    "author": "Odoo Community Association (OCA)," "Tecnativa",
-    "website": "https://github.com/OCA/account-payment/tree/12.0/"
-    "account_payment_return_import",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-payment",
     "depends": ["account_payment_return"],
-    "data": ["views/payment_return_view.xml", "wizard/payment_return_import_view.xml",],
+    "data": ["views/payment_return_view.xml", "wizard/payment_return_import_view.xml"],
     "installable": True,
 }
