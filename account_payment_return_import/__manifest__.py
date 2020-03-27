14f2d80b43d03c5acb5750feae5222795a3579e3
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-03-27 11:42:31 -0400

    [IMP] account_payment_return_import: black, isort

--- account_payment_return_import/__manifest__.py
+++ account_payment_return_import/__manifest__.py
@@ -4,21 +4,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Payment Return Import',
-    'category': 'Accounting',
-    'version': '12.0.1.0.0',
-    'summary': 'This module adds a generic wizard to import payment return'
-               'file formats. Is only the base to be extended by another'
-               'modules',
-    'license': 'AGPL-3',
-    'author': 'Odoo Community Association (OCA),'
-              'Tecnativa',
-    'website': 'https://github.com/OCA/account-payment/tree/12.0/'
-               'account_payment_return_import',
-    'depends': ['account_payment_return'],
-    'data': [
-        'views/payment_return_view.xml',
-        'wizard/payment_return_import_view.xml',
-    ],
-    'installable': True,
+    "name": "Account Payment Return Import",
+    "category": "Accounting",
+    "version": "12.0.1.0.0",
+    "summary": "This module adds a generic wizard to import payment return"
+    "file formats. Is only the base to be extended by another"
+    "modules",
+    "license": "AGPL-3",
+    "author": "Odoo Community Association (OCA)," "Tecnativa",
+    "website": "https://github.com/OCA/account-payment/tree/12.0/"
+    "account_payment_return_import",
+    "depends": ["account_payment_return"],
+    "data": ["views/payment_return_view.xml", "wizard/payment_return_import_view.xml",],
+    "installable": True,
 }
--- account_payment_return_import/models/payment_return_line.py
+++ account_payment_return_import/models/payment_return_line.py
@@ -6,16 +6,19 @@ from odoo import fields, models
 
 
 class PaymentReturnLine(models.Model):
-    _inherit = 'payment.return.line'
+    _inherit = "payment.return.line"
 
     # Ensure transactions can be imported only once (if the import format
     # provides unique transaction ids)
-    unique_import_id = fields.Char('Import ID', readonly=True, copy=False)
+    unique_import_id = fields.Char("Import ID", readonly=True, copy=False)
     raw_import_data = fields.Char(
-        help="XML RAW data stored for debugging/check purposes")
+        help="XML RAW data stored for debugging/check purposes"
+    )
 
     _sql_constraints = [
-        ('unique_import_id',
-         'unique (unique_import_id)',
-         'A payment return transaction can be imported only once!')
+        (
+            "unique_import_id",
+            "unique (unique_import_id)",
+            "A payment return transaction can be imported only once!",
+        )
     ]
