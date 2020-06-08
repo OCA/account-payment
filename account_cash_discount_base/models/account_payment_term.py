90a4de54f77362ab1e7c15f315098951c8a1d2d4
Benjamin Willig <benjamin.willig@acsone.eu>
2020-06-08 11:29:18 +0200

    [WIP] account_cash_discount_base/write_off/payment migration

--- account_cash_discount_base/models/account_payment_term.py
+++ account_cash_discount_base/models/account_payment_term.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
-from odoo.addons import decimal_precision as dp
 
 
 class AccountPaymentTerm(models.Model):
@@ -12,7 +10,7 @@ class AccountPaymentTerm(models.Model):
 
     discount_percent = fields.Float(
         string='Discount (%)',
-        digits=dp.get_precision('Discount'),
+        digits='Discount',
     )
     discount_delay = fields.Integer(
         string='Discount Delay (days)'
