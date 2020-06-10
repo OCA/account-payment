bd793c9620ef08b03b3e59e18d81a4c7cd688002
Benjamin Willig <benjamin.willig@acsone.eu>
2020-06-10 11:17:27 +0200

    [FIX] pre-commit

--- account_cash_discount_payment/tests/common.py
+++ account_cash_discount_payment/tests/common.py
@@ -3,39 +3,45 @@
 
 from odoo.tests.common import Form
 
-from odoo.addons.account_cash_discount_base.tests.common import \
-    TestAccountCashDiscountCommon
+from odoo.addons.account_cash_discount_base.tests.common import (
+    TestAccountCashDiscountCommon,
+)
 
 
 class TestAccountCashDiscountPaymentCommon(TestAccountCashDiscountCommon):
-
     @classmethod
     def setUpClass(cls):
         super(TestAccountCashDiscountPaymentCommon, cls).setUpClass()
-        cls.PaymentLineCreate = cls.env['account.payment.line.create']
-        cls.PaymentOrder = cls.env['account.payment.order']
+        cls.PaymentLineCreate = cls.env["account.payment.line.create"]
+        cls.PaymentOrder = cls.env["account.payment.order"]
 
         cls.payment_mode_out = cls.env.ref(
-            'account_payment_mode.payment_mode_outbound_ct1')
-        cls.bank_ing = cls.env.ref('base.bank_ing')
-        cls.partner_bank_ing = cls.env.ref('base_iban.bank_iban_main_partner')
+            "account_payment_mode.payment_mode_outbound_ct1"
+        )
+        cls.bank_ing = cls.env.ref("base.bank_ing")
+        cls.partner_bank_ing = cls.env.ref("base_iban.bank_iban_main_partner")
 
-        cls.bank_ing_journal = cls.Journal.create({
-            'name': 'ING Bank',
-            'code': 'ING-B',
-            'type': 'bank',
-            'bank_id': cls.bank_ing.id,
-        })
+        cls.bank_ing_journal = cls.Journal.create(
+            {
+                "name": "ING Bank",
+                "code": "ING-B",
+                "type": "bank",
+                "bank_id": cls.bank_ing.id,
+            }
+        )
 
     def create_supplier_invoice(
-            self, date, payment_mode, amount, discount_percent, taxes):
+        self, date, payment_mode, amount, discount_percent, taxes
+    ):
 
-        invoice_form = Form(self.AccountMove.with_context(
-            default_type='in_invoice',
-            default_company_id=self.company.id,
-            default_journal_id=self.purchase_journal.id,
-            default_payment_mode_id=payment_mode.id,
-        ))
+        invoice_form = Form(
+            self.AccountMove.with_context(
+                default_type="in_invoice",
+                default_company_id=self.company.id,
+                default_journal_id=self.purchase_journal.id,
+                default_payment_mode_id=payment_mode.id,
+            )
+        )
         invoice_form.partner_id = self.partner_agrolait
         invoice_form.invoice_date = date
         invoice_form.discount_due_date = date
