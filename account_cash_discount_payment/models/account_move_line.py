df80bb1b3d0d536499350654c207af22bd09dfba
Benjamin Willig <benjamin.willig@acsone.eu>
2020-06-10 09:34:53 +0200

    [MIG] account_cash_discount_payment to v13

--- account_cash_discount_payment/models/account_move_line.py
+++ account_cash_discount_payment/models/account_move_line.py
@@ -1,7 +1,7 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, fields
+from odoo import models, fields
 
 
 class AccountMoveLine(models.Model):
@@ -9,24 +9,23 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     discount_due_date = fields.Date(
-        related='invoice_id.discount_due_date',
+        related='move_id.discount_due_date',
         readonly=True,
     )
     discount_amount = fields.Monetary(
-        related='invoice_id.discount_amount',
+        related='move_id.discount_amount',
         readonly=True,
     )
 
-    @api.multi
     def _prepare_payment_line_vals(self, payment_order):
         self.ensure_one()
         values = super(AccountMoveLine, self)._prepare_payment_line_vals(
             payment_order)
 
-        invoice = self.invoice_id
-        if invoice and invoice.discount_due_date and invoice.has_discount:
-            pay_with_discount = invoice._can_pay_invoice_with_discount()
+        move = self.move_id
+        if move and move.discount_due_date and move.has_discount:
+            pay_with_discount = move._can_pay_invoice_with_discount()
             values['pay_with_discount'] = pay_with_discount
             if pay_with_discount:
-                values['amount_currency'] = invoice.residual_with_discount
+                values['amount_currency'] = move.residual_with_discount
         return values
