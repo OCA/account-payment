2a80c4cd88f38e6b4861fb9377aa4dc339333d5c
ps-tubtim <pimolnats@ecosoft.co.th>
2020-06-12 10:06:24 +0700

    [13.0][ADD] account_check_date

---
+++ account_check_date/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Check Date",
+    "version": "13.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-payment",
+    "license": "AGPL-3",
+    "category": "Accounting & Finance",
+    "summary": "Add check date on payment for check printing",
+    "depends": ["account_check_printing"],
+    "data": ["views/account_payment_views.xml"],
+    "installable": True,
+}
