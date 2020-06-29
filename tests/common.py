f0e6ed985a7ffc7ab75a0c5137869ccd70bb5273
Florent Cayré <florent@commown.fr>
2020-06-29 19:31:10 +0200

    [tests] Factor out a test helper class to ease writing test with slimpay payments

---
+++ tests/common.py
@@ -0,0 +1,39 @@
+from mock import patch
+
+from odoo.addons.payment_slimpay.models.payment import SlimpayClient
+
+
+def _get_from_doc_mock(doc, method_name):
+    """ Dummy mock for SimplayClient.get_from_doc that returns a hard-coded
+    hal document for the requested method name, whatever the specified doc.
+    """
+    return {'get-mandate': {'id': 'my-mandate-id'},
+            'get-bank-account': {'institutionName': 'my-bank',
+                                 'iban': 'my-iban'},
+            }[method_name]
+
+
+class MockedSlimpayMixin(object):
+
+    def setup_mocks(self):
+        self._patchers = []
+        # Mock SlimpayClient
+        self._start_patcher(
+            patch('odoo.addons.payment_slimpay.models.'
+                  'slimpay_utils.get_client'))
+        # Mock its "get" and "get_from_doc" methods (to ease their config)
+        self.fake_get = self._start_patcher(patch.object(SlimpayClient, 'get'))
+        self._start_patcher(patch.object(
+            SlimpayClient, 'get_from_doc', side_effect=_get_from_doc_mock))
+        # Mock approval_url
+        self._start_patcher(patch.object(
+            SlimpayClient, 'approval_url',
+            side_effect=lambda tx_ref, *args, **kw: tx_ref))
+        self.slimpay = self.env.ref('payment.payment_acquirer_slimpay')
+        # Stop patchers in case of a test exception or normal termination
+        for patcher in self._patchers:
+            self.addCleanup(patcher.stop)
+
+    def _start_patcher(self, patcher):
+        self._patchers.append(patcher)
+        return patcher.start()
--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -3,50 +3,18 @@ import urllib2
 import json
 from datetime import datetime
 
-from mock import patch
-
-from odoo.addons.payment_slimpay.models.payment import SlimpayClient
-
 from odoo.tests.common import HttpCase, at_install, post_install, HOST, PORT
 
-
-def _get_from_doc_mock(doc, method_name):
-    """ Dummy mock for SimplayClient.get_from_doc that returns a hard-coded
-    hal document for the requested method name, whatever the specified doc.
-    """
-    return {'get-mandate': {'id': 'my-mandate-id'},
-            'get-bank-account': {'institutionName': 'my-bank',
-                                 'iban': 'my-iban'},
-            }[method_name]
+from .common import MockedSlimpayMixin
 
 
 @at_install(False)
 @post_install(True)
-class SlimpayControllersTC(HttpCase):
+class SlimpayControllersTC(HttpCase, MockedSlimpayMixin):
 
     def setUp(self):
-        self._patchers = []
-        # Mock SlimpayClient
-        self._start_patcher(
-            patch('odoo.addons.payment_slimpay.models.'
-                  'slimpay_utils.get_client'))
-        # Mock its "get" and "get_from_doc" methods (to ease their config)
-        self.fake_get = self._start_patcher(patch.object(SlimpayClient, 'get'))
-        self._start_patcher(patch.object(
-            SlimpayClient, 'get_from_doc', side_effect=_get_from_doc_mock))
-        # Mock approval_url
-        self._start_patcher(patch.object(
-            SlimpayClient, 'approval_url',
-            side_effect=lambda tx_ref, *args, **kw: tx_ref))
         super(SlimpayControllersTC, self).setUp()
-        # Stop patchers in case of a test exception or normal termination
-        for patcher in self._patchers:
-            self.addCleanup(patcher.stop)
-        self.slimpay = self.env.ref('payment.payment_acquirer_slimpay')
-
-    def _start_patcher(self, patcher):
-        self._patchers.append(patcher)
-        return patcher.start()
+        self.setup_mocks()
 
     def post(self, path, data=None, encoder=urlencode,
              headers=None, timeout=30, assert_code=200):
