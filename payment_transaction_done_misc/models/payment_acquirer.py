88ad4b4be9f41696262e6ec6d5a294af197f8bfb
victoralmau <victoralmau@gmail.com>
2020-07-15 11:12:22 +0200

    travis errors

--- payment_transaction_done_misc/models/payment_acquirer.py
+++ payment_transaction_done_misc/models/payment_acquirer.py
@@ -1,11 +1,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, fields
+from odoo import models, fields
 
 
 class PaymentAcquirer(models.Model):
     _inherit = 'payment.acquirer'
-    
+
     done_sale_order_customer_mail_template_id = fields.Many2one(
         comodel_name='mail.template',
         domain=[('model_id.model', '=', 'payment.transaction')],
--- payment_transaction_done_misc/models/payment_transaction.py
+++ payment_transaction_done_misc/models/payment_transaction.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, fields
+from odoo import api, models
 
 
 class PaymentTransaction(models.Model):
@@ -9,28 +9,28 @@ class PaymentTransaction(models.Model):
     @api.multi
     def write(self, vals):
         self.ensure_one()
-        #state_done_now
+        # state_done_now
         state_done_now = False
         if 'state' in vals:
-            if vals['state']=='done' and self.state!='done':
-                if self.sale_order_id.id>0:
+            if vals['state'] == 'done' and self.state != 'done':
+                if self.sale_order_id.id > 0:
                     state_done_now = True
-        #write
+        # write
         return_object = super(PaymentTransaction, self).write(vals)
-        #operations
-        if state_done_now==True:
+        # operations
+        if state_done_now:
             #done_sale_order_customer_mail_template_id
-            if self.acquirer_id.done_sale_order_customer_mail_template_id.id>0:
+            if self.acquirer_id.done_sale_order_customer_mail_template_id.id > 0:
                 for sale_order_id in self.sale_order_ids:
-                    #send_mail
-                    if sale_order_id.user_id.id>0:
+                    # send_mail
+                    if sale_order_id.user_id.id > 0:
                         mail_compose_message_obj = self.env['mail.compose.message'].sudo(sale_order_id.user_id.id).create({})
                     else:
                         mail_compose_message_obj = self.env['mail.compose.message'].sudo().create({})
-                    #onchange_template_id
+                    # onchange_template_id
                     return_mail_compose_message_obj = mail_compose_message_obj.onchange_template_id(self.acquirer_id.done_sale_order_customer_mail_template_id.id, 'comment', 'payment.transaction', self.id)
                     mail_body = return_mail_compose_message_obj['value']['body']
-                    #update
+                    # update
                     mail_compose_message_obj.composition_mode = 'comment'
                     mail_compose_message_obj.model = 'sale.order'
                     mail_compose_message_obj.res_id = sale_order_id.id
@@ -38,9 +38,9 @@ class PaymentTransaction(models.Model):
                     mail_compose_message_obj.template_id = self.acquirer_id.done_sale_order_customer_mail_template_id.id
                     mail_compose_message_obj.body = mail_body
                     mail_compose_message_obj.subject = return_mail_compose_message_obj['value']['subject']
-                    #send_mail_action
+                    # send_mail_action
                     mail_compose_message_obj.send_mail_action()
-            #done_sale_order_user_id_mail_template_id
+            # done_sale_order_user_id_mail_template_id
             if self.acquirer_id.done_sale_order_user_id_mail_template_id.id > 0:
                 if self.sale_order_id.id > 0:
                     mail_compose_message_obj = self.env['mail.compose.message'].sudo().create({})
@@ -67,10 +67,10 @@ class PaymentTransaction(models.Model):
                         mail_message_obj = self.env['mail.message'].sudo(self.sale_order_id.user_id.id).create(
                             mail_message_vals)
                     else:
-                        mail_message_obj = self.env['mail.message'].sudo().create(mail_message_vals)
-            #done_account_journal_id_account_payment
-            if self.acquirer_id.done_account_journal_id_account_payment.id>0:
-                #vals
+                        self.env['mail.message'].sudo().create(mail_message_vals)
+            # done_account_journal_id_account_payment
+            if self.acquirer_id.done_account_journal_id_account_payment.id > 0:
+                # vals
                 account_payment_vals = {
                     'payment_type': 'inbound',
                     'partner_type': 'customer',
@@ -83,9 +83,9 @@ class PaymentTransaction(models.Model):
                     'payment_method_id': self.acquirer_id.done_account_journal_id_account_payment_method.id,
                     'payment_transaction_id': self.id
                 }
-                #create
+                # create
                 account_payment_obj = self.env['account.payment'].sudo().create(account_payment_vals)
-                #post
+                # post
                 account_payment_obj.post()
-        #return
+        # return
         return return_object
