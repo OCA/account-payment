e1e585312abfcf29ce77ec131363cdf43db1c1e1
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-07-16 19:00:31 +0200

    [FIX] account_cash_invoice : bad domain in xml view generate error in many journals context

--- account_cash_invoice/__manifest__.py
+++ account_cash_invoice/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Account cash invoice',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'category': 'Accounting',
     'author': 'Creu Blanca,'
               'Odoo Community Association (OCA)',
