3987f6cff452dd58716a8f79f2b1148f10a9ca7d
victoralmau <victoralmau@gmail.com>
2020-07-27 10:26:21 +0200

    [12.0][Add] Automate some actions relate to payment transaction done
    It allows you to automate some things just when a transaction is 'Done':
    
    Send email to the client
    Create internal note in the linked order highlighting the message to the user_id
    Create a payment in the corresponding journal

---
+++ payment_transaction_done_misc/models/payment_acquirer.py
@@ -0,0 +1,32 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class PaymentAcquirer(models.Model):
+    _inherit = 'payment.acquirer'
+
+    done_so_customer_template_id = fields.Many2one(
+        comodel_name='mail.template',
+        domain=[('model_id.model', '=', 'payment.transaction')],
+        string='Mail template from customer',
+        help='Email that will be sent to the customer'
+             ' when the transaction is completed and linked to a sales order'
+    )
+    done_so_user_id_template_id = fields.Many2one(
+        comodel_name='mail.template',
+        domain=[('model_id.model', '=', 'payment.transaction')],
+        string='Mail template from user_id',
+        help='Create an internal note related to user_id from sale_order'
+    )
+    done_account_journal_id = fields.Many2one(
+        comodel_name='account.journal',
+        string='Account journal',
+        help='Account journal used to create payments from transactions done'
+    )
+    done_payment_method_id = fields.Many2one(
+        comodel_name='account.payment.method',
+        domain=[('payment_type', '=', 'inbound')],
+        string='Payment method',
+        help='Payment method used in payment'
+    )
---
+++ payment_transaction_done_misc/models/payment_transaction.py
@@ -0,0 +1,101 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class PaymentTransaction(models.Model):
+    _inherit = 'payment.transaction'
+
+    @api.multi
+    def write(self, vals):
+        self.ensure_one()
+        # state_done_now
+        state_done_now = False
+        if 'state' in vals:
+            if vals['state'] == 'done' and self.state != 'done':
+                if self.sale_order_id:
+                    state_done_now = True
+        # write
+        return_object = super(PaymentTransaction, self).write(vals)
+        # operations
+        if state_done_now:
+            # done_so_customer_template_id
+            if self.acquirer_id.done_so_customer_template_id:
+                for sale_order_id in self.sale_order_ids:
+                    # send_mail
+                    if sale_order_id.user_id:
+                        mcm_obj = self.env['mail.compose.message'].sudo(
+                            sale_order_id.user_id.id
+                        ).create({})
+                    else:
+                        mcm_obj = self.env['mail.compose.message'].sudo().create({})
+                    # onchange_template_id
+                    res = mcm_obj.onchange_template_id(
+                        self.acquirer_id.done_so_customer_template_id.id,
+                        'comment',
+                        'payment.transaction',
+                        self.id
+                    )
+                    mail_body = res['value']['body']
+                    # update
+                    mcm_obj.composition_mode = 'comment'
+                    mcm_obj.model = 'sale.order'
+                    mcm_obj.res_id = sale_order_id.id
+                    mcm_obj.record_name = sale_order_id.name
+                    mcm_obj.template_id = \
+                        self.acquirer_id.done_so_customer_template_id.id
+                    mcm_obj.body = mail_body
+                    mcm_obj.subject = res['value']['subject']
+                    # send_mail_action
+                    mcm_obj.send_mail_action()
+            # done_so_user_id_template_id
+            if self.acquirer_id.done_so_user_id_template_id:
+                if self.sale_order_id:
+                    mcm_obj = self.env['mail.compose.message'].sudo().create({})
+                    # onchange_template_id
+                    res = mcm_obj.onchange_template_id(
+                        self.acquirer_id.done_so_user_id_template_id.id,
+                        'comment',
+                        'payment.transaction', self.id)
+                    mail_body = res['value']['body']
+                    # create
+                    vals = {
+                        'subtype_id': 2,
+                        'message_type': 'notification',
+                        'body': mail_body,
+                        'model': 'sale.order',
+                        'res_id': self.sale_order_id.id,
+                        'record_name': self.sale_order_id.name
+                    }
+                    # add_auto_starred
+                    if self.sale_order_id.user_id:
+                        vals['starred_partner_ids'] = \
+                            [[6, False, [self.sale_order_id.user_id.partner_id.id]]]
+                    # create
+                    if self.sale_order_id.user_id:
+                        self.env['mail.message'].sudo(
+                            self.sale_order_id.user_id.id
+                        ).create(vals)
+                    else:
+                        self.env['mail.message'].sudo().create(vals)
+            # done_payment_method_id
+            if self.acquirer_id.done_payment_method_id:
+                # vals
+                vals = {
+                    'payment_type': 'inbound',
+                    'partner_type': 'customer',
+                    'partner_id': self.partner_id.id,
+                    'journal_id': self.acquirer_id.done_account_journal_id.id,
+                    'amount': self.amount,
+                    'currency_id': self.currency_id.id,
+                    'payment_date': self.date_validate,
+                    'communication': self.reference,
+                    'payment_method_id': self.acquirer_id.done_payment_method_id.id,
+                    'payment_transaction_id': self.id
+                }
+                # create
+                account_payment_obj = self.env['account.payment'].sudo().create(vals)
+                # post
+                account_payment_obj.post()
+        # return
+        return return_object
