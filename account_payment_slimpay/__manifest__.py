ad5dc2883b35207da43453ccb37d90f160963b24
Florent Cayré <florent@commown.fr>
2020-08-31 18:53:39 +0200

    Improve manifest file using the dedicated OCA template

--- account_payment_slimpay/__manifest__.py
+++ account_payment_slimpay/__manifest__.py
@@ -1,20 +1,28 @@
+# Copyright 2020 Commown SCIC SAS (https://commown.coop)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Slimpay Payment Acquirer',
-    'category': 'Accounting',
-    'summary': 'Payment Acquirer: Slimpay Implementation',
-    'version': '12.0.1.0.0',
-    'description': """Slimpay Payment Acquirer""",
-    'author': "Commown SCIC SAS",
-    'license': "AGPL-3",
-    'website': "https://commown.coop",
-    'depends': ['payment', 'partner_firstname', 'base_phone'],
-    'external_dependencies': {
-        'python': ['hal_codec', 'iso8601', 'requests', 'phonenumbers']
+    "name": "Slimpay Payment base",
+    "summary": "Provides server to server implementation of Slimpay payment",
+    "version": "12.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/account-payment",
+    "author": "Commown SCIC SAS, Odoo Community Association (OCA)",
+    "maintainers": ["fcayre"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": ["hal_codec", "iso8601", "requests", "phonenumbers"]
     },
-    'data': [
-        'views/payment_views.xml',
-        'views/payment_slimpay_templates.xml',
-        'data/payment_acquirer_data.xml',
+    "depends": [
+        "payment",
+        "partner_firstname",
+        "base_phone",
     ],
-    'installable': True,
+    "data": [
+        "views/payment_views.xml",
+        "views/payment_slimpay_templates.xml",
+        "data/payment_acquirer_data.xml",
+    ],
 }
