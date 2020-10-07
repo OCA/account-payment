9a00d7eaf8e5087dc4ea027e822dc7aa68954217
Carlos Roca <carlos.roca@tecnativa.com>
2020-10-07 08:57:16 +0200

    [MIG] account_check_printing_report_base: Migration to v13

--- account_check_printing_report_base/__manifest__.py
+++ account_check_printing_report_base/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Account Check Printing Report Base",
-    "version": "12.0.1.2.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Eficent,"
     "Serpent Consulting Services Pvt. Ltd.,"
