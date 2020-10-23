991ad288b7dbcb71204d44612f33072be18970ac
Carlos Roca <carlos.roca@tecnativa.com>
2020-10-23 12:04:56 +0200

    [FIX+IMP] account_payment_promissory_note: Bug on payments + Unnecesary inheritance Fixed a bug that causes the expiration date not to be defined by default
    Eliminated the inheritance on 'account.payment' and 'account.register.payment'
    of 'account.abstract.payment' because the main models deppend on it yet.

--- account_payment_promissory_note/__manifest__.py
+++ account_payment_promissory_note/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Account Payment Promissory Note',
-    'version': '12.0.1.0.0',
+    'version': '12.0.2.0.0',
     'license': 'AGPL-3',
     'author': "Tecnativa,"
               "Odoo Community Association (OCA)",
