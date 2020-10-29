96ef142ce21ef534400e6002dc7e1bcb1bb4c171
Carlos Roca <carlos.roca@tecnativa.com>
2020-10-29 09:48:36 +0100

    [IMP] account_payment_promissory_note: black, isort, prettier

--- account_payment_promissory_note/wizard/account_register_payments.py
+++ account_payment_promissory_note/wizard/account_register_payments.py
@@ -6,24 +6,23 @@ from odoo import api, models
 
 
 class AccountRegisterPayments(models.TransientModel):
-    _inherit = 'account.register.payments'
+    _inherit = "account.register.payments"
 
     def get_payments_vals(self):
         vals = super(AccountRegisterPayments, self).get_payments_vals()
         for val in vals:
-            val.update({
-                'promissory_note': self.promissory_note,
-                'date_due': self.date_due,
-            })
+            val.update(
+                {"promissory_note": self.promissory_note, "date_due": self.date_due,}
+            )
         return vals
 
-    @api.onchange('promissory_note')
+    @api.onchange("promissory_note")
     def _onchange_promissory_note(self):
         super()._onchange_promissory_note()
         if not self.date_due and self.promissory_note:
             invoices = False
-            if self._name == 'account.register.payments':
-                active_ids = self._context.get('active_ids')
-                invoices = self.env['account.invoice'].browse(active_ids)
+            if self._name == "account.register.payments":
+                active_ids = self._context.get("active_ids")
+                invoices = self.env["account.invoice"].browse(active_ids)
             if invoices:
-                self.date_due = max(invoices.mapped('date_due'))
+                self.date_due = max(invoices.mapped("date_due"))
