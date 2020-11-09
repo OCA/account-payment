fd5eed9e400fab0eacd13d14381418525b04f429
Tran Thanh Phuc <phuc@trobz.com>
2020-11-09 23:04:00 +0700

    [IMP] account_early_payment_discount: black, isort, prettier

--- account_early_payment_discount/tests/test_payment_term.py
+++ account_early_payment_discount/tests/test_payment_term.py
@@ -8,34 +8,36 @@ from odoo.tools.misc import mute_logger
 
 
 class TestPaymentTerm(TransactionCase):
-
-    @mute_logger('odoo.sql_db')
+    @mute_logger("odoo.sql_db")
     def test_constraint__failed(self):
         # Logs are muted to prevent the expected integrity error from being
         # reported in the logs, which would cause a failed test status.
         with self.assertRaises(IntegrityError):
-            self.env['account.payment.term'].create({
-                'name': 'Unittest payment term',
-                'early_payment_discount': True,
-            })
+            self.env["account.payment.term"].create(
+                {"name": "Unittest payment term", "early_payment_discount": True,}
+            )
 
     def test_constraint__good(self):
-        payment_term = self.env['account.payment.term'].create({
-            'name': 'Unittest payment term',
-            'early_payment_discount': True,
-            'epd_nb_days': 5,
-            'epd_discount': 2
-        })
+        payment_term = self.env["account.payment.term"].create(
+            {
+                "name": "Unittest payment term",
+                "early_payment_discount": True,
+                "epd_nb_days": 5,
+                "epd_discount": 2,
+            }
+        )
         self.assertTrue(payment_term.early_payment_discount)
 
     def test_onchange_early_payment_discount(self):
-        payment_term = self.env['account.payment.term'].create({
-            'name': 'Unittest payment term',
-            'early_payment_discount': True,
-            'epd_nb_days': 5,
-            'epd_discount': 2,
-            'epd_tolerance': 0.05,
-        })
+        payment_term = self.env["account.payment.term"].create(
+            {
+                "name": "Unittest payment term",
+                "early_payment_discount": True,
+                "epd_nb_days": 5,
+                "epd_discount": 2,
+                "epd_tolerance": 0.05,
+            }
+        )
 
         payment_term.early_payment_discount = False
         payment_term._onchange_early_payment_discount()
