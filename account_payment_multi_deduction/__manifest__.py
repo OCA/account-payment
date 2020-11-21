0ad7a7afdd96d37b9dac67f4a92019da049d4bb5
Saran440 <saranl@ecosoft.co.th>
2020-11-21 21:43:42 +0700

    [14.0][MIG] account_payment_multi_deduction

--- account_payment_multi_deduction/__manifest__.py
+++ account_payment_multi_deduction/__manifest__.py
@@ -3,13 +3,16 @@
 
 {
     "name": "Payment Register with Multiple Deduction",
-    "version": "13.0.1.1.0",
-    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/account-payment",
     "category": "Accounting",
     "depends": ["account"],
-    "data": ["security/ir.model.access.csv", "views/account_payment_view.xml"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/account_payment_register_views.xml",
+    ],
     "installable": True,
     "development_status": "Alpha",
     "maintainers": ["kittiu"],
