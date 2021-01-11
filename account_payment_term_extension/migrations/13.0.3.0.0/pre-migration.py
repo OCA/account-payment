6a8278f576d3dfd9b131e7bce6dd445b5c82a060
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-01-11 19:50:48 +0100

    [OU] account_payment_term_extension: Don't execute migration script (2)
    Trying an invalid query wastes the cursor and the update process
    doesn't continue, so we can't use such approach. Instead, we check the
    column existence.

--- account_payment_term_extension/migrations/13.0.3.0.0/pre-migration.py
+++ account_payment_term_extension/migrations/13.0.3.0.0/pre-migration.py
@@ -3,16 +3,21 @@
 
 
 def migrate(cr, version):
-    try:
-        cr.execute(
-            """UPDATE account_payment_term_line
-            SET value_amount = value_amount_untaxed
-            WHERE value = 'amount_untaxed'"""
-        )
-        cr.execute(
-            """UPDATE account_payment_term_line
-            SET value = 'percent_amount_untaxed'
-            WHERE value = 'amount_untaxed'"""
-        )
-    except Exception:
-        pass  # no previous version was installed
+    cr.execute(
+        """SELECT count(attname) FROM pg_attribute
+        WHERE attrelid =
+        (SELECT oid FROM pg_class WHERE relname = 'account_payment_term_line')
+        AND attname = 'value_amount_untaxed'""",
+    )
+    if cr.fetchone()[0] < 1:
+        return
+    cr.execute(
+        """UPDATE account_payment_term_line
+        SET value_amount = value_amount_untaxed
+        WHERE value = 'amount_untaxed'"""
+    )
+    cr.execute(
+        """UPDATE account_payment_term_line
+        SET value = 'percent_amount_untaxed'
+        WHERE value = 'amount_untaxed'"""
+    )
