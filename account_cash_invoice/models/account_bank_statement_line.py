b8ca99365f93ab8b736cff0f9aef4b50041e37e9
François Honoré <francois.honore@acsone.eu>
2021-02-05 09:06:49 +0100

    [IMP] account_cash_invoice: black, isort, prettier

--- account_cash_invoice/models/account_bank_statement_line.py
+++ account_cash_invoice/models/account_bank_statement_line.py
@@ -5,30 +5,26 @@ from odoo import api, fields, models
 
 
 class AccountBankStatementLine(models.Model):
-    _inherit = 'account.bank.statement.line'
+    _inherit = "account.bank.statement.line"
 
     invoice_id = fields.Many2one(
-        comodel_name='account.invoice',
-        string='Invoice',
-        readonly=True,
+        comodel_name="account.invoice", string="Invoice", readonly=True,
     )
 
     @api.multi
     def fast_counterpart_creation(self):
         for st_line in self:
             if not st_line.invoice_id:
-                super(
-                    AccountBankStatementLine, st_line
-                ).fast_counterpart_creation()
+                super(AccountBankStatementLine, st_line).fast_counterpart_creation()
             else:
                 invoice = st_line.invoice_id
                 move_line = invoice.move_id.line_ids.filtered(
                     lambda r: r.account_id.id == invoice.account_id.id
                 )
                 vals = {
-                    'name': st_line.name,
-                    'debit': st_line.amount < 0 and -st_line.amount or 0.0,
-                    'credit': st_line.amount > 0 and st_line.amount or 0.0,
-                    'move_line': move_line
+                    "name": st_line.name,
+                    "debit": st_line.amount < 0 and -st_line.amount or 0.0,
+                    "credit": st_line.amount > 0 and st_line.amount or 0.0,
+                    "move_line": move_line,
                 }
                 st_line.process_reconciliation(counterpart_aml_dicts=[vals])
