7b31ec720174722109a35819b7f27f35563069b7
François Honoré <francois.honore@acsone.eu>
2021-02-05 09:06:49 +0100

    [IMP] account_cash_invoice: black, isort, prettier

--- account_cash_invoice/__manifest__.py
+++ account_cash_invoice/__manifest__.py
@@ -2,19 +2,13 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Account cash invoice',
-    'version': '12.0.1.1.0',
-    'category': 'Accounting',
-    'author': 'Creu Blanca,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-payment',
-    'summary': 'Pay and receive invoices from bank statements',
-    'license': 'LGPL-3',
-    'depends': [
-        'account',
-    ],
-    'data': [
-        'wizard/cash_invoice_out.xml',
-        'wizard/cash_invoice_in.xml',
-    ],
+    "name": "Account cash invoice",
+    "version": "12.0.1.1.0",
+    "category": "Accounting",
+    "author": "Creu Blanca," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-payment",
+    "summary": "Pay and receive invoices from bank statements",
+    "license": "LGPL-3",
+    "depends": ["account",],
+    "data": ["wizard/cash_invoice_out.xml", "wizard/cash_invoice_in.xml",],
 }
