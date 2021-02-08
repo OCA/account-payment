8d19e0b502c893ec4878887213d1f2d686344829
François Honoré <francois.honore@acsone.eu>
2021-02-08 10:15:06 +0100

    [13.0][MIG] account_cash_invoice

--- account_cash_invoice/__manifest__.py
+++ account_cash_invoice/__manifest__.py
@@ -3,12 +3,12 @@
 
 {
     "name": "Account cash invoice",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.1.0",
     "category": "Accounting",
     "author": "Creu Blanca," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-payment",
     "summary": "Pay and receive invoices from bank statements",
     "license": "LGPL-3",
-    "depends": ["account",],
-    "data": ["wizard/cash_invoice_out.xml", "wizard/cash_invoice_in.xml",],
+    "depends": ["account"],
+    "data": ["wizard/cash_invoice_out.xml", "wizard/cash_invoice_in.xml"],
 }
