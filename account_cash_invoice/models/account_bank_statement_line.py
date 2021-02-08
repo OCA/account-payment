300d8389b98776ec645ed61b0baa697970a88be7
François Honoré <francois.honore@acsone.eu>
2021-02-08 10:15:06 +0100

    [13.0][MIG] account_cash_invoice
    Co-authored-by: Enric Tobella <etobella@creublanca.es>

--- account_cash_invoice/models/account_bank_statement_line.py
+++ account_cash_invoice/models/account_bank_statement_line.py
@@ -1,25 +1,25 @@
-# Copyright (C) 2017 Creu Blanca
+# Copyright (C) 2017-2021 Creu Blanca
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class AccountBankStatementLine(models.Model):
     _inherit = "account.bank.statement.line"
 
     invoice_id = fields.Many2one(
-        comodel_name="account.invoice", string="Invoice", readonly=True,
+        comodel_name="account.move", string="Invoice", readonly=True,
     )
 
-    @api.multi
     def fast_counterpart_creation(self):
         for st_line in self:
             if not st_line.invoice_id:
                 super(AccountBankStatementLine, st_line).fast_counterpart_creation()
             else:
                 invoice = st_line.invoice_id
-                move_line = invoice.move_id.line_ids.filtered(
-                    lambda r: r.account_id.id == invoice.account_id.id
+                move_line = invoice.line_ids.filtered(
+                    lambda r: r.exclude_from_invoice_tab
+                    and r.account_id.user_type_id.type in ("receivable", "payable")
                 )
                 vals = {
                     "name": st_line.name,
