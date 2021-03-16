0067f25532a87b768bf430ebc047c22620b71307
Manuel Calero <manuelcalerosolis@gmail.com>
2021-03-16 11:13:57 +0100

    [IMP] account_payment_show_invoice: black, isort

--- account_payment_show_invoice/models/account_payment.py
+++ account_payment_show_invoice/models/account_payment.py
@@ -5,15 +5,15 @@ from odoo import api, fields, models
 
 
 class AccountPayment(models.Model):
-    _inherit = 'account.payment'
+    _inherit = "account.payment"
 
     @api.multi
     def _compute_invoice_vendor_references(self):
         for payment in self:
             ref = payment.invoice_ids.mapped(lambda x: x.reference or x.number)
             ref.sort()
-            payment.invoice_vendor_references = ', '.join(ref)
+            payment.invoice_vendor_references = ", ".join(ref)
 
     invoice_vendor_references = fields.Char(
-        string='Ref Invoices',
-        compute=_compute_invoice_vendor_references)
+        string="Ref Invoices", compute=_compute_invoice_vendor_references
+    )
