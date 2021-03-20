3b336f68827d698629f4cf9da9887be6e398c14a
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-03-20 12:56:33 +0100

    [OU-FIX] account_payment_return: Missing migration script
    We need to transfer the flag `returned_payment` from invoice to move.

--- account_payment_return/migrations/13.0.1.0.0/post-migration.py
+++ account_payment_return/migrations/13.0.1.0.0/post-migration.py
@@ -1,3 +1,5 @@
+# Copyright 2020 Antoni Romera
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade  # pylint: disable=W7936
@@ -5,6 +7,14 @@ from openupgradelib import openupgrade  # pylint: disable=W7936
 
 @openupgrade.migrate()
 def migrate(env, version):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE account_move am
+        SET returned_payment = ai.returned_payment
+        FROM account_invoice ai
+        WHERE ai.id = am.old_invoice_id""",
+    )
     openupgrade.load_data(
         env.cr, "account_payment_return", "migrations/13.0.1.0.0/noupdate_changes.xml"
     )
