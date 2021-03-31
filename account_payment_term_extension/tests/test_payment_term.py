9a51f435808aecb49dffe4a51f701fb99712601c
Andrea Piovesana <andrea.m.piovesana@gmail.com>
2021-03-31 09:17:14 +0200

    black
    black

--- account_payment_term_extension/tests/test_payment_term.py
+++ account_payment_term_extension/tests/test_payment_term.py
@@ -58,7 +58,7 @@ class TestAccountPaymentTerm(SavepointCase):
             "2015-03-16",
             "Error in the compute of payment terms 15 days",
         )
-        
+
     def test_03_compute(self):
         two_month_payterm_after_invoice_month = self.account_payment_term.create(
             {
@@ -84,7 +84,7 @@ class TestAccountPaymentTerm(SavepointCase):
             "2015-05-31",
             "Error in the compute of payment terms with months after invoice month",
         )
-    
+
     def test_postpone_holiday(self):
         str_date_invoice = "2015-03-02"
         str_date_holiday = "2015-03-16"
