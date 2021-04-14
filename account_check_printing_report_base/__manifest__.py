2e0a749de2d945859d3fbe50eb52c76b88b19608
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2021-04-15 00:33:22 +0300

    [13.0][FIX] account_check_printing_report_base: mistypo in args

--- account_check_printing_report_base/__manifest__.py
+++ account_check_printing_report_base/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Account Check Printing Report Base",
-    "version": "13.0.2.1.0",
+    "version": "13.0.2.1.1",
     "license": "AGPL-3",
     "author": "Eficent,"
     "Serpent Consulting Services Pvt. Ltd.,"
