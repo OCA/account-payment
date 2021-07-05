7148610c519abd13eafc96e08e933f86a2bbff38
Víctor Martínez <victor.martinez@tecnativa.com>
2021-07-05 08:09:27 +0200

    [IMP] account_payment_term_partner_holiday: black, isort, prettier

--- account_payment_term_partner_holiday/tests/test_partner_holiday.py
+++ account_payment_term_partner_holiday/tests/test_partner_holiday.py
@@ -1,62 +1,85 @@
 # Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo import fields
 from odoo.tests import common
 from odoo.tests.common import Form
-from odoo import fields
 
 
 class TestPartnerHoliday(common.TransactionCase):
     def setUp(self):
         super().setUp()
-        self.partner_1 = self.env["res.partner"].create({
-            "name": "Partner test 1",
-            "holiday_ids": [(0, 0, {
-                "day_from": "1",
-                "month_from": "2",
-                "day_to": "31",
-                "month_to": "2",
-            })]
-        })
+        self.partner_1 = self.env["res.partner"].create(
+            {
+                "name": "Partner test 1",
+                "holiday_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "day_from": "1",
+                            "month_from": "2",
+                            "day_to": "31",
+                            "month_to": "2",
+                        },
+                    )
+                ],
+            }
+        )
         self.partner_2 = self.env["res.partner"].create({"name": "Partner test 2"})
-        self.payment_term_immediate = self.env["account.payment.term"].create({
-            "name": "Immediate",
-            "line_ids": [(0, 0, {
-                "value": "balance",
-                "days": 0,
-                "option": "day_after_invoice_date"
-            })]
-        })
-        self.payment_term_10_days = self.env["account.payment.term"].create({
-            "name": "10 Days",
-            "line_ids": [(0, 0, {
-                "value": "balance",
-                "days": 10,
-                "option": "day_after_invoice_date"
-            })]
-        })
-        self.journal = self.env["account.journal"].create({
-            "name": "Test sale",
-            "type": "sale",
-            "code": "TEST-SALE"
-        })
-        self.product = self.env["product.product"].create({
-            "name": "Test product",
-            "type": "service"
-        })
+        self.payment_term_immediate = self.env["account.payment.term"].create(
+            {
+                "name": "Immediate",
+                "line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "value": "balance",
+                            "days": 0,
+                            "option": "day_after_invoice_date",
+                        },
+                    )
+                ],
+            }
+        )
+        self.payment_term_10_days = self.env["account.payment.term"].create(
+            {
+                "name": "10 Days",
+                "line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "value": "balance",
+                            "days": 10,
+                            "option": "day_after_invoice_date",
+                        },
+                    )
+                ],
+            }
+        )
+        self.journal = self.env["account.journal"].create(
+            {"name": "Test sale", "type": "sale", "code": "TEST-SALE"}
+        )
+        self.product = self.env["product.product"].create(
+            {"name": "Test product", "type": "service"}
+        )
 
     def test_check_dates_in_partner_1(self):
         self.assertEqual(
-            self.partner_1.is_date_in_holiday(fields.Date.from_string("2021-02-01")), [
+            self.partner_1.is_date_in_holiday(fields.Date.from_string("2021-02-01")),
+            [
                 fields.Date.from_string("2021-02-01"),
-                fields.Date.from_string("2021-02-28")
-            ]
+                fields.Date.from_string("2021-02-28"),
+            ],
         )
         self.assertEqual(
-            self.partner_1.is_date_in_holiday(fields.Date.from_string("2021-02-10")), [
+            self.partner_1.is_date_in_holiday(fields.Date.from_string("2021-02-10")),
+            [
                 fields.Date.from_string("2021-02-01"),
-                fields.Date.from_string("2021-02-28")
-            ]
+                fields.Date.from_string("2021-02-28"),
+            ],
         )
         self.assertFalse(
             self.partner_1.is_date_in_holiday(fields.Date.from_string("2021-03-01"))
@@ -78,12 +101,12 @@ class TestPartnerHoliday(common.TransactionCase):
 
     def test_invoice_payment_term_partner_1(self):
         invoice_form = Form(
-            self.env['account.invoice'].with_context(
+            self.env["account.invoice"].with_context(
                 default_journal_id=self.journal.id,
                 default_partner_id=self.partner_1.id,
-                default_date_invoice="2021-02-01"
+                default_date_invoice="2021-02-01",
             ),
-            view="account.invoice_form"
+            view="account.invoice_form",
         )
         invoice_form.payment_term_id = self.payment_term_immediate
         self.assertEqual(invoice_form.date_due, fields.Date.from_string("2021-03-01"))
@@ -100,12 +123,12 @@ class TestPartnerHoliday(common.TransactionCase):
 
     def test_invoice_payment_term_partner_2(self):
         invoice_form = Form(
-            self.env['account.invoice'].with_context(
+            self.env["account.invoice"].with_context(
                 default_journal_id=self.journal.id,
                 default_partner_id=self.partner_2.id,
-                default_date_invoice="2021-02-01"
+                default_date_invoice="2021-02-01",
             ),
-            view="account.invoice_form"
+            view="account.invoice_form",
         )
         invoice_form.payment_term_id = self.payment_term_immediate
         self.assertEqual(invoice_form.date_due, fields.Date.from_string("2021-02-01"))
