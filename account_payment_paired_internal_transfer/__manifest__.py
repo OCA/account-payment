859a8da0b134381fd2944474d47b4e16b6164025
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-07-10 18:04:26 -0500

    [14.0][ADD] account_payment_paired_internal_transfer: new module to allow to make internal transfers between banks in one move

---
+++ account_payment_paired_internal_transfer/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Jarsa
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Account Payment Paired Internal Transfer",
+    "version": "14.0.1.0.0",
+    "summary": "Crete internal transfers in one move.",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "author": "Odoo Community Association (OCA), Jarsa, Odoo SA",
+    "website": "https://github.com/OCA/account-payment",
+    "data": [
+        "views/account_payment_views.xml",
+    ],
+}
---
+++ account_payment_paired_internal_transfer/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module allows to generate an internal bank or cash transfer bank in one move.
+
+When you create an internal transfer you can select the destination journal and the incoming payment will be created.
+
+This module is based on Odoo feature added in 14.3 branch.
