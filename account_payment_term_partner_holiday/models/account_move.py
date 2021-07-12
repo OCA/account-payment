f801f1378c583d7de28f55d149eea47051d9fec0
Víctor Martínez <victor.martinez@tecnativa.com>
2021-07-12 10:08:02 +0200

    [FIX+IMP] account_payment_term_partner_holiday: [FIX] Improve is_date_in_holiday() to filtered correctly when day_to is less than date to compare [IMP] Improve is_date_in_holiday() to work fine in child partners [IMP] Add constraint to holidays to prevent month_to < month_from
    [FIX] account_payment_term_partner_holiday: Change month_from and month_to fields to convert in 2 characters to order correctly holidays + Script migration

--- account_payment_term_partner_holiday/models/account_move.py
+++ account_payment_term_partner_holiday/models/account_move.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import api, models
 
 
 class AccountMove(models.Model):
@@ -12,3 +12,21 @@ class AccountMove(models.Model):
         if self.partner_id:
             _self = self.with_context(move_partner_id=self.partner_id.id)
         super(AccountMove, _self)._recompute_payment_terms_lines()
+
+    @api.onchange("invoice_date_due")
+    def _onchange_invoice_date_due_account_payment_term_partner_holiday(self):
+        if self.invoice_date_due and self.partner_id:
+            new_invoice_date_due = self.partner_id._get_valid_due_date(
+                self.invoice_date_due
+            )
+            if new_invoice_date_due != self.invoice_date_due:
+                self.invoice_date_due = new_invoice_date_due
+
+    def action_post(self):
+        """Inject a context for getting the partner when computing payment term.
+        The trade-off is that we should split the call to super record per record,
+        but it shouldn't impact in performance.
+        """
+        for item in self:
+            _item = item.with_context(move_partner_id=item.partner_id.id)
+            return super(AccountMove, _item).action_post()
