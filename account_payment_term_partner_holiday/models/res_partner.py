1e700e9adaf3b5e6a80f983286db0f9cea89b667
Víctor Martínez <victor.martinez@tecnativa.com>
2021-07-12 10:08:02 +0200

    [FIX+IMP] account_payment_term_partner_holiday: [FIX] Improve is_date_in_holiday() to filtered correctly when day_to is less than date to compare [IMP] Improve is_date_in_holiday() to work fine in child partners [IMP] Add constraint to holidays to prevent month_to < month_from
    [FIX] account_payment_term_partner_holiday: Change month_from and month_to fields to convert in 2 characters to order correctly holidays + Script migration

--- account_payment_term_partner_holiday/models/res_partner.py
+++ account_payment_term_partner_holiday/models/res_partner.py
@@ -1,5 +1,10 @@
 # Copyright 2021 Tecnativa - Víctor Martínez
+# Copyright 2021 Tecnativa - João Marques
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+import calendar
+
+from dateutil.relativedelta import relativedelta
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
@@ -16,33 +21,34 @@ class ResPartner(models.Model):
         auto_join=True,
     )
 
+    def _get_valid_due_date(self, date):
+        if isinstance(date, str):
+            date = fields.Date.from_string(date)
+        is_date_in_holiday = self.is_date_in_holiday(date)
+        while is_date_in_holiday:
+            date = is_date_in_holiday[1] + relativedelta(days=1)
+            is_date_in_holiday = self.is_date_in_holiday(date)
+        return date
+
     def is_date_in_holiday(self, date):
         if isinstance(date, str):
             date = fields.Date.from_string(date)
-        res = self.holiday_ids.filtered(
-            lambda x: int(x.month_from) <= date.month
-            and int(x.month_to) >= date.month
-            and int(x.day_from) <= date.day
-            and int(x.day_to) >= date.day
-        )
-        if bool(res):
-            res = res[0]
-            res_date_to = False
-            day_to = int(res.day_to)
-            while not res_date_to:
-                try:
-                    res_date_to = fields.Date.from_string(
-                        "%s-%s-%s" % (date.year, res.month_to, day_to)
-                    )
-                except ValueError:
-                    day_to -= 1
-            return [
-                fields.Date.from_string(
-                    "%s-%s-%s" % (date.year, res.month_from, res.day_from)
-                ),
-                res_date_to,
-            ]
+        for holiday in self.commercial_partner_id.holiday_ids:
+            holiday_start_date = self._generate_field_date(
+                date.year, int(holiday.month_from), int(holiday.day_from)
+            )
+            holiday_end_date = self._generate_field_date(
+                date.year, int(holiday.month_to), int(holiday.day_to)
+            )
+            if date >= holiday_start_date and date <= holiday_end_date:
+                return [holiday_start_date, holiday_end_date]
         return False
+
+    def _generate_field_date(self, year, month, day):
+        # When the user selects a date that does not exist, assume the last day
+        # for that month
+        days = (day, max(calendar.monthrange(year, month)))
+        return fields.Date.from_string("%s-%s-%s" % (year, month, min(days)))
 
 
 class ResPartnerHoliday(models.Model):
@@ -57,15 +63,15 @@ class ResPartnerHoliday(models.Model):
     @api.model
     def _selection_months(self):
         return [
-            ("1", _("January")),
-            ("2", _("February")),
-            ("3", _("March")),
-            ("4", _("April")),
-            ("5", _("May")),
-            ("6", _("June")),
-            ("7", _("July")),
-            ("8", _("August")),
-            ("9", _("September")),
+            ("01", _("January")),
+            ("02", _("February")),
+            ("03", _("March")),
+            ("04", _("April")),
+            ("05", _("May")),
+            ("06", _("June")),
+            ("07", _("July")),
+            ("08", _("August")),
+            ("09", _("September")),
             ("10", _("October")),
             ("11", _("November")),
             ("12", _("December")),
@@ -91,6 +97,14 @@ class ResPartnerHoliday(models.Model):
     month_to = fields.Selection(
         selection="_selection_months", string="Month to", required=True,
     )
+
+    _sql_constraints = [
+        (
+            "month_consistency",
+            "CHECK(month_from <= month_to)",
+            "Month from should be higher than month from",
+        ),
+    ]
 
     @api.constrains("day_from", "month_from", "day_to", "month_to")
     def _check_from_end_dates(self):
