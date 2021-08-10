8d9065f7e420ebe1512b121434d75bae0e55c5a4
ps-tubtim <pimolnats@ecosoft.co.th>
2021-08-10 16:18:53 +0700

    [MIG] account_check_date: Migration to 14.0

--- account_check_date/__manifest__.py
+++ account_check_date/__manifest__.py
@@ -3,13 +3,19 @@
 
 {
     "name": "Account Check Date",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-payment",
     "license": "AGPL-3",
     "category": "Accounting & Finance",
     "summary": "Add check date on payment for check printing",
     "depends": ["account_check_printing"],
-    "data": ["views/account_payment_views.xml"],
+    "data": [
+        "views/account_payment_views.xml",
+        "wizard/account_payment_register_views.xml",
+    ],
     "installable": True,
+    "post_init_hook": "assign_check_date",
+    "development_status": "Alpha",
+    "maintainers": ["ps-tubtim"],
 }
