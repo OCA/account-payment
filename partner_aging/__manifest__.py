e5bcd58fced5cac9b8e9392633e91313c7007616
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2021-08-10 12:01:23 -0500

    [IMP] Documentation (dummy commit to fix the missing package on Pypi)

--- partner_aging/__manifest__.py
+++ partner_aging/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2012 - 2019 Open Source Integrators
+# Copyright 2012 - 2021 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Interactive Partner Aging at any date",
