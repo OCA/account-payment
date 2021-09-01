6ac439dfb6bf9c3a5de9559349f0dc719d846497
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2021-09-01 09:42:47 +0200

    [FIX] account_payment_multi_deduction: bump version
    to solve a pypi upload issue

--- account_payment_multi_deduction/__manifest__.py
+++ account_payment_multi_deduction/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Payment Register with Multiple Deduction",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/account-payment",
