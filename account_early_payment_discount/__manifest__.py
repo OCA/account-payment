616d521ad5f446590ab61d15085deb697f6fa92d
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2021-09-01 09:41:25 +0200

    [FIX] account_early_payment_discount: bump version
    to solve a pypi upload issue

--- account_early_payment_discount/__manifest__.py
+++ account_early_payment_discount/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Account Early Payment Discount',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'author': 'Camptocamp, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'category': 'Accounting & Finance',
