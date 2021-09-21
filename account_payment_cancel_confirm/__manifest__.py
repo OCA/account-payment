c789231fa0a86fceca7da13efbf370ec18e9f06d
ps-tubtim <pimolnats@ecosoft.co.th>
2021-09-21 15:41:48 +0700

    [14.0][ADD] account_payment_cancel_confirm

---
+++ account_payment_cancel_confirm/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Payment Cancel Confirm",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "category": "Usability",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/account-payment",
+    "depends": ["base_cancel_confirm", "account"],
+    "installable": True,
+    "maintainers": ["ps-tubtim"],
+    "development_status": "Alpha",
+}
