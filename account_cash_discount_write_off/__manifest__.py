9a5c9b6557e762b1056685cde497752395c6618f
hkapatel <helly.kapatel@initos.com>
2021-09-24 17:06:45 +0530

    [MIG] account_cash_discount_write_off: Migration to 14.0

--- account_cash_discount_write_off/__manifest__.py
+++ account_cash_discount_write_off/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Create an automatic writeoff for payment with discount on the payment
         order confirmation""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-payment",
