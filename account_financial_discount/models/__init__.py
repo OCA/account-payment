b3cb81b5c7cedcdc5c811bfe2661f7c5df2945f9
Akim Juillerat <akim.juillerat@camptocamp.com>
2021-10-11 11:31:23 +0200

    Refactor account payment with register wizard

--- account_financial_discount/models/__init__.py
+++ account_financial_discount/models/__init__.py
@@ -1,6 +1,5 @@
 from . import account_payment_term
 from . import account_move
-from . import account_payment
 from . import res_company
 from . import res_config_settings
 from . import account_move_line
