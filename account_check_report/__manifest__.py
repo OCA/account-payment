abaf1ddf5f666f81c9bbe315c0c2e2d3de5414dd
iwkse <iwkse@claws-mail.org>
2021-12-15 22:40:36 +0100

    [IMP] account_check_report: black, isort, prettier

--- account_check_report/__manifest__.py
+++ account_check_report/__manifest__.py
@@ -3,16 +3,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Check Report',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': "Eficent,"
-              "Odoo Community Association (OCA)",
-    'category': 'Generic Modules/Accounting',
-    'website': "https://github.com/OCA/account-payment",
-    'depends': [],
-    'data': [
-        'report/account_check_report.xml',
+    "name": "Account Check Report",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Eficent," "Odoo Community Association (OCA)",
+    "category": "Generic Modules/Accounting",
+    "website": "https://github.com/OCA/account-payment",
+    "depends": [],
+    "data": [
+        "report/account_check_report.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
