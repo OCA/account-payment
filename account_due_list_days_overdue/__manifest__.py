957064e9f5e71b1bb46bd325bcb4acbd6c476b9c
Víctor Martínez <victor.martinez@tecnativa.com>
2021-12-17 16:39:52 +0100

    [FIX] account_due_list_days_overdue: postprocess_and_fields() function is used to simplify the code in fields_view_get() a bit. The sum of the values of the new fields (overdue_term_xx) is now displayed in the tree view.
    account_due_list_days_overdue 13.0.1.0.2

--- account_due_list_days_overdue/__manifest__.py
+++ account_due_list_days_overdue/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Payments Due list days overdue",
-    "version": "13.0.1.0.1",
+    "version": "13.0.1.0.2",
     "category": "Accounting",
     "author": "Odoo Community Association (OCA), ForgeFlow",
     "website": "https://github.com/OCA/account-payment",
