0b87ecd5e4d1792d72e79c72e3ba6dbc2d88486a
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-20 10:22:04 -0500

    [IMP] account_payment_widget_amount: black, isort, prettier

--- account_payment_widget_amount/models/account_move_line.py
+++ account_payment_widget_amount/models/account_move_line.py
@@ -1,6 +1,6 @@
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
@@ -9,33 +9,40 @@ class AccountMoveLine(models.Model):
 
     @api.model
     def update_amount_reconcile(
-            self, temp_amount_residual, temp_amount_residual_currency,
-            amount_reconcile, credit_move, debit_move):
+        self,
+        temp_amount_residual,
+        temp_amount_residual_currency,
+        amount_reconcile,
+        credit_move,
+        debit_move,
+    ):
 
         super(AccountMoveLine, self).update_amount_reconcile(
-            temp_amount_residual, temp_amount_residual_currency,
-            amount_reconcile, credit_move, debit_move)
+            temp_amount_residual,
+            temp_amount_residual_currency,
+            amount_reconcile,
+            credit_move,
+            debit_move,
+        )
         # Check if amount is positive
-        paid_amt = self.env.context.get('paid_amount', 0.0)
+        paid_amt = self.env.context.get("paid_amount", 0.0)
         if not paid_amt:
-            return temp_amount_residual, temp_amount_residual_currency, \
-                amount_reconcile
+            return temp_amount_residual, temp_amount_residual_currency, amount_reconcile
         paid_amt = float(paid_amt)
         if paid_amt < 0:
-            raise UserError(_(
-                "The specified amount has to be strictly positive"))
+            raise UserError(_("The specified amount has to be strictly positive"))
 
         # We need those temporary value otherwise the computation might
         # be wrong below
 
         # Compute paid_amount currency
-        if debit_move.amount_residual_currency or \
-                credit_move.amount_residual_currency:
+        if debit_move.amount_residual_currency or credit_move.amount_residual_currency:
 
             temp_amount_residual_currency = min(
                 debit_move.amount_residual_currency,
                 -credit_move.amount_residual_currency,
-                paid_amt)
+                paid_amt,
+            )
         else:
             temp_amount_residual_currency = 0.0
 
@@ -43,30 +50,32 @@ class AccountMoveLine(models.Model):
         # currency taking into account the rate
         if temp_amount_residual_currency:
             paid_amt = debit_move.currency_id._convert(
-                paid_amt, debit_move.company_id.currency_id,
+                paid_amt,
+                debit_move.company_id.currency_id,
                 debit_move.company_id,
-                credit_move.date or fields.Date.today())
-        temp_amount_residual = min(debit_move.amount_residual,
-                                   -credit_move.amount_residual,
-                                   paid_amt)
-        amount_reconcile = temp_amount_residual_currency or \
-            temp_amount_residual
+                credit_move.date or fields.Date.today(),
+            )
+        temp_amount_residual = min(
+            debit_move.amount_residual, -credit_move.amount_residual, paid_amt
+        )
+        amount_reconcile = temp_amount_residual_currency or temp_amount_residual
 
-        return temp_amount_residual, temp_amount_residual_currency, \
-            amount_reconcile
+        return temp_amount_residual, temp_amount_residual_currency, amount_reconcile
 
     @api.model
     def _check_remove_debit_move(self, amount_reconcile, debit_move, field):
         res = super(AccountMoveLine, self)._check_remove_debit_move(
-            amount_reconcile, debit_move, field)
-        if not isinstance(self.env.context.get('paid_amount', False), bool):
+            amount_reconcile, debit_move, field
+        )
+        if not isinstance(self.env.context.get("paid_amount", False), bool):
             return True
         return res
 
     @api.model
     def _check_remove_credit_move(self, amount_reconcile, credit_move, field):
         res = super(AccountMoveLine, self)._check_remove_credit_move(
-            amount_reconcile, credit_move, field)
-        if not isinstance(self.env.context.get('paid_amount', False), bool):
+            amount_reconcile, credit_move, field
+        )
+        if not isinstance(self.env.context.get("paid_amount", False), bool):
             return True
         return res
