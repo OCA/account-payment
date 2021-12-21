92ad098960bc56f312dcab1a1b53b3a6cf768c03
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-21 11:54:42 -0500

    [IMP] added as mainteiner

--- account_payment_widget_amount/__manifest__.py
+++ account_payment_widget_amount/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019-2021 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment Widget Amount",
@@ -6,7 +6,7 @@
     "version": "14.0.1.0.0",
     "category": "Account-payment",
     "website": "https://github.com/OCA/account-payment",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
@@ -17,4 +17,5 @@
     "qweb": [
         "static/src/xml/account_payment.xml",
     ],
+    "maintainers": ["ChrisOForgeFlow"],
 }
