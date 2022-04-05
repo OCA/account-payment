76775a461abba4f0d7383b33bd1ce62971cd77eb
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-04-05 15:43:47 +0200

    [ADD] account_customer_wallet
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ account_customer_wallet/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Customer Wallet",
+    "summary": """
+        Allow customers to pay using a wallet which is tracked by the company.""",
+    "version": "12.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [],
+    "excludes": [],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
