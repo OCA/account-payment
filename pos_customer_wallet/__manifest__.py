8e50934cd0ec97de29da98a2028e14ece1217007
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-04-07 10:25:20 +0200

    [ADD] pos_customer_wallet
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ pos_customer_wallet/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Point of Sale Customer Wallet",
+    "summary": """
+        Enable usage of the Customer Wallet in the Point of Sale.""",
+    "version": "12.0.1.0.0",
+    "category": "Point of Sale",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "point_of_sale",
+        "account_customer_wallet",
+    ],
+    "excludes": [],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
