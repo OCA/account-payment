5680efdc7eb4505288c7e2d7d904b77f3e78802f
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-04-13 16:15:03 +0200

    [ADD] pos_customer_wallet: Check sufficient balance before POS purchase
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- pos_customer_wallet/static/src/js/screens.js
+++ pos_customer_wallet/static/src/js/screens.js
@@ -27,22 +27,46 @@ odoo.define("pos_customer_wallet.screens", function (require) {
             }
             return null;
         },
+        amount_paid_with_customer_wallet() {
+            var order = this.pos.get_order();
+            var cashregister = this.find_customer_wallet_payment_method();
+            var total = 0;
+            var lines = order.paymentlines.models;
+            for (var i = 0; i < lines.length; i++) {
+                if (lines[i].cashregister === cashregister) {
+                    total += lines[i].amount;
+                }
+            }
+            return total;
+        },
         order_is_valid: function () {
             if (!this._super()) {
                 return false;
             }
 
             var client = this.pos.get_client();
-            var customer_wallet_payment_method =
-                this.find_customer_wallet_payment_method();
-            if (!client && customer_wallet_payment_method) {
-                this.gui.show_popup("error", {
-                    title: _t("No customer selected"),
-                    body: _t(
-                        "Cannot user customer wallet payment method without selecting a customer.\n\n Please select a customer or use a different payment method."
-                    ),
-                });
-                return false;
+            var wallet_amount = this.amount_paid_with_customer_wallet();
+
+            if (!client) {
+                if (wallet_amount > 0) {
+                    this.gui.show_popup("error", {
+                        title: _t("No customer selected"),
+                        body: _t(
+                            "Cannot user customer wallet payment method without selecting a customer.\n\n Please select a customer or use a different payment method."
+                        ),
+                    });
+                    return false;
+                }
+            } else {
+                if (client.customer_wallet_balance - wallet_amount <= -0.00001) {
+                    this.gui.show_popup("error", {
+                        title: _t("Customer wallet balance not sufficient"),
+                        body: _t(
+                            "There is not enough balance in the customer's wallet to perform this order."
+                        ),
+                    });
+                    return false;
+                }
             }
 
             return true;
