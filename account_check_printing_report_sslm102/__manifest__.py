303c8008208bd4e9b8cc95935bf55c2c8795b60f
Juany Davila <juanyadavilap@gmail.com>
2022-05-12 11:59:12 -0500

    [MIG] account_check_printing_report_sslm102: Migration to 14.0

--- account_check_printing_report_sslm102/__manifest__.py
+++ account_check_printing_report_sslm102/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Account Check Printing Report SSLM102",
     "summary": "Allows you to print SSLM102 lined checks.",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Eficent," "Odoo Community Association (OCA)",
     "category": "Generic Modules/Accounting",
--- account_check_printing_report_sslm102/readme/CONTRIBUTORS.rst
+++ account_check_printing_report_sslm102/readme/CONTRIBUTORS.rst
@@ -1,2 +1,4 @@
 * Eficent Business and IT Consulting Services, S.L.
     * Jordi Ballester Alomar <jordi.ballester@eficent.com>
+    * Juany Davila <juany.davila@forgeflow.com>
+
