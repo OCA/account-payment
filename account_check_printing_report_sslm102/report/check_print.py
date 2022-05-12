a82202c33c4acefdf3929e8f419b06906505a17f
Juany Davila <juanyadavilap@gmail.com>
2022-05-12 11:59:12 -0500

    [MIG] account_check_printing_report_sslm102: Migration to 14.0

--- account_check_printing_report_sslm102/report/check_print.py
+++ account_check_printing_report_sslm102/report/check_print.py
@@ -1,5 +1,4 @@
-# Copyright 2016 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
+# Copyright 2023 ForgeFlow S.L. (http://www.forgeflow.com)
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
