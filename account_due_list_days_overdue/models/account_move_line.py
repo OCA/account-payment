0966e2a923c6e8d0883f33a8ad7071eedc1bf514
Víctor Martínez <victor.martinez@tecnativa.com>
2022-05-16 12:46:21 +0200

    [IMP] account_due_list_days_overdue: black, isort, prettier

--- account_due_list_days_overdue/models/account_move_line.py
+++ account_due_list_days_overdue/models/account_move_line.py
@@ -69,7 +69,10 @@ class AccountMoveLine(models.Model):
         self, view_id=None, view_type="form", toolbar=False, submenu=False
     ):
         result = super().fields_view_get(
-            view_id, view_type, toolbar=toolbar, submenu=submenu,
+            view_id,
+            view_type,
+            toolbar=toolbar,
+            submenu=submenu,
         )
 
         doc = etree.XML(result["arch"])
