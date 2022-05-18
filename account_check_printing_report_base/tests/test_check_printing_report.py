113b89ecc38ed1f14f486f705d87ce08ef0fa0db
AmmarOfficewalaSerpentCS <ammar.o.serpentcs@gmail.com>
2022-05-18 18:57:31 +0530

    [IMP] account_check_printing_report_base: black, isort, prettier

--- account_check_printing_report_base/tests/test_check_printing_report.py
+++ account_check_printing_report_base/tests/test_check_printing_report.py
@@ -44,7 +44,8 @@ class TestAccountCheckPrintingReportBase(TransactionCase):
             {"name": "Test Check Layout By Journal"}
         )
         self.payment_method_check = self.payment_method_model.search(
-            [("code", "=", "check_printing")], limit=1,
+            [("code", "=", "check_printing")],
+            limit=1,
         )
         if not self.payment_method_check:
             self.payment_method_check = self.payment_method_model.create(
@@ -154,7 +155,7 @@ class TestAccountCheckPrintingReportBase(TransactionCase):
         self.assertEquals(content[1], "html")
 
     def test_02_check_printing_with_layout(self):
-        """ Test if the check is printed when the layout is specified for a
+        """Test if the check is printed when the layout is specified for a
         company and journal."""
 
         self.company.check_layout_id = self.check_report
