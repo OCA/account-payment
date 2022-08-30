557e45b28cfb78ce51fa72e9a3b357674e5c3700
Denis Leemann <denis.leemann@camptocamp.com>
2022-08-30 15:05:46 +0200

    [MIG] account_payment_batch_process_discount: Migration to 15.0

--- account_payment_batch_process_discount/__manifest__.py
+++ account_payment_batch_process_discount/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Discount on batch payments",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Accounting & Finance",
