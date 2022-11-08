bed657ad8b0a1ca614ce3d40986ff445b468b69d
Urvisha-OSI <udesai@opensourceintegrators.com>
2022-11-08 11:45:38 +0530

    [FIX] add fixed for the partner_aging

--- partner_aging/wizard/__init__.py
+++ partner_aging/wizard/__init__.py
@@ -1,5 +1,7 @@
-# Copyright (C) 2022 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import customer
-from . import supplier
+from . import (
+    res_partner_aging_customer,
+    res_partner_aging_date,
+    res_partner_aging_supplier,
+)
