06baf927ddba1e1dac44bfea34d02f790e8112e5
FernandoRomera <fromera@astrohotel.es>
2022-12-21 10:38:23 +0100

    [16.0][MIG] account_payment_terminal: Migration to 16.0

--- account_payment_terminal/__manifest__.py
+++ account_payment_terminal/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Payment Terminal",
     "summary": """This addon allows to pay invoices using payment terminal""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "maintainers": ["sbejaoui"],
@@ -12,10 +12,17 @@
     "depends": ["account"],
     "data": [
         "security/account_payment_terminal.xml",
-        "views/webclient_templates.xml",
-        "views/account_payment_terminal.xml",
-        "views/account_journal.xml",
+        "views/account_payment_terminal_views.xml",
+        "views/account_journal_views.xml",
         "views/oca_payment_terminal_form_mixin.xml",
         "wizards/account_payment_register.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "account_payment_terminal/static/src/js/views/"
+            "payment_terminal_form/payment_terminal_form_controller.js",
+            "account_payment_terminal/static/src/js/views/"
+            "payment_terminal_form/payment_terminal_form_view.js",
+        ],
+    },
 }
