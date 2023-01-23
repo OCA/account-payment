98a4251df466fd5ebd38b766c4a500725616f62a
Víctor Martínez <victor.martinez@tecnativa.com>
2023-01-23 17:13:43 +0100

    [ADD] sale_payment_term_security: New module
    TT41273

---
+++ sale_payment_term_security/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Payment Term Security",
+    "version": "13.0.1.0.0",
+    "category": "Accounting & Finance",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainer": "OCA",
+    "website": "https://github.com/OCA/account-payment",
+    "license": "AGPL-3",
+    "depends": ["account_payment_term_security", "sale"],
+    "installable": True,
+    "maintainers": ["victoralmau"],
+}
