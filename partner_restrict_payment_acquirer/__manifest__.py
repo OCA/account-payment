8464626499e0aa058c756c5e2046689f42a795ec
geomer198 <geomer198@gmail.com>
2023-01-25 02:36:38 +0300

    [ADD] partner_restrict_payment_acquirer: Module added

---
+++ partner_restrict_payment_acquirer/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Partner Restrict Payment Acquirer",
+    "summary": "Partner Restrict Payment Acquirer",
+    "author": "Cetmix, Odoo Community Association (OCA)",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/account-payment",
+    "maintainers": ["geomer198", "CetmixGitDrone"],
+    "depends": ["sale"],
+    "data": [
+        "views/payment_templates.xml",
+        "views/res_partner_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
---
+++ partner_restrict_payment_acquirer/readme/ROADMAP.rst
@@ -0,0 +1,1 @@
+Would be reasonable to create an extension of this module for E-Commerce. Eg "partner_restrict_payment_acquirer_website"
