566d26a3edd555bffdd399ea93685c2ad4fcf2a0
Luis Ernesto García Medina <ernesto.garcia@tecnativa.com>
2023-02-22 18:09:23 -0600

    [MIG] account_payment_term_security: Migration to 15.0

--- account_payment_term_security/tests/test_account_payment_term_security.py
+++ account_payment_term_security/tests/test_account_payment_term_security.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from lxml import etree
 
-from odoo.tests.common import SavepointCase, new_test_user, users
+from odoo.tests import TransactionCase, new_test_user, users
 
 
-class TestAccountPaymentTermSecurity(SavepointCase):
+class TestAccountPaymentTermSecurity(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
