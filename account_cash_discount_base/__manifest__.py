6f87ddafdbecc72e2ea0fc9715f67351b985a99f
Ruchir Shukla <ruchir@bizzappdev.com>
2023-02-28 12:10:07 +0530

    [MIG][15.0]account_cash_discount_base:Migration to 15.0

--- account_cash_discount_base/__manifest__.py
+++ account_cash_discount_base/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Cash Discount Base",
-    "version": "14.0.1.0.0",
+    "version": "15.0.0.1.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-payment",
--- account_cash_discount_base/tests/common.py
+++ account_cash_discount_base/tests/common.py
@@ -1,10 +1,10 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestAccountCashDiscountCommon(SavepointCase):
+class TestAccountCashDiscountCommon(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestAccountCashDiscountCommon, cls).setUpClass()
