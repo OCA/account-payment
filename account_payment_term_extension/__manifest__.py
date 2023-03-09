fe99ad1219fcb93ed1df69c45b4c1087019b91cf
Diep Huu Hoang <hoang@trobz.com>
2023-03-09 17:03:30 +0700

    [16.0][MIG] account_payment_term_extension: Migration to 16.0

--- account_payment_term_extension/__manifest__.py
+++ account_payment_term_extension/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Payment Term Extension",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Accounting & Finance",
     "summary": "Adds rounding, months, weeks and multiple payment days "
     "properties on payment term lines",
