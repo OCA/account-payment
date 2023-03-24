9651567466fdf2a4cfda34a81df2378d941c6a39
Florent Cayré <florent@commown.coop>
2023-03-24 15:35:37 +0100

    Simplify web-side slimpay payment controller call
    It does not need an access_token in the useage context nor a so_id,
    stored in the session.

--- account_payment_slimpay/static/src/js/slimpay.js
+++ account_payment_slimpay/static/src/js/slimpay.js
@@ -9,15 +9,10 @@ odoo.define('account_payment_slimpay.slimpay', function(require) {
 
     require('web.dom_ready');
 
-    var access_token = ($("input[name='access_token']").val()
-                        || $("input[name='token']").val()
-                        || '');
-    var order_id = parseInt($("input[name='order_id']").val()) || '';
     var acquirer_id = parseInt($(
       'form[provider="slimpay"] #acquirer_slimpay').val());
 
-    ajax.jsonRpc('/payment/slimpay_transaction/' + acquirer_id,
-                 'call', {access_token: access_token, so_id: order_id})
+    ajax.jsonRpc('/payment/slimpay_transaction/' + acquirer_id, 'call')
         .done(function (data) {
             window.location.href = data;
         })
