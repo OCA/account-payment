68baa747a3655ede7c586a8e184e0d9e3d1f767b
sergio-teruel <sergio.teruel@tecnativa.com>
2023-04-17 19:23:23 +0200

    [FIX] account_check_printing_report_base: Migration script fails

--- account_check_printing_report_base/migrations/15.0.1.0.0/pre-migration.py
+++ account_check_printing_report_base/migrations/15.0.1.0.0/pre-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2022 Tecnativa - Carlos Roca
+# Copyright 2023 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
@@ -10,44 +11,31 @@ def set_account_check_printing_layout(env):
         env.cr,
         """
         UPDATE res_company
-        SET account_check_printing_layout = CASE
-            WHEN check_layout_id = %s
-                THEN 'account_check_printing_report_base.action_report_check_base'
-            WHEN check_layout_id = %s
-                THEN 'account_check_printing_report_base.action_report_check_base_a4'
-            ELSE 'disabled'
-            END
+        SET account_check_printing_layout = imd.module ||'.'|| imd.name
+            FROM ir_model_data imd
+            WHERE imd.module='account_check_printing_report_base'
+                AND imd.model='account.payment.check.report'
+                AND imd.res_id=res_company.check_layout_id
         """,
-        (
-            env.ref(
-                "account_check_printing_report_base.account_payment_check_report_base"
-            ),
-            env.ref(
-                "account_check_printing_report_base.account_payment_check_report_base_a4"
-            ),
-        ),
     )
     # Change in journals
     openupgrade.logged_query(
         env.cr,
         """
-        UPDATE account_journal
-        SET account_check_printing_layout = CASE
-            WHEN check_layout_id = %s
-                THEN 'account_check_printing_report_base.action_report_check_base'
-            WHEN check_layout_id = %s
-                THEN 'account_check_printing_report_base.action_report_check_base_a4'
-            ELSE NULL
-            END
+        ALTER TABLE account_journal ADD COLUMN
+            IF NOT EXISTS account_check_printing_layout VARCHAR
         """,
-        (
-            env.ref(
-                "account_check_printing_report_base.account_payment_check_report_base"
-            ),
-            env.ref(
-                "account_check_printing_report_base.account_payment_check_report_base_a4"
-            ),
-        ),
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE account_journal
+        SET account_check_printing_layout = imd.module ||'.'|| imd.name
+            FROM ir_model_data imd
+            WHERE imd.module='account_check_printing_report_base'
+                AND imd.model='account.payment.check.report'
+                AND imd.res_id=account_journal.check_layout_id
+        """,
     )
 
 
