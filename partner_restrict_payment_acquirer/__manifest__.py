cf732a6f1172859e3da83a4d1bc75bd424f7a951
geomer198 <geomer198@gmail.com>
2023-05-17 00:02:48 +0300

    [IMP] partner_restrict_payment_acquirer: black, isort, prettier

--- partner_restrict_payment_acquirer/__manifest__.py
+++ partner_restrict_payment_acquirer/__manifest__.py
@@ -2,15 +2,12 @@
     "name": "Partner Restrict Payment Acquirer",
     "summary": "Partner Restrict Payment Acquirer",
     "author": "Cetmix, Odoo Community Association (OCA)",
-    "version": "14.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Accounting",
     "website": "https://github.com/OCA/account-payment",
     "maintainers": ["geomer198", "CetmixGitDrone"],
     "depends": ["sale"],
-    "data": [
-        "views/payment_templates.xml",
-        "views/res_partner_views.xml",
-    ],
+    "data": ["views/payment_templates.xml", "views/res_partner_views.xml"],
     "license": "AGPL-3",
     "installable": True,
 }
