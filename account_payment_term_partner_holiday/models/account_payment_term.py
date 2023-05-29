515c87e9354951fafc9c3ffb65c868351b4a9d5a
Jonathan Oscategui Taza <info@pesol.es>
2023-05-29 12:01:54 -0500

    [MIG] account_payment_term_partner_holiday: Migration to 16.0

--- account_payment_term_partner_holiday/models/account_payment_term.py
+++ account_payment_term_partner_holiday/models/account_payment_term.py
@@ -1,13 +1,23 @@
 # Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import models
 
 
 class AccountPaymentTerm(models.Model):
     _inherit = "account.payment.term"
 
-    def compute(self, value, date_ref=False, currency=None):
+    def _compute_terms(
+        self,
+        date_ref,
+        currency,
+        company,
+        tax_amount,
+        tax_amount_currency,
+        sign,
+        untaxed_amount,
+        untaxed_amount_currency,
+    ):
         """Compute the due date taking into account the holiday periods
         set in the partner.
 
@@ -16,20 +26,28 @@ class AccountPaymentTerm(models.Model):
         Then, apply_payment_days() and apply_holidays() to prevent
         incompatibilities.
         """
-        result = super().compute(value=value, date_ref=date_ref, currency=currency)
+        result = super()._compute_terms(
+            date_ref,
+            currency,
+            company,
+            tax_amount,
+            tax_amount_currency,
+            sign,
+            untaxed_amount,
+            untaxed_amount_currency,
+        )
         ctx = self.env.context
         partner_id = ctx.get("move_partner_id", ctx.get("default_partner_id"))
         if partner_id:
             partner = self.env["res.partner"].browse(partner_id)
             result2 = []
             for key, item in enumerate(result):
-                next_date = partner._get_valid_due_date(item[0])
-                if next_date != item[0]:
-                    line = self.line_ids[key]
+                next_date = partner._get_valid_due_date(item["date"])
+                if next_date != item["date"]:
+                    line = self.line_ids.sorted(lambda x: x.value == "balance")[key]
                     next_date = self.apply_payment_days(line, next_date)
                     next_date = self.apply_holidays(next_date)
-                    result2.append((fields.Date.to_string(next_date), item[1]))
-                else:
-                    result2.append(item)
+                    item.update({"date": next_date})
+                result2.append(item)
             result = result2
         return result
--- account_payment_term_partner_holiday/readme/CONTRIBUTORS.rst
+++ account_payment_term_partner_holiday/readme/CONTRIBUTORS.rst
@@ -3,3 +3,7 @@
   * Víctor Martínez
   * Pedro M. Baeza
   * Luis D. Lafaurie
+
+* `Pesol <https://www.pesol.es>`__:
+
+  * Jonathan Oscategui Taza <info@pesol.es>
