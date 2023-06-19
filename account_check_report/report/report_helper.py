74897aaa99df7495dba853ddf90fba815deb993a
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-06-19 10:45:16 +0530

    [IMP] account_check_report: black, isort, prettier

--- account_check_report/report/report_helper.py
+++ account_check_report/report/report_helper.py
@@ -3,26 +3,29 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import time
+
 from odoo import api, models
 from odoo.tools import float_is_zero
 
 
 class ReportCheckPrint(models.AbstractModel):
-    _name = 'report.account_check_report.check_report'
+    _name = "report.account_check_report.check_report"
     _description = "Report Check Print"
 
     def _format_date_to_partner_lang(self, date, partner_id):
-        lang_code = self.env['res.partner'].browse(partner_id).lang
-        lang = self.env['res.lang']._lang_get(lang_code)
+        lang_code = self.env["res.partner"].browse(partner_id).lang
+        lang = self.env["res.lang"]._lang_get(lang_code)
         return date.strftime(lang.date_format)
 
     def _get_paid_lines(self, payment):
         rec_lines = payment.move_line_ids.filtered(
-            lambda x: x.account_id.reconcile and x.account_id ==
-            payment.destination_account_id
-            and x.partner_id == payment.partner_id)
-        amls = rec_lines.mapped('matched_credit_ids.credit_move_id') + \
-            rec_lines.mapped('matched_debit_ids.debit_move_id')
+            lambda x: x.account_id.reconcile
+            and x.account_id == payment.destination_account_id
+            and x.partner_id == payment.partner_id
+        )
+        amls = rec_lines.mapped("matched_credit_ids.credit_move_id") + rec_lines.mapped(
+            "matched_debit_ids.debit_move_id"
+        )
         amls -= rec_lines
         # Here we need to handle a nasty corner case.
         # Sometimes we match a payment with invoices and refunds. Internally
@@ -37,7 +40,8 @@ class ReportCheckPrint(models.AbstractModel):
         invoice_amls = payment.invoice_ids.payment_move_line_ids.filtered(
             lambda x: x.account_id.reconcile
             and x.account_id == payment.destination_account_id
-            and x.partner_id == payment.partner_id)
+            and x.partner_id == payment.partner_id
+        )
         amls |= invoice_amls
         return amls
 
@@ -46,8 +50,8 @@ class ReportCheckPrint(models.AbstractModel):
         if amt < 0.0:
             amt *= -1
         amt = payment.company_id.currency_id.with_context(
-            date=payment.payment_date).compute(
-            amt, payment.currency_id)
+            date=payment.payment_date
+        ).compute(amt, payment.currency_id)
         return amt
 
     def _get_paid_amount(self, payment, line):
@@ -55,22 +59,21 @@ class ReportCheckPrint(models.AbstractModel):
         total_amount_to_show = 0.0
         # We pay out
         if line.matched_credit_ids:
-            amount = -1 * sum([p.amount for p in line.matched_credit_ids])
+            amount = -1 * sum(p.amount for p in line.matched_credit_ids)
         # We receive payment
         elif line.matched_debit_ids:
-            amount = sum([p.amount for p in line.matched_debit_ids])
+            amount = sum(p.amount for p in line.matched_debit_ids)
 
         # In case of customer payment, we reverse the amounts
-        if payment.partner_type == 'customer':
+        if payment.partner_type == "customer":
             amount *= -1
 
-        amount_to_show = \
-            payment.company_id.currency_id.with_context(
-                date=payment.payment_date).compute(
-                amount, payment.currency_id)
+        amount_to_show = payment.company_id.currency_id.with_context(
+            date=payment.payment_date
+        ).compute(amount, payment.currency_id)
         if not float_is_zero(
-                amount_to_show,
-                precision_rounding=payment.currency_id.rounding):
+            amount_to_show, precision_rounding=payment.currency_id.rounding
+        ):
             total_amount_to_show = amount_to_show
         return total_amount_to_show
 
@@ -79,22 +82,22 @@ class ReportCheckPrint(models.AbstractModel):
         if amt < 0.0:
             amt *= -1
         amt = payment.company_id.currency_id.with_context(
-            date=payment.payment_date).compute(
-            amt, payment.currency_id)
+            date=payment.payment_date
+        ).compute(amt, payment.currency_id)
         return amt
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        payments = self.env['account.payment'].browse(docids)
+        payments = self.env["account.payment"].browse(docids)
         docargs = {
-            'doc_ids': docids,
-            'doc_model': 'account.payment',
-            'docs': payments,
-            'time': time,
-            'total_amount': self._get_total_amount,
-            'paid_lines': self._get_paid_lines,
-            'residual_amount': self._get_residual_amount,
-            'paid_amount': self._get_paid_amount,
-            '_format_date_to_partner_lang': self._format_date_to_partner_lang,
+            "doc_ids": docids,
+            "doc_model": "account.payment",
+            "docs": payments,
+            "time": time,
+            "total_amount": self._get_total_amount,
+            "paid_lines": self._get_paid_lines,
+            "residual_amount": self._get_residual_amount,
+            "paid_amount": self._get_paid_amount,
+            "_format_date_to_partner_lang": self._format_date_to_partner_lang,
         }
         return docargs
