cf68ba90eef466401cd6564e945d55373aa854bc
Raul-S73 <raul@studio73.es>
2023-07-04 11:20:10 +0200

    [FIX] account_payment_return: Refactor wrong code Added in #639

--- account_payment_return/__manifest__.py
+++ account_payment_return/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Account Payment Returns",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "summary": "Manage the return of your payments",
     "license": "AGPL-3",
     "depends": ["mail", "account"],
