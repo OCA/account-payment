4e69e1ae6e9e72dec6209ee194703340d3c5814b
Hieu.VMB <hieu.vmb@komit-consulting.com>
2023-07-07 16:17:52 +0700

    [FIX]-[15.0]-account_payment_widget_amount split wrong amount with multiple period payment term

--- account_payment_widget_amount/models/account_move_line.py
+++ account_payment_widget_amount/models/account_move_line.py
@@ -30,54 +30,111 @@ class AccountMoveLine(models.Model):
         aml_model = self.env["account.move.line"]
         partials = super(AccountMoveLine, self)._prepare_reconciliation_partials()
         if self.env.context.get("paid_amount", 0.0):
-            total_paid = self.env.context.get("paid_amount", 0.0)
+            total_paid_invoice_curr = self.env.context.get("paid_amount", 0.0)
             current_am = am_model.browse(self.env.context.get("move_id"))
             current_aml = aml_model.browse(self.env.context.get("line_id"))
             decimal_places = current_am.company_id.currency_id.decimal_places
-            if current_am.currency_id.id != current_am.company_currency_id.id:
-                total_paid = current_am.currency_id._convert(
-                    total_paid,
-                    current_aml.currency_id,
-                    current_am.company_id,
-                    current_aml.date,
-                )
+            new_partials = []
             for partial in partials:
+                if current_am.currency_id.id != current_am.company_currency_id.id:
+                    total_paid_payment_cur = current_am.currency_id._convert(
+                        total_paid_invoice_curr,
+                        current_aml.currency_id,
+                        current_am.company_id,
+                        current_aml.date,
+                    )
                 debit_line = self.browse(partial.get("debit_move_id"))
                 credit_line = self.browse(partial.get("credit_move_id"))
+                debit_amount_currency = partial.get("debit_amount_currency", 0.0)
+                credit_amount_currency = partial.get("credit_amount_currency", 0.0)
+                current_aml_is_debit_line = bool(debit_line == current_aml)
+
                 different_currency = (
                     debit_line.currency_id.id != credit_line.currency_id.id
                 )
-                to_apply = min(total_paid, partial.get("amount", 0.0))
-                partial.update(
-                    {
-                        "amount": to_apply,
-                    }
-                )
+                partial_amount = partial.get("amount", 0.0)
+                # When Payment currency is different with Invoice currency
                 if different_currency:
-                    partial.update(
-                        {
-                            "debit_amount_currency": credit_line.company_currency_id._convert(
-                                to_apply,
-                                debit_line.currency_id,
-                                credit_line.company_id,
-                                credit_line.date,
-                            ),
-                            "credit_amount_currency": debit_line.company_currency_id._convert(
-                                to_apply,
+                    if current_aml_is_debit_line:
+                        paid_in_credit_amount_currency = (
+                            debit_line.currency_id._convert(
+                                total_paid_payment_cur,
                                 credit_line.currency_id,
                                 debit_line.company_id,
                                 debit_line.date,
-                            ),
-                        }
-                    )
-                else:
+                            )
+                        )
+                        if credit_amount_currency > paid_in_credit_amount_currency:
+                            to_apply = paid_in_credit_amount_currency
+                            to_apply_company_curr = debit_line.currency_id._convert(
+                                to_apply,
+                                debit_line.company_currency_id,
+                                debit_line.company_id,
+                                debit_line.date,
+                            )
+                        else:
+                            to_apply = credit_amount_currency
+                            to_apply_company_curr = partial_amount
+                    else:
+                        paid_in_dedit_amount_currency = (
+                            credit_line.currency_id._convert(
+                                total_paid_payment_cur,
+                                debit_line.currency_id,
+                                credit_line.company_id,
+                                credit_line.date,
+                            )
+                        )
+                        if debit_amount_currency > paid_in_dedit_amount_currency:
+                            to_apply = paid_in_dedit_amount_currency
+                            to_apply_company_curr = debit_line.currency_id._convert(
+                                to_apply,
+                                debit_line.company_currency_id,
+                                debit_line.company_id,
+                                debit_line.date,
+                            )
+                        else:
+                            to_apply = debit_amount_currency
+                            to_apply_company_curr = partial_amount
                     partial.update(
                         {
+                            "amount": to_apply_company_curr,
                             "debit_amount_currency": to_apply,
                             "credit_amount_currency": to_apply,
                         }
                     )
-                total_paid -= to_apply
-                if float_compare(total_paid, 0.0, precision_digits=decimal_places) <= 0:
+                # When Payment currency is the same with Invoice currency
+                else:
+                    if current_aml_is_debit_line:
+                        to_apply = min(credit_amount_currency, total_paid_payment_cur)
+                        to_apply_company_curr = credit_line.currency_id._convert(
+                            to_apply,
+                            debit_line.company_currency_id,
+                            credit_line.company_id,
+                            credit_line.date,
+                        )
+                    else:
+                        to_apply = min(debit_amount_currency, total_paid_payment_cur)
+                        to_apply_company_curr = debit_line.currency_id._convert(
+                            to_apply,
+                            credit_line.company_currency_id,
+                            debit_line.company_id,
+                            debit_line.date,
+                        )
+                    partial.update(
+                        {
+                            "amount": min(to_apply_company_curr, partial_amount),
+                            "debit_amount_currency": to_apply,
+                            "credit_amount_currency": to_apply,
+                        }
+                    )
+                total_paid_invoice_curr -= to_apply
+                new_partials.append(partial)
+                if (
+                    float_compare(
+                        total_paid_invoice_curr, 0.0, precision_digits=decimal_places
+                    )
+                    <= 0
+                ):
                     break
+            return new_partials
         return partials
