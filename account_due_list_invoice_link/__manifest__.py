f0a5ca9dbcaeb9091265d249b56e65a74c6b4f9d
Manuel Calero <manuelcalerosolis@gmail.com>
2023-10-09 09:14:46 +0200

    [16.0][ADD] New module : Account due list invoice link

---
+++ account_due_list_invoice_link/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Xtendoo - Manuel Calero
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Account Due List Invoice Link",
+    "version": "16.0.1.0.1",
+    "category": "Generic Modules/Payment",
+    "summary": "Adds link between invoices and due list",
+    "author": "Xtendoo, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-payment",
+    "license": "AGPL-3",
+    "depends": ["account_due_list"],
+    "data": ["views/account_invoice_view.xml"],
+    "installable": True,
+}
