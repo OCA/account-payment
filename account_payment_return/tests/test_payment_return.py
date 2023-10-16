87c9a60bda08f5093de3452972ed91b412f639c8
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-16 09:13:32 +0200

    [IMP] account_payment_return: test performance improvement - Include context keys for avoiding mail operations overhead.

--- account_payment_return/tests/test_payment_return.py
+++ account_payment_return/tests/test_payment_return.py
@@ -12,6 +12,16 @@ class TestPaymentReturn(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context,
+                mail_create_nolog=True,
+                mail_create_nosubscribe=True,
+                mail_notrack=True,
+                no_reset_password=True,
+                tracking_disable=True,
+            )
+        )
         cls.journal = cls.env["account.journal"].create(
             {"name": "Test Sales Journal", "code": "tVEN", "type": "sale"}
         )
