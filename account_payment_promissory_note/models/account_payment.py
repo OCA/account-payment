a7f298b85f2b7049e2fb23deca2fa3236ca10166
Alfredo <alfredodelafuente@avanzosc.es>
2023-12-21 08:56:19 +0100

    [MIG] account_payment_promissory_note: Migration to 14.0

--- account_payment_promissory_note/models/account_payment.py
+++ account_payment_promissory_note/models/account_payment.py
@@ -10,9 +10,13 @@ class AccountPayment(models.Model):
     _inherit = ["account.payment", "account.promissory.note.mixin"]
 
     promissory_note = fields.Boolean(
-        readonly=True, states={"draft": [("readonly", False)]},
+        readonly=True,
+        states={"draft": [("readonly", False)]},
     )
-    date_due = fields.Date(readonly=True, states={"draft": [("readonly", False)]},)
+    date_due = fields.Date(
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+    )
 
     def _prepare_payment_moves(self):
         res = super()._prepare_payment_moves()
@@ -24,9 +28,21 @@ class AccountPayment(models.Model):
 
     @api.onchange("promissory_note")
     def _onchange_promissory_note(self):
-        super()._onchange_promissory_note()
+        result = super()._onchange_promissory_note()
         if not self.date_due and self.promissory_note:
-            invoices = self.invoice_ids
+            invoices = self.reconciled_invoice_ids
             same_partner = len(invoices.mapped("partner_id")) == 1
             if invoices and same_partner:
                 self.date_due = max(invoices.mapped("invoice_date_due"))
+        return result
+
+    def write(self, vals):
+        for payment in self:
+            if "promissory_note" in vals:
+                if not vals["promissory_note"]:
+                    payment.line_ids.date_maturity = vals.get("date") or payment.date
+                elif "date_due" in vals:
+                    payment.line_ids.date_maturity = vals["date_due"]
+            elif payment.promissory_note and "date_due" in vals:
+                payment.line_ids.date_maturity = vals["date_due"]
+        return super().write(vals)
--- account_payment_promissory_note/tests/test_account_payment_promissory_note.py
+++ account_payment_promissory_note/tests/test_account_payment_promissory_note.py
@@ -20,7 +20,7 @@ class TestAccountPaymentPromissoryNote(TransactionCase):
                 "partner_id": partner.id,
                 "invoice_date": "2020-09-14",
                 "invoice_date_due": "2020-09-23",
-                "type": "out_invoice",
+                "move_type": "out_invoice",
                 "invoice_line_ids": [(0, 0, {"name": "Test", "price_unit": 20})],
             }
         )
@@ -31,53 +31,51 @@ class TestAccountPaymentPromissoryNote(TransactionCase):
                 "partner_id": partner.id,
                 "invoice_date": "2020-09-14",
                 "invoice_date_due": "2020-09-22",
-                "type": "out_invoice",
+                "move_type": "out_invoice",
                 "invoice_line_ids": [(0, 0, {"name": "Test", "price_unit": 20})],
             }
         )
         self.invoice_2.action_post()
-        self.payment_1 = self.env["account.payment"].create(
-            {
-                "payment_type": "inbound",
-                "payment_method_id": self.payment_method.id,
-                "amount": 50.00,
-                "journal_id": self.env["account.journal"]
-                .search([("type", "=", "sale")], limit=1)
-                .id,
-            }
-        )
-        self.payment_2 = self.env["account.payment"].create(
-            {
-                "invoice_ids": [(4, self.invoice_1.id), (4, self.invoice_2.id)],
-                "payment_type": "inbound",
-                "payment_method_id": self.payment_method.id,
-                "amount": 50.00,
-                "journal_id": self.env["account.journal"]
-                .search([("type", "=", "sale")], limit=1)
-                .id,
-            }
-        )
 
     def test_1_onchange_promissory_note_without_invoices(self):
-        self.payment_1.date_due = "2020-09-21"
-        self.payment_1._onchange_promissory_note()
-        self.assertFalse(self.payment_1.date_due)
-        self.payment_1.promissory_note = True
-        self.payment_1.date_due = "2020-09-21"
-        self.payment_1._onchange_promissory_note()
+        payment = self.env["account.payment"].create(
+            {
+                "payment_type": "inbound",
+                "payment_method_id": self.payment_method.id,
+                "amount": 50.00,
+                "journal_id": self.env["account.journal"]
+                .search([("type", "=", "bank")], limit=1)
+                .id,
+            }
+        )
+        payment.date_due = "2020-09-21"
+        payment._onchange_promissory_note()
+        self.assertFalse(payment.date_due)
+        payment.promissory_note = True
+        payment.date_due = "2020-09-21"
+        payment._onchange_promissory_note()
         self.assertEqual(
-            self.payment_1.date_due,
+            payment.date_due,
             datetime.datetime.strptime("2020-09-21", "%Y-%m-%d").date(),
         )
 
     def test_2_onchange_promissory_note_with_invoices(self):
-        self.payment_2.date_due = "2020-09-21"
-        self.payment_2._onchange_promissory_note()
-        self.assertFalse(self.payment_2.date_due)
-        self.payment_2.promissory_note = True
-        self.payment_2._onchange_promissory_note()
+        wiz_form = Form(
+            self.env["account.payment.register"].with_context(
+                active_model="account.move",
+                active_ids=[self.invoice_1.id, self.invoice_2.id],
+            )
+        )
+        wiz_form.group_payment = True
+        wiz = wiz_form.save()
+        payment = wiz._create_payments()
+        payment.date_due = "2020-09-21"
+        payment._onchange_promissory_note()
+        self.assertFalse(payment.date_due)
+        payment.promissory_note = True
+        payment._onchange_promissory_note()
         self.assertEqual(
-            self.payment_2.date_due,
+            payment.date_due,
             datetime.datetime.strptime("2020-09-23", "%Y-%m-%d").date(),
         )
 
@@ -88,12 +86,10 @@ class TestAccountPaymentPromissoryNote(TransactionCase):
                 active_ids=[self.invoice_1.id, self.invoice_2.id],
             )
         )
-        wiz_form.payment_method_id = self.payment_method
         wiz_form.promissory_note = True
         wiz_form.group_payment = True
         wiz_form.date_due = datetime.datetime.strptime("2020-09-23", "%Y-%m-%d").date()
         wiz = wiz_form.save()
-        action_vals = wiz.create_payments()
-        payment = self.env["account.payment"].search(action_vals["domain"])
-        for line in payment.move_line_ids:
-            self.assertEquals(line.date_maturity, payment.date_due)
+        payment = wiz._create_payments()
+        for line in payment.line_ids:
+            self.assertEqual(line.date_maturity, payment.date_due)
