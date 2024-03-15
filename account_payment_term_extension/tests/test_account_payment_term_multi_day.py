3a9190cf24444c5106f66e80cecd68b2001f78c8
RicardCForgeFlow <ricard.calvo@forgeflow.com>
2024-03-15 17:01:13 +0100

    [MIG] account_payment_term_extension: Migration to 17.0

--- account_payment_term_extension/tests/test_account_payment_term_multi_day.py
+++ account_payment_term_extension/tests/test_account_payment_term_multi_day.py
@@ -31,8 +31,9 @@ class TestAccountPaymentTermMultiDay(common.TransactionCase):
                         0,
                         0,
                         {
-                            "value": "balance",
-                            "days": 5,
+                            "value": "percent",
+                            "value_amount": 100.0,
+                            "nb_days": 5,
                             "payment_days": "5",
                         },
                     )
@@ -44,7 +45,16 @@ class TestAccountPaymentTermMultiDay(common.TransactionCase):
                 "name": "Payment for days 5 and 10",
                 "active": True,
                 "line_ids": [
-                    (0, 0, {"value": "balance", "days": 0, "payment_days": "5,10"})
+                    (
+                        0,
+                        0,
+                        {
+                            "value": "percent",
+                            "value_amount": 100.0,
+                            "nb_days": 0,
+                            "payment_days": "5,10",
+                        },
+                    )
                 ],
             }
         )
@@ -60,7 +70,7 @@ class TestAccountPaymentTermMultiDay(common.TransactionCase):
                         {
                             "value": "percent",
                             "value_amount": 50.0,
-                            "days": 0,
+                            "nb_days": 0,
                             "payment_days": "15,20",
                         },
                     ),
@@ -68,8 +78,9 @@ class TestAccountPaymentTermMultiDay(common.TransactionCase):
                         0,
                         0,
                         {
-                            "value": "balance",
-                            "days": 0,
+                            "value": "percent",
+                            "value_amount": 50.0,
+                            "nb_days": 0,
                             "payment_days": "10-5",
                         },
                     ),
@@ -89,7 +100,7 @@ class TestAccountPaymentTermMultiDay(common.TransactionCase):
                             "value": "percent",
                             "value_amount": 50.0,
                             "amount_round": 1,
-                            "days": 10,
+                            "nb_days": 10,
                             "payment_days": "15,20",
                         },
                     ),
@@ -97,8 +108,9 @@ class TestAccountPaymentTermMultiDay(common.TransactionCase):
                         0,
                         0,
                         {
-                            "value": "balance",
-                            "days": 10,
+                            "value": "percent",
+                            "value_amount": 50.0,
+                            "nb_days": 10,
                             "payment_days": "10-5",
                         },
                     ),
@@ -125,13 +137,13 @@ class TestAccountPaymentTermMultiDay(common.TransactionCase):
                         {
                             "value": "percent_amount_untaxed",
                             "value_amount": 40.0,
-                            "days": 1,
+                            "nb_days": 1,
                         },
                     ),
                     (
                         0,
                         0,
-                        {"value": "balance", "days": 1},
+                        {"value": "percent", "value_amount": 100.0, "nb_days": 1},
                     ),
                 ],
             }
