53227150a11e0274dc1b6abbe9cdb684b6d34a0e
Daniel Reis <dreis@opensourceintegrators.com>
2024-09-03 14:48:44 +0100

    [ADD] account_payment_invoice_automatic: Automatically charge the Credit Card for Invoices with a stored Payment Token

---
+++ account_payment_invoice_automatic/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2024 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Invoice Automatic Credit Card Payment",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/OCA/account-payment",
+    "depends": ["account_payment"],
+    "data": ["views/account_move_views.xml"],
+    "development_status": "Beta",
+    "maintainers": ["dreispt"],
+    "installable": True,
+}
