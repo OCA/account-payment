6fe658459115829f0bc4928be228912613fcb467
Ruchir Shukla <ruchir@bizzappdev.com>
2024-09-30 16:48:44 +0530

    [MIG][15.0] account_cash_discount_payment: Migration to 15.0

--- account_cash_discount_payment/__manifest__.py
+++ account_cash_discount_payment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Cash Discount Payment",
-    "version": "14.0.1.0.2",
+    "version": "15.0.1.0.0",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-payment",
     "license": "AGPL-3",
