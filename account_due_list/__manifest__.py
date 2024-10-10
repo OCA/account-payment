174f5343ece23763d98f24f1f5465acb3e9156f6
David Ramia <david.ramia@processcontrol.es>
2024-10-10 09:33:58 +0200

    [MIG] account_due_list: Migration to 18.0

--- account_due_list/__manifest__.py
+++ account_due_list/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Payments Due list",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Generic Modules/Payment",
     "development_status": "Production/Stable",
     "author": "Odoo Community Association (OCA)",
