56f0c2a1ca2fb87534596b8893f749a5a9c903d4
Saran440 <saranl@ecosoft.co.th>
2025-01-03 16:02:12 +0700

    [FIX] send to_auto_reconcile when config state draft only

--- account_payment_register_draft/wizard/account_payment_register.py
+++ account_payment_register_draft/wizard/account_payment_register.py
@@ -3,30 +3,33 @@
 
 from odoo import models
 
+MAPPING_TYPE = {
+    "inbound": "register_customer_payment_state",
+    "outbound": "register_vendor_payment_state",
+}
+
 
 class AccountPaymentRegister(models.TransientModel):
     _inherit = "account.payment.register"
 
+    def _check_payment_draft(self):
+        return bool(self.env.company[MAPPING_TYPE.get(self.payment_type)] == "draft")
+
     def _create_payment_vals_from_wizard(self):
         payment_vals = super()._create_payment_vals_from_wizard()
-        payment_vals["to_auto_reconcile"] = self._get_batches()[0]["lines"]
+        if self._check_payment_draft():
+            payment_vals["to_auto_reconcile"] = self._get_batches()[0]["lines"]
         return payment_vals
 
     def _create_payment_vals_from_batch(self, batch_result):
         payment_vals = super()._create_payment_vals_from_batch(batch_result)
-        payment_vals["to_auto_reconcile"] = batch_result["lines"]
+        if self._check_payment_draft():
+            payment_vals["to_auto_reconcile"] = batch_result["lines"]
         return payment_vals
 
     def _create_payments(self):
         self = self.with_context(skip_clear_auto_reconcile=1)
         payments = super()._create_payments()
-        mapping_function = {
-            "inbound": "register_customer_payment_state",
-            "outbound": "register_vendor_payment_state",
-        }
-        if (
-            mapping_function.get(self.payment_type, False)
-            and self.env.company[mapping_function[self.payment_type]] == "draft"
-        ):
+        if self._check_payment_draft():
             payments.with_context(skip_clear_auto_reconcile=1).action_draft()
         return payments
