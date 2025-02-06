7ac27aa18032b7901f2dc43e7b98d44fda020453
abdessamad salim <abdessamad.salim@smile-maroc.com>
2025-02-06 15:30:59 +0100

    [18.0][MIG] account_payment_return_import: Migration to 18.0

--- account_payment_return_import/tests/test_import_base.py
+++ account_payment_return_import/tests/test_import_base.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.exceptions import UserError
-from odoo.tests.common import Form
+from odoo.tests import Form
 
 from .test_import_file import TestPaymentReturnFile
 
