dd5ef281c0efd757e18bbdfc5f623aa12b11838b
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2025-03-25 11:17:57 +0530

    [MIG] account_payment_show_invoice: Migration to 18.0

--- account_payment_show_invoice/__manifest__.py
+++ account_payment_show_invoice/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2025 ForgeFlow, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment Show Invoice",
     "summary": "Extends the tree view of payments to show the paid invoices "
     "related to the payments using the vendor reference by default",
-    "version": "13.0.1.0.1",
+    "version": "18.0.1.0.0",
     "category": "Account-payment",
     "website": "https://github.com/OCA/account-payment",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
--- account_payment_show_invoice/readme/CONTRIBUTORS.md
+++ account_payment_show_invoice/readme/CONTRIBUTORS.md
@@ -1,3 +1,3 @@
-- Lois Rilo \<<lois.rilo@eficent.com>\>
+- Lois Rilo \<<lois.rilo@forgeflow.com>\>
 - Kitti Upariphutthiphong \<<kittiu@ecosoft.co.th>\>
 - Manuel Calero \<<manuelcalerosolis@gmail.com>\>
