f882e7a1ee1151a62a07cac437856222a9877d21
Anusha <anusha.pp@calin.co.in>
2025-05-26 09:03:43 +0200

    [MIG] account_voucher_killer: Migration to 18.0
    [IMP]account_voucher_killer : view name changed

---
+++ account_voucher_killer/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2013 Camptocamp SA (http://www.camptocamp.com)
+# @author Nicolas Bessi
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Accounting Payment Access",
+    "summary": "Prevent the usage of payments from invoices",
+    "version": "18.0.1.0.0",
+    "category": "Accounting & Finance",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-payment",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["security/invoice_data.xml", "view/invoice_view.xml"],
+    "installable": True,
+}
