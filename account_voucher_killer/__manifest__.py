e3aec03339750ab7a04dc733a5b4587b05c1ad95
Anusha <anusha.pp@calin.co.in>
2025-06-24 07:10:13 +0200

    [MIG] account_voucher_killer: Migration to 18.0

--- account_voucher_killer/__manifest__.py
+++ account_voucher_killer/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Accounting Payment Access",
     "summary": "Prevent the usage of payments from invoices",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting & Finance",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-payment",
