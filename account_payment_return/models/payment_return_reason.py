1c6475ba14d5a96e9447d9de2564904d5a6db82b
Víctor Martínez <victor.martinez@tecnativa.com>
2025-08-18 17:24:32 +0200

    [IMP] account_payment_return: Set search method for the display_name field to prevent error log
    ERROR devel odoo.osv.expression: Non-stored field payment.return.reason.display_name cannot be searched.
    NoneType: None

--- account_payment_return/models/payment_return_reason.py
+++ account_payment_return/models/payment_return_reason.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+from odoo.osv import expression
 
 
 class PaymentReturnReason(models.Model):
@@ -11,9 +12,25 @@ class PaymentReturnReason(models.Model):
 
     code = fields.Char()
     name = fields.Char(string="Reason", translate=True)
-    display_name = fields.Char(compute="_compute_display_name")
+    display_name = fields.Char(
+        compute="_compute_display_name", search="_search_display_name"
+    )
 
     @api.depends("code", "name")
     def _compute_display_name(self):
         for record in self:
             record.display_name = f"[{record.code}] {record.name}"
+
+    @api.model
+    def _search_display_name(self, operator, value):
+        domain = []
+        for f_name in self._rec_names_search:
+            domain_item = [(f_name, operator, value)]
+            if domain == []:
+                domain = domain_item
+            else:
+                if operator in expression.NEGATIVE_TERM_OPERATORS:
+                    domain = expression.AND([domain, domain_item])
+                else:
+                    domain = expression.OR([domain, domain_item])
+        return domain
