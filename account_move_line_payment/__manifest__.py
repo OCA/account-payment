bfe02a9d5d22a2d57389df7b462450035fbab509
RedaKarimi <101425667+RedaKarimi@users.noreply.github.com>
2025-10-20 15:58:34 +0200

    [IMP] account_move_line_payment: pre-commit auto fixes

--- account_move_line_payment/__manifest__.py
+++ account_move_line_payment/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Register due payments",
     "summary": "Register only due payments",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "category": "Accounting/Payment",
     "website": "https://github.com/OCA/account-payment",
