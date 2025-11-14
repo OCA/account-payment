32747754446344cfed11fd13c861200885e6c01e
Edilio Escalona Almira <ediliodevelop1991@gmail.com>
2025-11-14 11:13:16 -0500

    [MIG] account_payment_term_partner_paydays: Migration to 17.0

--- account_payment_term_partner_paydays/__manifest__.py
+++ account_payment_term_partner_paydays/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Payment Term - Partner Payment Days",
-    "version": "18.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Accounting & Finance",
     "summary": "Allows to define payment days for partners.",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
