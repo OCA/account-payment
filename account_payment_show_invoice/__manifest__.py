f406c0ad33c11659eeda7bbada47a5aac849424a
Don Kendall <dkendall@ledoweb.com>
2026-05-28 08:26:35 -0400

    [MIG] account_payment_show_invoice: Migration to 19.0

--- account_payment_show_invoice/__manifest__.py
+++ account_payment_show_invoice/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Account Payment Show Invoice",
     "summary": "Extends the tree view of payments to show the paid invoices "
     "related to the payments using the vendor reference by default",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Account-payment",
     "website": "https://github.com/OCA/account-payment",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
